2b00cc98b4e5b31906c162f413d3a041bf548c0a
Emilia Andrzejewska <alime1722@gmail.com>
2020-01-30 21:54:17 +0100

    Add users index and show action (milestone 3)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'users#index'
+
   devise_for :users
+
+  resources :users, only: [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
