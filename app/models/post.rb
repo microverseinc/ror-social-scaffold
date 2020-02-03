3303e1ac4ba1ff05e418b1b9407ead16c261a93d
Emilia Andrzejewska <alime1722@gmail.com>
2020-02-03 16:22:03 +0100

    Add validations to posts and users (milestone 3)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 20 }
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 20 }
+
   has_many :posts
 end
