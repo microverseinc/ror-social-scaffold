9a28657f53597864e09e49e009c220f8497080aa
Emilia Andrzejewska <alime1722@gmail.com>
2020-02-03 16:22:03 +0100

    Add validations to posts and users (milestone 3)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
-    @posts = current_user.posts.includes(:user)
+    timeline_posts
   end
 
   def create
@@ -12,11 +12,16 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, notice: 'Post was successfully created.'
     else
-      render :new, error: 'Post was not created.'
+      timeline_posts
+      render :index, alert: 'Post was not created.'
     end
   end
 
   private
+
+  def timeline_posts
+    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+  end
 
   def post_params
     params.require(:post).permit(:content)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  validates :content, presence: true
+
   belongs_to :user
+
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
