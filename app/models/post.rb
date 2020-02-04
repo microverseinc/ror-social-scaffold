ec6e0cc52e9a032cfcda2a2c38f3788bea4a5e3c
jofer86 <jofer86@gmail.com>
2020-02-04 10:57:20 -0600

    Solve merge conflicts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
