0d722dd93971caada5ad09dcacbf4bea9563f133
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-26 22:40:45 +0100

    add associations to friendship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def send_request
+    current_user.friendships.create(friend_id: params[:id])
+    flash[:success] = 'Friend request sent'
+    redirect_to root_path
+  end
+
+  def accept_request
+    user = User.find(params[:id])
+    current_user.confirm_friend(user)
+    flash[:success] = "You are now friends with #{user.name}"
+    redirect_to root_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+   post 'user/sendrequest/:id', to: 'users#send_request', as: 'sendrequest'
+  post 'user/recieverequest/:id', to: 'users#accept_request', as: 'recieverequest'
+
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
