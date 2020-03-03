0d804ac6be989d391b690ddf1e83f33c2ee5ab64
Elijahscriptdev <elijahobominuru@gmail.com>
2020-03-03 17:00:38 +0100

    add routes and view friend requests

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
    post 'user/sendrequest/:id', to: 'users#send_request', as: 'sendrequest'
   post 'user/recieverequest/:id', to: 'users#accept_request', as: 'recieverequest'
 
+  delete 'friendships/unfriend/:id', to: 'friendships#unfriend', as: 'unfriend'
+
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
+  
+  resources :friendships, except: [:edit, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
