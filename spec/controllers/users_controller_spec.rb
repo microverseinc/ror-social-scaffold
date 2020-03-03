87529ddcccf8f4cec65f142dfad4d21d7f2e69f4
Elijahscriptdev <elijahobominuru@gmail.com>
2020-03-03 17:16:39 +0100

    add test for controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+    before(:each) do
+    @user = create(:user)
+    @user_two = create(:user, email: 'example@example.com', password: 'example', name: 'Example')
+  end
+end
