85c60694b663df2e8e32ac514578816d9808ad28
davidauza.engineer <davidauza01@gmail.com>
2020-03-04 12:47:58 -0500

    WIP: Implement friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: {maximum: 20}
 
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  # Returns an array with the friends
+  def friends
+    friends_array = friendships.map do |friendship|
+      friendship.friend if friendship.confirmed?
+    end
+    friends_array + inverse_friendships.map do |friendship|
+      friendship.user if friendship.confirmed?
+    end
+  end
+
+  def pending_friends
+    friendships.map do |friendship|
+      friendship.friend unless friendship.confirmed?
+    end
+  end
 end
