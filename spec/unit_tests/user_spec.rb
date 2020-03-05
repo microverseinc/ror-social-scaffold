65de3c5c7bd9da426ab6179973599f6b8614ab4b
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-05 16:44:37 -0400

    Add test for the list of friends

--- spec/unit_tests/user_spec.rb
+++ spec/unit_tests/user_spec.rb
@@ -1,18 +1,7 @@
 require 'spec_helper'
-# require '../app/models/user'
+require 'rails_helper'
 
 RSpec.describe 'User model tests', type: :model do
-  let(:test_user) {
-    User.new(name: 'test_user', email: 'test@user.com',
-                             password: 'secret', password_confirmation: 'secret') }
-  let(:test_user_2) { 
-    User.new(name: 'test_user_2', email: 'test2@user.com',
-                               password: 'secret', password_confirmation: 'secret') }
-  let(:test_user_3) { 
-    User.new(name: 'test_user_3', email: 'test3@user.com',
-                               password: 'secret', password_confirmation: 'secret') }
-  let(:test_friendship) { Friendship.new(user_id: 1, friend_id: 2, confirmed: true) }
-  let(:test_friendship_2) { Friendship.new(user_id: 3, friend_id: 1, confirmed: true) }
 
   def store_in_database
     test_user.save
@@ -23,11 +12,22 @@ RSpec.describe 'User model tests', type: :model do
   end
 
   describe 'instance methods' do
+    let(:test_user) {
+      User.new(name: 'test_user', email: 'test@user.com',
+                               password: 'secret', password_confirmation: 'secret') }
+    let(:test_user_2) { 
+      User.new(name: 'test_user_2', email: 'test2@user.com',
+                                 password: 'secret', password_confirmation: 'secret') }
+    let(:test_user_3) { 
+      User.new(name: 'test_user_3', email: 'test3@user.com',
+                                 password: 'secret', password_confirmation: 'secret') }
+    let(:test_friendship) { Friendship.new(user_id: 1, friend_id: 2, confirmed: true) }
+    let(:test_friendship_2) { Friendship.new(user_id: 3, friend_id: 1, confirmed: true) }
     it 'returns the correct list of friends' do
       store_in_database
       user = User.first
       user_friends = user.friends
-      expect(user_friends.include?(User.second.id) && user_friends.include?(User.third.id)).to eq(true)
+      expect(user_friends.include?(User.second) && user_friends.include?(User.third)).to eq(true)
     end
   end
 end
