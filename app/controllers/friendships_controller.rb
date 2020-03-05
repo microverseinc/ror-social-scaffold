4067d5ca478a974630bdd2d105fc10361db7f33f
davidauza.engineer <davidauza01@gmail.com>
2020-03-05 14:37:33 -0500

    Add friendship validation

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,11 @@
 class FriendshipsController < ApplicationController
 
   def create
-    Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id], confirmed: false)
+    if friendship_valid?(params[:user_id], params[:friend_id])
+      Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id], confirmed: false)
+    else
+      flash[:alert] = 'Something went wrong!'
+    end
     redirect_to users_url
   end
 
@@ -15,5 +19,11 @@ class FriendshipsController < ApplicationController
     friendship = Friendship.find(params[:id])
     friendship.destroy
     redirect_to users_url
+  end
+
+  private
+
+  def friendship_valid?(user_id, friend_id)
+    !Friendship.exists?(user_id: user_id, friend_id: friend_id) && !Friendship.exists?(user_id: friend_id, friend_id: user_id)
   end
 end
