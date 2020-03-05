5abf598c721c396b86151ea630ca8b6a0d0fb64c
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-05 17:07:11 -0400

    Add tests for friendship invitation detection methods

--- spec/unit_tests/user_spec.rb
+++ spec/unit_tests/user_spec.rb
@@ -21,13 +21,28 @@ RSpec.describe 'User model tests', type: :model do
     let(:test_user_3) { 
       User.new(name: 'test_user_3', email: 'test3@user.com',
                                  password: 'secret', password_confirmation: 'secret') }
-    let(:test_friendship) { Friendship.new(user_id: 1, friend_id: 2, confirmed: true) }
-    let(:test_friendship_2) { Friendship.new(user_id: 3, friend_id: 1, confirmed: true) }
+    let(:test_friendship) { Friendship.new(user_id: test_user.id, friend_id: test_user_2.id, confirmed: true) }
+    let(:test_friendship_2) { Friendship.new(user_id: test_user_3.id, friend_id: test_user.id, confirmed: true) }
     it 'returns the correct list of friends' do
       store_in_database
       user = User.first
       user_friends = user.friends
       expect(user_friends.include?(User.second) && user_friends.include?(User.third)).to eq(true)
+    end
+
+    it 'returns the sent invitations' do
+      store_in_database
+      test_friendship.update(confirmed: false)
+
+      expect(test_user.friends_requests_sent).to include(test_user_2)
+    end 
+
+
+    it 'returns the received invitations' do
+      store_in_database
+      test_friendship_2.update(confirmed: false)
+
+      expect(test_user.friends_requests_received).to include(test_user_3)
     end
   end
 end
