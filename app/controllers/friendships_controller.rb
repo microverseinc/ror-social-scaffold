16a994726b56541ea0b190694afd70312e936228
davidauza.engineer <davidauza01@gmail.com>
2020-03-05 11:27:35 -0500

    Implement friendship controller methods for users#index

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,19 @@
 class FriendshipsController < ApplicationController
 
   def create
-    flash.now[:alert] = 'Invitation sent'
-    render controller: 'user', action: 'index'
+    Friendship.create(user_id: params[:user_id], friend_id: params[:friend_id], confirmed: false)
+    redirect_to users_url
   end
 
   def update
     friendship = Friendship.find(params[:id])
     friendship.update(confirmed: true)
+    redirect_to users_url
+  end
+
+  def destroy
+    friendship = Friendship.find(params[:id])
+    friendship.destroy
     redirect_to users_url
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,13 +2,10 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  # def self.current_friendship(user_id, friend_id)
-  #   Friendship.where("user_id = #{user_id} AND friend_id = #{friend_id}").first.id
-  # end
-
   def Friendship.get_friendship(friendship_list, user_id, friend_id)
     correct_friendship = friendship_list.find do |friendship|
-      friendship[:user_id] == user_id && friendship[:friend_id] == friend_id
+      (friendship[:user_id] == user_id && friendship[:friend_id] == friend_id) ||
+          (friendship[:user_id] == friend_id && friendship[:friend_id] == user_id)
     end
     correct_friendship.id
   end
