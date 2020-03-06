09a64e82361dc2cd666fef97d274fbfbf4663271
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-06 11:11:10 -0400

    Add test for #show method of users_controller

--- spec/integration_tests/users_controller_spec.rb
+++ spec/integration_tests/users_controller_spec.rb
@@ -14,12 +14,24 @@ RSpec.describe 'Integration tests for the user controller', type: :feature do
     test_user_2.save
   end
 
-  scenario 'displaying all the users' do
-    store_in_database
+  def log_in
     visit users_path
     fill_in 'Email', with: test_user.email
     fill_in 'Password', with: test_user.password
     click_button 'Log in'
+  end
+
+  scenario 'displaying all the users' do
+    store_in_database
+    log_in
     expect(page).to have_selector 'li', text: 'test_user_2'
+  end
+
+  scenario "'See profile' button shows the right user" do
+    store_in_database
+    log_in
+    # profile_link = find("#user-#{test_user_2.id}")
+    click_link("user-#{test_user_2.id}")
+    expect(page).to have_selector 'h2', text: "#{test_user.name}"
   end
 end
