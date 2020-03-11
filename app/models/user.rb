84a4b1c4138fe7fec4dd4e6d7d55fc34bf6850d2
Michael Zhu <michaelzhu63@hotmail.com>
2020-03-10 21:28:46 -0400

    Rubocop favors unless for negative conditions

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,12 +20,12 @@ class User < ApplicationRecord
 
   # Users who have YET to confirm friend requests
   def pending_friends
-    friendships.map { |friendship| friendship.friend unless !friendship.status }.compact
+    friendships.map { |friendship| friendship.friend unless friendship.status }.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless !friendship.status }.compact
+    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
   end
 
   def confirm_friend(user)
