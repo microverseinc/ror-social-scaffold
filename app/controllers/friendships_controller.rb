eaa7d720fcd932c665b4045788d505512518b5de
TashfeenRao <raotashfeen25@gmail.com>
2020-03-11 23:49:05 +0500

    botstrap and styling added

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,5 @@
 class FriendshipsController < ApplicationController
-
+  before_action :authenticate_user!
   def send_request
     friendShip = Friendship.create(user_id:current_user.id,
                                   friend_id: params[:friend_id],
