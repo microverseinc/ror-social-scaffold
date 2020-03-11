7edaa8965904611e2fc85791bd29093505d9e17a
konyan <devkonyan@gmail.com>
2020-03-12 00:37:06 +0630

    add feature testing  for friend request send and add friend and cancel friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,8 +5,7 @@ class FriendshipsController < ApplicationController
                                   friend_id: params[:friend_id],
                                   status: false)
     if friendShip.save
-      flash.now[:success] = "Send your friend request successfully"
-      redirect_to users_path
+      redirect_to users_path, notice: 'Send your friend request successfully'
     end
   end
 
@@ -14,18 +13,18 @@ class FriendshipsController < ApplicationController
     accept = Friendship.find_by(id: params[:id])
     if accept
     accept.update(status: true)
-    flash[:success] = "You accepted request"
-    redirect_to requests_path
+    redirect_to requests_path , notice: 'You accepted request'
     end
   end
+
   def decline
     decline = Friendship.find_by(id: params[:id])
     if decline
       decline.destroy
-      flash[:danger] = "You decline request"
-      redirect_to requests_path
+      redirect_to requests_path, notice: 'You decline request'
     end
   end
+
   def show_requests
     @request = Friendship.where(friend_id:current_user.id,status: false)
   end
---
+++ spec/request/friendship_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.feature 'Friendships', type: :feature do
+  before do
+    @user = User.create(name: 'Tash', email: 't@mail.com', password: 'foobar')
+    @user2 = User.create(name: 'Nyan', email: 'n@mail.com', password: '123456')
+    @user3 = User.create(name: 'User', email: 'u@mail.com', password: '123456')
+    @friendShip = Friendship.create(user_id: @user2.id,friend_id:@user.id,status:false)
+    @friendShip2 = Friendship.create(user_id: @user3.id,friend_id:@user.id,status:false)
+  end
+
+  scenario 'click friend request invite send' do
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+    expect(find('.notice')).to have_content('Signed in successfully.')
+    visit users_path
+    expect(find('.users-section')).to have_content('Send Friend Request >>')
+    assert_selector 'ul.users-list' do
+      assert_selector '.friend-request'
+      click_link('Send Friend Request >>',match: :first)
+    end
+    expect(find('.notice')).to have_content('Send your friend request successfully')
+  end
+
+  scenario 'accept friend request' do
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+    expect(find('.notice')).to have_content('Signed in successfully.')
+    visit requests_path
+    expect(find('h1').text).to have_content('Requests List')
+    assert_selector 'ul' do
+      assert_selector 'li'
+      find_link('Accept', match: :first).visible?
+      click_link('Accept', match: :first)
+    end
+    expect(find('.notice')).to have_content('You accepted request')
+  end
+
+  scenario 'cancel friend request' do
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Log in'
+    expect(find('.notice')).to have_content('Signed in successfully.')
+    visit requests_path
+    expect(find('h1').text).to have_content('Requests List')
+    assert_selector 'ul' do
+      assert_selector 'li'
+      find_link('Decline', match: :first).visible?
+      click_link('Decline', match: :first)
+    end
+    expect(find('.notice')).to have_content('You decline request')
+  end
+end
