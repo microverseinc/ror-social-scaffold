b72543e1bf2052e4768ca3cbe31da339751ffaf1
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-03-13 14:42:39 -0600

    add unit test for the friendship model

---
+++ spec/model/friendship_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe Friendship, type: :model do
+  describe 'friendship' do
+
+    let (:user1) { User.create(name: "Example User", email: "example@email.com", password: "foobar", password_confirmation: "foobar") }
+    let (:user2) { User.create(name: "Other User", email: "other@email.com", password: "foobar", password_confirmation: "foobar") }
+    let (:friend_request) { Friendship.create(user_id: user1.id, friend_id: user2.id) }
+
+    it 'belongs_to user' do
+      expect(friend_request).to eq(user1.friendships.first)
+    end
+
+    it 'belongs_to user' do
+        expect(friend_request).to eq(user2.inverse_friendships.first)
+      end
+  end
+
+  describe 'associations' do
+    it { should belong_to(:friend).class_name('User') }
+
+    it { should belong_to(:user)}
+  end
+end
