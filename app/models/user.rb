32448c2ef7fb02976d906c4b4cf429b3e71532bd
Ivan <fivanunam@gmail.com>
2020-03-17 17:21:35 -0600

    add forms for friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,34 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships, dependent: :destroy
   has_many :oposite_friendships, foreign_key: 'friend_id', class_name: 'Friendship', dependent: :destroy
+
+  def friends
+    friends = friendships.map do |friendship| 
+      friendship.friend if friendship.status
+    end
+    friends = friends + oposite_friendships.map do |friendship| 
+      friendship.user if friendship.status
+    end
+    friends.compact
+  end
+
+  def requested_friends
+    friendships.map{ |friendship| friendship.friend if !friendship.status }.compact
+  end
+
+  def pending_friends
+    oposite_friendships.map{ |friendship| friendship.user if !friendship.status }.compact
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
+  def pending?(user)
+    pending_friends.include?(user)
+  end
+
+  def requested?(user)
+    requested_friends.include?(user)
+  end
 end
