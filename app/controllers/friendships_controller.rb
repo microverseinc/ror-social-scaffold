832ab9de33fd605385c95b4b982100b57c8feb32
certilremy <certilremy@gmail.com>
2020-03-18 16:59:04 -0400

    fixed some linters errors

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,23 +4,23 @@ class FriendshipsController < ApplicationController
     current_user.request_friendship(@user)
     redirect_to @user
   end
- 
-   def cancel_friend_request
+
+  def cancel_friend_request
     @user = User.find(params[:id])
     friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
     friendship.destroy
-    flash[:notice] = "You have canceled the friend request"
+    flash[:notice] = 'You have canceled the friend request'
     redirect_to @user
-   end
+  end
 
-   def accept_friend_request
+  def accept_friend_request
     @user = User.find(params[:id])
     friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
-      friendship.update(status: true)
-      flash[:notice] = "You have accepted #{@user.name} as your friend!!!"
-      redirect_to @user
-   end
-  
+    friendship.update(status: true)
+    flash[:notice] = "You have accepted #{@user.name} as your friend!!!"
+    redirect_to @user
+  end
+
   def destroy
     @user = User.find(params[:id])
     friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
@@ -28,6 +28,4 @@ class FriendshipsController < ApplicationController
     flash[:notice] = "You have canceled #{@user.name} request to be your friend!"
     redirect_to @user
   end
-
-  
 end
