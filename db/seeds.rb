abb9c916ffefc2f61d32bdc4f8152cc6a5495c4b
NewIncome <alfredo_cardenas@live.com>
2020-03-18 12:30:13 -0600

    Add the friendship table with some relationships for the user and the friendship model. And tried to add some automatic seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do |i|
+  User.create(
+    name: "user#{i}",
+    email: "user#{i}@email.com",
+    password: 'password'
+  )
+end
+
+5.times do
+  Friendship.create(
+    user_id: Random.rand(1..15),
+    friend_id: val=Random.rand(1..15) == :user_id ? Random.rand(1..15) : val,
+    confirmed: [true, false].sample
+  )
+end
