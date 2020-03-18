77cd426b493e4cac9d8cb43b9a49e0cc536a538e
certilremy <certilremy@gmail.com>
2020-03-18 16:56:33 -0400

    add class to button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,37 +1,33 @@
 class FriendshipsController < ApplicationController
-  before_action :set_user
-  before_action :set_friendship, except: %i[create]
-
   def create
+    @user = User.find(params[:friend_id])
     current_user.request_friendship(@user)
     redirect_to @user
   end
-
-  def cancel_friend_request
-    @friendship.destroy
-    flash[:notice] = 'You have canceled the friend request'
+ 
+   def cancel_friend_request
+    @user = User.find(params[:id])
+    friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
+    friendship.destroy
+    flash[:notice] = "You have canceled the friend request"
     redirect_to @user
-  end
+   end
 
-  def accept_friend_request
-    @friendship.update(status: true)
-    flash[:notice] = "You have accepted #{@user.name} as your friend!!!"
-    redirect_to @user
-  end
-
+   def accept_friend_request
+    @user = User.find(params[:id])
+    friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
+      friendship.update(status: true)
+      flash[:notice] = "You have accepted #{@user.name} as your friend!!!"
+      redirect_to @user
+   end
+  
   def destroy
-    @friendship.destroy
+    @user = User.find(params[:id])
+    friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
+    friendship.destroy
     flash[:notice] = "You have canceled #{@user.name} request to be your friend!"
     redirect_to @user
   end
 
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def set_friendship
-    @friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
-  end
+  
 end
