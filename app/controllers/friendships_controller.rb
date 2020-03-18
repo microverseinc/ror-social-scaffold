54320a018ca229e1e8de4820ef06fdeace38c6cb
certilremy <certilremy@gmail.com>
2020-03-18 18:07:28 -0400

    fixed freinds buggs

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -15,7 +15,7 @@ class FriendshipsController < ApplicationController
 
   def accept_friend_request
     @user = User.find(params[:id])
-    friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
+    friendship = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
     friendship.update(status: true)
     flash[:notice] = "You have accepted #{@user.name} as your friend!!!"
     redirect_to @user
@@ -23,7 +23,7 @@ class FriendshipsController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    friendship = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
+    friendship = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
     friendship.destroy
     flash[:notice] = "You have canceled #{@user.name} request to be your friend!"
     redirect_to @user
