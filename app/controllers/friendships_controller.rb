b44bd2055785789dde08bf6f136dbc1da47c717d
certilremy <certilremy@gmail.com>
2020-03-18 15:32:35 -0400

    Add acept friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,9 +7,17 @@ class FriendshipsController < ApplicationController
  
    def cancel_friend_request
     @user = User.find(params[:id])
-    current_user.pending_requested_friendship.delete(@user)
+    @user.inverse_friendships.delete(current_user.id)
     current_user.requested_friends.delete(@user)
     redirect_to @user
+   end
+
+   def accept_friend_request
+      @user = User.find(params[:id])
+      friendship = Friendship.find_by(friend_id: params[:user_id], user_id: current_user.id)
+      friendship.update(status: true)
+      flash[:notice] = "You have accepted #{user.name} as your friend!!!"
+      redirect_to @user
    end
   
   def destroy
