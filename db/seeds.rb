e5bc7f5ac6831ab8a0baecee5326991a0f898093
Ivan <fivanunam@gmail.com>
2020-03-19 12:58:30 -0600

    add friendships

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,29 @@ User.create!(name:  "ivancito",
 
             
 20.times do |n|
-  name  = Faker::Name.name[0..20]
+  name  = Faker::Name.name[0..19]
   email = "example-#{n+1}@gmail.com"
   password = "password"
   User.create!(name:  name,
               email: email,
               password:              password,
               password_confirmation: password)
+end
+
+users = User.order(:created_at).take(6)
+count = 1
+users.each do |user|
+  content = Faker::Lorem.sentence(5) + " #{count}"
+  user.posts.create!(content: content)
+  count = count + 1
+end
+
+user_to_add_friends = User.find(4)
+users = User.order(:id).take(3)
+users.each do |user|
+  user_to_add_friends.requested_friends << user
+end
+user_to_add_friends.friendships.each do |friendship|
+  friendship.status = true
+  friendship.save
 end
