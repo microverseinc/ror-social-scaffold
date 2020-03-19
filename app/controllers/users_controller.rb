f5f47e54ee4ed99d400d921071ca07cd994f71a1
Ivan <fivanunam@gmail.com>
2020-03-18 20:08:39 -0600

    add store location for index and show views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    store_location_for(:user, users_path)
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    store_location_for(:user, user_path(@user))
     @posts = @user.posts.ordered_by_most_recent
   end
 end
