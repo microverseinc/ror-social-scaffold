94645def604c5e7ebcdf01ed1aba879c394e4db1
Ivan <fivanunam@gmail.com>
2020-03-19 20:57:27 -0600

    setting factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
+require_relative './suppor/factory_bot.rb'
 
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
