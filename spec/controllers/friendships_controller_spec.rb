e054da3f45d3e9c3e8a993a93a17886e18125156
Ivan <fivanunam@gmail.com>
2020-03-19 23:07:12 -0600

    change controller test

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -1,17 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe FriendshipsController, type: :controller do
-  describe 'GET #create' do
-    it 'returns http success' do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe 'friendship' do
+    before(:all) do
+      @ivan = create(:user)
+      @remy = create(:user)
     end
-  end
 
-  describe 'GET #destroy' do
-    it 'returns http success' do
-      get :destroy
-      expect(response).to have_http_status(:success)
+    it 'request' do
+      sign_in @ivan
+      post :create, :params => { :friend_id => @remy.id }
+      expect(response).to have_http_status(302)
+    end
+
+    it 'cancel' do
+      sign_in @ivan
+      post :create, :params => { :friend_id => @remy.id }
+      post :cancel_friend_request, :params => { :id => @remy.id }
+      expect(response).to have_http_status(302)
+    end
+
+    it 'accept' do
+      sign_in @ivan
+      post :create, :params => { :friend_id => @remy.id }
+      sign_out @ivan
+      sign_in @remy
+      post :accept_friend_request, :params => { :id => @ivan.id }
+      expect(response).to have_http_status(302)
+    end
+
+    it 'reject' do
+      sign_in @ivan
+      post :create, :params => { :friend_id => @remy.id }
+      sign_out @ivan
+      sign_in @remy
+      delete :destroy, :params => { :id => @ivan.id }
+      expect(response).to have_http_status(302)
     end
   end
 end
