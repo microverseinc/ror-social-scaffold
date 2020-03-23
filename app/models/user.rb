57b801b174d0ea862c33caedd55bc772b8ceab19
sick-carlito <carlb1319@gmail.com>
2020-03-24 02:01:23 +0300

    Added friendships model, created the associations and worked on the expected views page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,35 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friendship_id"
+
+  def friends
+  	friends_array = friendship.map{| friendship | friendship.friend if friendship.confirmed }
+  	friends_array + inverse_friendships.map{ |friendship| friendship.user if friendship.confirmed }
+  	friends_array.compact
+  end
+
+
+ def pending_friends
+ 	friendships.map{ |friendship| friendship.friend if !friendship.confirmed }.compact
+ end
+
+ def friend_requests
+ 	inverse_friendships.map{ |friendship| friendship.user if !friendship.confirmed }.compact
+ end
+
+
+ def confirm_friend(user)
+ 	friendship = inverse_friendships.find{ |friendship| friendship.user == user }
+ 	friendship.confirm = true
+ 	friendship.save
+ end
+
+
+ def friend?(user)
+ 	friends.include?(user)
+ end
+
 end
