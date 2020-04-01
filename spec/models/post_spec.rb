dd43aa84708b4f2273469b2c8291663ad38605e4
Amadou Ibrahim <amadou140589@gmail.com>
2020-04-01 02:39:07 +0100

    Post  model test added

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) { Post.new(content: 'A simple post content') }
+
+  it 'has to have a content' do
+    expect(post.content).to eq('A simple post content')
+  end
+  it 'returns an error for missing content' do
+    post.content = nil
+    expect(post.save).to eq(false)
+  end
 end
