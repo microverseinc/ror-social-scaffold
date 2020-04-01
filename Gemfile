fab6064ead1ed93df06f8f16b1b70c8312792487
Amadou Ibrahim <amadou140589@gmail.com>
2020-04-01 01:10:34 +0100

    Page UI edited. menu helpers edited

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-
+gem "font-awesome-rails"
 gem 'devise'
 
 group :development, :test do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,22 @@
 module ApplicationHelper
-  def menu_link_to(link_text, link_path)
+  def menu_link_to(link_text, link_path,icon)
     class_name = current_page?(link_path) ? 'navbar-item is-active' : 'navbar-item'
 
-    link_to link_text, link_path,class:class_name
-    # content_tag(:div, class: class_name) do
-    # end
+    # link_to link_text, link_path,class:class_name
+    # content_tag(:a,href: link_path,link_text, class: class_name)
+    content_tag :a, :href => "#{link_path}", class: class_name do
+      fa_icon("#{icon}", text: "#{link_text}")
+      
+    end
+    
   end
 
   def like_or_dislike_btn(post)
     like = Like.find_by(post: post, user: current_user)
     if like
-      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete)
+      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete,class:"tag is-danger is-rounded")
     else
-      link_to('Like!', post_likes_path(post_id: post.id), method: :post)
+      link_to('Like!', post_likes_path(post_id: post.id), method: :post,class:"tag is-light is-rounded")
     end
   end
 end
