8c30188c572b4adf718be69202fa89d02fefdbf4
Amadou Ibrahim <amadou140589@gmail.com>
2020-04-01 02:49:16 +0100

    Like Model test added

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  let(:user) { User.new(name: 'JohnDoe', email: 'johndoe@ymail.com', password: 'password') }
+  let(:post) { Post.new(content: 'A simple post content',user_id:user.id) }
+  let(:like) { Like.new(post_id: post.id,user_id:user.id) }
+
+  context 'it has name, email and password' do
+
+    it 'has to have a post_id' do
+      expect(like.post_id).to eq(post.id)
+    end
+    it 'has to have a user_id' do
+      expect(like.user_id).to eq(user.id)
+    end
+  end
+  context 'it has to return an error for missing fields' do
+    it 'returns an error for missing post_id' do
+      like.post_id = nil
+      expect(like.save).to eq(false)
+    end
+    it 'returns an error for missing user_id' do
+      like.user_id = nil
+      expect(like.save).to eq(false)
+    end
+  end
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { Post.new(content: 'A simple post content') }
+  let(:user) { User.new(name: 'JohnDoe', email: 'johndoe@ymail.com', password: 'password') }
+  let(:post) { Post.new(content: 'A simple post content',user_id:user.id) }
 
   it 'has to have a content' do
     expect(post.content).to eq('A simple post content')
   end
+  it 'has to have a user_id' do
+    expect(post.user_id).to eq(user.id)
+  end
   it 'returns an error for missing content' do
     post.content = nil
+    expect(post.save).to eq(false)
+  end
+  it 'returns an error for missing user_id' do
+    post.user_id = nil
     expect(post.save).to eq(false)
   end
 end
