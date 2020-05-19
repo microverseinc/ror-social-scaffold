bedc7ec4b71500d58981d27b8c21ae1606f5d713
SpaYco <zizou.aziz101@gmail.com>
2020-05-19 11:42:53 +0100

    removed tabs

--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -11,14 +11,14 @@ describe 'Post controller and view spec', type: :feature do
     visit '/users/sign_in'
     within('form') do
       fill_in 'user[email]', with: 'user@example.com'
-      fill_in 'user[password]',	with: 'password1234'
+      fill_in 'user[password]', with: 'password1234'
     end
     click_button 'commit'
   end
   it 'post creation' do
     visit '/posts'
     within('form') do
-      fill_in 'post[content]',	with: 'blah blah blah'
+      fill_in 'post[content]', with: 'blah blah blah'
     end
     click_button 'commit'
     expect(page).to have_content 'blah blah blah'
