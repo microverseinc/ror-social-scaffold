d9d35a534b8a49cd7fd5d1e9d390711ee6ef50aa
SpaYco <zizou.aziz101@gmail.com>
2020-05-19 09:08:47 +0100

    removed commented method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  # def friends
-  #  friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-  #  friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-  #  friends_array.compact
-  # end
 
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
