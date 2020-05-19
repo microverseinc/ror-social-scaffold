52dad4fc2b7e567bdfc6b96cc596d1d657af6250
Theophile-Kango <fadhili.kango@gmail.com>
2020-05-19 12:27:20 +0200

    change two-way friendship to one-way friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User < ApplicationRecord
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.status }
     friends_array.compact
   end
 
