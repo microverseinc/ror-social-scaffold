bf81c661c6abff14beb7770680f54b1f0c9650ee
psialedev <psialedev@gmail.com>
2020-05-26 11:57:01 -0500

    added link to invite another user on the index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user.id
     @posts = @user.posts.ordered_by_most_recent
   end
 end
