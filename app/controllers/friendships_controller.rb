e83c09aff0f73e8458d793fbad42bf7f44bdff16
Shabab Ali <shababsaifi@gmail.com>
2020-06-02 18:29:05 +0530

    Added Friendship controller tests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,6 +3,7 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(friend_id: params[:user_id])
     @friendship.save
     redirect_to users_path
+    
   end
 
   # def new
--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -4,37 +4,36 @@ RSpec.describe FriendshipsController, type: :controller do
   include Devise::Test::ControllerHelpers
   before(:each) do
     @user = FactoryBot.create(:user)
+    @friend = FactoryBot.create(:user, name:'User2',email:'user2@example.com')
     sign_in @user
   end
 
   describe 'Freindship features ' do
-    context 'Creating a Freind with correct params' do
+    context 'Creating a Freindship with correct params' do
       it 'Creates a freind' do
-        post :create, params: { user_id: @user.id, @friend_id:  }
-        expect(response).to redirect_to(posts_path)
+        post :create, params: {user_id: @friend.id }
+        expect(response).to redirect_to(users_path)
         expect(response).to have_http_status(302)
-        expect(flash[:notice]).to match(/You liked a post./)
       end
     end
 
     context 'Accepts a Friend Request with correct params' do
       it 'Accepts a Friend Request' do
-        post :accept, params: { post_id: @post.id }
-        expect(response).to redirect_to(posts_path)
+        post :create, params: {user_id: @friend.id }
+        sign_out @user
+        sign_in @friend
+        post :accept, params: { user_id: @user.id }
+        expect(response).to redirect_to(users_path)
         expect(response).to have_http_status(302)
-        expect(flash[:notice]).to match(/You liked a post./)
       end
     end
 
-    context 'removing a freind with valid params' do
-      it 'removes a freind' do
-        post :create, params: { post_id: @post.id }
-        expect(flash[:notice]).to match(/You liked a post./)
-        delete :destroy, params: { id: Like.all[0].id, post_id: @post.id, user_id: @user.id }
-        expect(response).to redirect_to(posts_path)
-        expect(response).to have_http_status(302)
-        expect(flash[:notice]).to match(/You disliked a post./)
-      end
-    end
+    # context 'removing a freind with valid params' do
+    #   it 'removes a freind' do
+    #     delete :destroy, friendship: {friend_id: @user.id, params: { user_id: @friend.id} }
+    #     expect(response).to redirect_to(users_path)
+    #     expect(response).to have_http_status(302)
+    #   end
+    # end
   end
 end
