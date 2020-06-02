e78b53a07365527f8c4dc584273f5c09b3a00118
Manezeu Patricia <pmanezeu@gmail.com>
2020-06-02 17:46:23 +0100

    Updated the friendship migration file

--- db/migrate/20200602150334_create_friendships.rb
+++ db/migrate/20200602150334_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, foreign_key: true, index: true
-      t.references :friend, foreign_key: true, index: true
+      t.references :friend, index: true
       t.boolean :status
 
       t.timestamps
