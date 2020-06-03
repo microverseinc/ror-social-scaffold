5e7629a5ea1e13a761a48e2c4f6bac26286afed6
Manezeu Patricia <pmanezeu@gmail.com>
2020-06-03 11:04:49 +0100

    Fixed the rubocop issues

--- spec/model/friendship_spec.rb
+++ spec/model/friendship_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
   describe 'associations' do
-    it { should belong_to(:user) } 
-    it { should belong_to(:friend).class_name('User') } 
+    it { should belong_to(:user) }
+    it { should belong_to(:friend).class_name('User') }
   end
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Associations' do
-    it { should have_many(:friendships)}
-    it { should have_many(:inverse_friendships).with_foreign_key('friend_id').class_name('Friendship')}
-    it { should have_many(:posts)}
-    it { should have_many(:comments)}
-    it { should have_many(:likes)}
+    it { should have_many(:friendships) }
+    it { should have_many(:inverse_friendships).with_foreign_key('friend_id').class_name('Friendship') }
+    it { should have_many(:posts) }
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
   end
 
   describe 'Validator' do
     subject { User.create(name: 'Test user', email: 'textor@test.com', password: 'testor') }
 
-    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(20) }
   end
 end
