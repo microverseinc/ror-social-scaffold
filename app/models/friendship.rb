ff64c1d60177d91f2dace054b7b562570994eba8
Manezeu Patricia <pmanezeu@gmail.com>
2020-06-03 13:43:20 +0100

    Added the validation for uniqueness of friendship and for the non duplication in case of inverse friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,14 @@
 class Friendship < ApplicationRecord
+  validates :user_id, uniqueness: { scope: :friend_id }
+  validate :check_duplicate
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  private 
+
+  def check_duplicate
+    friendships = Friendship.all
+    errors.add(:user_id, 'Friendship between these users already exist') if friendships.any?{|f| f.user_id == friend_id && f.friend_id == user_id}
+  end
+
 end
