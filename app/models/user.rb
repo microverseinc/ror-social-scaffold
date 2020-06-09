f763118ae384da27778e808cb8969846c20cb8af
FrancoRosa <km115.franco@gmail.com>
2020-06-09 17:30:28 -0500

    Add friends and confirm friend methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,27 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
-  has_many :inverse_friendshiṕ, class_name: "Friendship", foreign_key: :friend_id
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
+
+  def friends
+    friends = self.friendships.collect{ |x| x.friend_id if x.confirmed }
+    friends += self.inverse_friendships.collect{ |x| x.user_id if x.confirmed }
+    friends.compact
+  end
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+  end
+
+  def confirm_friend(friend)
+    self.friendships.find_by(friend_id: friend[:id] ).update(confirmed: true)
+  end
+
+  def friend?(user)
+  end
+
 end
