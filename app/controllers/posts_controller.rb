f069a9c71ed0710974fe7dffe12f4c40351336ed
rsnazario <rafael.s.nazario@gmail.com>
2020-06-10 17:57:56 -0300

    Posts to each user - first version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,20 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    all_my_friends = []
+    
+    current_user.friendships.each do |c|
+      all_my_friends << c.friend_id
+    end
+    
+    Friendship.all.each do |c|
+      
+      if c.friend_id == current_user.id
+        all_my_friends << c.user_id
+      end
+
+    end
+    @timeline_posts ||= Post.where(user_id: all_my_friends).ordered_by_most_recent
   end
 
   def post_params
