c365aca06aa7d512143a627b5128aebaf35803a0
SevlaMare <sevilamare@gmail.com>
2020-06-11 16:24:51 -0300

    unit test for user model

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 # rspec ./spec/model/user_spec.rb
 RSpec.describe User, type: :model do
   let(:user) { User.new}
-  let(:user1) { User.create(
-    name: 'test', email: 'email@email.com', password: 'abc123'
-  )}
+  let(:user1) { User.create(name: 'test', email: 'email@email.com', password: 'abc123') }
+  let(:post1) { user1.posts.create(content: 'abc') }
 
   context 'Model' do
     it 'should exist' do
@@ -13,16 +12,25 @@ RSpec.describe User, type: :model do
     end
   end
 
-  context 'Associations' do 
-    it 'user can create a post' do
+  context 'Autofill id using association' do 
+    it 'create a post' do
       user1.posts.create(content: 'abc')
       expect(user1.posts.first).not_to be_nil
     end
   end
 
-  # TODO: can create a post
-
-  # TODO: can create a comment
+  context 'Autofill id using association' do 
+    it 'create a comment' do
+      post1.comments.create(user_id: 1, content: 'abc')
+      expect(post1.comments).not_to be_nil
+    end
+  end
 
   # TODO: can leave a like
+  context 'etc' do
+    it 'leave a like' do
+      user1.likes.create(post_id: 1)
+      expect(post1.likes).not_to be_nil
+    end
+  end
 end
