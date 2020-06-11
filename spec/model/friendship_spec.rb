9e9637e998b01075f9636e8cd54eba647b2140a4
SevlaMare <sevilamare@gmail.com>
2020-06-11 16:55:14 -0300

    unit test to friendship

--- spec/model/friendship_spec.rb
+++ spec/model/friendship_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  # TODO: retrieve all user friends
-  # TODO: retrieve received invitations
+  let(:user1) { User.create(name: 'test', email: 'email@email.com', password: 'abc123') }
+  let(:friend1) { user1.frienships.create(friend_id: 2) }
+
+  context 'check associations' do
+    it 'retrieve all user friends' do
+      user1
+      friend1
+      expect(Friendship.first.user_id).not_to be_nil
+    end
+  end
+
+  # context 'check associations' do
+  #   it 'retrieve all user friends' do
+  #   end
+  # end
+
+  # context 'check associations' do
+  #   it 'retrieve received invitations' do
+  #   end
+  # end
 end
