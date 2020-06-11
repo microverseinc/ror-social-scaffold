9a50e36ded2934c6dc4e4140a347a582a917c410
SevlaMare <sevilamare@gmail.com>
2020-06-11 17:00:26 -0300

    linter fix2

--- spec/views/user_spec.rb
+++ spec/views/user_spec.rb
@@ -1,60 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Main Flow', type: :system do
-  describe 'User Actions of Platform' do
-    it 'Demonstrate Actions: Create Post | Add Friend | Accept Friend' do
-      visit('/')
-      sleep 1
+  describe 'Integration test' do
+    context 'main users flow' do
+      it 'can create a user, login, add others, others can accept' do
+        visit('/')
 
-      # Register
-      page.find_by_id('sign-up-link').click
-      sleep 1
+        # Register
+        page.find_by_id('sign-up-link').click
 
-      # Fill the fields
-      fill_in('user[name]', with: 'Crunchy Bacon')
-      fill_in('user[email]', with: 'crunchy@bacon.com')
-      fill_in('user[password]', with: '123123')
-      fill_in('user[password_confirmation]', with: '123123')
-      sleep 1
-      click_button 'Sign up'
-      sleep 1
+        # Fill the fields
+        fill_in('user[name]', with: 'Crunchy Bacon')
+        fill_in('user[email]', with: 'crunchy@bacon.com')
+        fill_in('user[password]', with: '123123')
+        fill_in('user[password_confirmation]', with: '123123')
+        click_button 'Sign up'
 
-      fill_in('post[content]', with: 'This test worked')
-      click_button 'Save'
-      sleep 1
-      click_link 'Sign out'
+        fill_in('post[content]', with: 'This test worked')
+        click_button 'Save'
+        click_link 'Sign out'
 
-      visit('/')
-      sleep 1
+        visit('/')
 
-      # Register
-      page.find_by_id('sign-up-link').click
-      sleep 1
+        # Register
+        page.find_by_id('sign-up-link').click
 
-      # Fill the fields
-      fill_in('user[name]', with: 'Salty Bacon')
-      fill_in('user[email]', with: 'salty@bacon.com')
-      fill_in('user[password]', with: '123123')
-      fill_in('user[password_confirmation]', with: '123123')
-      sleep 1
-      click_button 'Sign up'
+        # Fill the fields
+        fill_in('user[name]', with: 'Salty Bacon')
+        fill_in('user[email]', with: 'salty@bacon.com')
+        fill_in('user[password]', with: '123123')
+        fill_in('user[password_confirmation]', with: '123123')
+        click_button 'Sign up'
 
-      sleep 1
-      click_link 'All users'
-      sleep 1
-      click_button 'Add Friend'
-      sleep 1
-      click_link 'Sign out'
+        click_link 'All users'
+        click_button 'Add Friend'
+        click_link 'Sign out'
 
-      sleep 1
-      fill_in('user[email]', with: 'crunchy@bacon.com')
-      fill_in('user[password]', with: '123123')
-      click_button 'Log in'
-      sleep 1
-      click_link 'Friends'
-      sleep 1
-      click_button 'Confirm'
-      sleep 3
+        fill_in('user[email]', with: 'crunchy@bacon.com')
+        fill_in('user[password]', with: '123123')
+        click_button 'Log in'
+        click_link 'Friends'
+        click_button 'Confirm'
+      end
     end
   end
 end
