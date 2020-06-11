d94fcd57aad14bfb4ceaf181be76f1a90d3bc655
rsnazario <rafael.s.nazario@gmail.com>
2020-06-11 13:41:52 -0300

    First Actions for Capybara Tests

--- spec/views/user_spec.rb
+++ spec/views/user_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe 'Main Flow', type: :system do
       sleep 1
 
       # Register
-      click_link find_by_id('sign_up-link')
-      sleep 5
+      page.find_by_id('sign-up-link').click
+      sleep 2
 
-
+      # Fill the fields
+      fill_in('user[name]', with:'Crunchy Bacon')
+      fill_in('user[email]', with:'crunchy@bacon.com')
+      fill_in('user[password]', with:'123123')
+      fill_in('user[password_confirmation]', with:'123123')
+      sleep 3
+      click_button 'Sign up'
+      sleep 3
     end
   end
 end
