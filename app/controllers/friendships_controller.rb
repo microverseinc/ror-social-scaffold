7f22c17b1f7695804c9d8d97f67dbd4e9a27d01d
Samkiroko <kirokodev@gmail.com>
2020-06-15 01:43:47 +0300

    Updating order by most recent

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,40 @@
+class FriendshipsController < ApplicationController
+  def send_invitation
+    if current_user.send_invitation(params[:user_id])
+      flash.notice = 'Friend invitation sent'
+      redirect_to users_path
+    else
+      flash.now[:notice] = 'error occured'
+    end
+  end
+
+  def accept_invitation
+    if current_user.confirm_invites(params[:user_id])
+      flash.notice = 'friend accepted'
+      redirect_to users_path
+    else
+      flash.now[:notice] = 'error occured'
+    end
+  end
+
+  def reject_invitation
+    current_user.reject_invites(params[:user_id])
+    redirect_to users_path
+  end
+
+  def pending_invitation
+    @pending_invitations = current_user.pending_invites
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    friend = current_user.friendships.find_by_friend_id(user)
+
+    if friend.delete
+      flash.notice = "#{user.name} has been removed as your friend"
+      redirect_to users_path
+    else
+      flash.now[:notice] = 'error occurred'
+    end
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    @timeline_posts = Post.where(user: current_user.friends).or(Post.where(user: current_user)).ordered_by_most_recent
   end
 
   def post_params
