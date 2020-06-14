91bd5e7ae0b2bc45225496f65f9a0021c590ad34
Samkiroko <kirokodev@gmail.com>
2020-06-15 01:28:47 +0300

    Add migration, route model for the friendship features

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,32 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+   has_many :friendships
+  has_many :inverse_frienships, class_name: 'Friendship', foreign_key: :friend_id
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.status}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
+    friends_array.compact
+  end
+
+  # Users who have yet to confirmee friend invites
+  def pending_invites
+    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_invites
+    inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
+  end
+
+  def confirm_invites(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
