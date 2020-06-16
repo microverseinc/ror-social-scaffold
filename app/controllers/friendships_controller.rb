23445198be788227b7c551e4f78e589130c5ed54
shubhsk88 <shubham88singh@gmail.com>
2020-06-17 03:47:49 +0530

    Adding the friend request model and updating it in user view

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,4 +4,12 @@ class FriendshipsController < ApplicationController
         @request=Friendship.where(user_id:current_user.id,status:true)
     end
 
+    def create
+        @user=User.find(params[:user_id])
+        current_user.friendships.create(friend:@user)
+        redirect_to request.referrer,notice:"Friend request succesffuly sent"
+    end
+
+
+
 end
