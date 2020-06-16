71b3effca328640780f2dfa9bf75393193c39325
shubhsk88 <shubham88singh@gmail.com>
2020-06-17 01:16:04 +0530

    Adding the migration and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
+  has_many :friendships,dependent: :destroy 
 
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def friend
+    friendships.map{|friendship| friendship.friend if(friendship.status)}.compact
+  end
+
+  def friends(user)
+    friends.include?(user)
+  end
 end
