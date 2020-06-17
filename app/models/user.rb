4523841483b73d6ad1147ea13d553c520eca07de
SevlaMare <sevilamare@gmail.com>
2020-06-17 17:28:07 -0300

    linter fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
 
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
   has_many :friendships
 
   # FRIENDS
