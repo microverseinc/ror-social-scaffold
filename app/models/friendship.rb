581d619644c7bdb97e1800219b1088aeffab502a
SevlaMare <sevilamare@gmail.com>
2020-06-17 14:30:58 -0300

    fix inverse relationship, tips by mosesogwo

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,9 +6,12 @@ class Friendship < ApplicationRecord
   scope :pendant_add, -> { where('acceptance =?', false) }
   scope :my_friends, -> (id) { where('friend_id =?', id) }
 
-  before_update :reverse_friend
+  after_update :reverse_friend
 
   def reverse_friend
-    Friendship.create(user_id: friend.id, friend_id: user.id, acceptance: true)
+    Friendship.create(
+      user_id: self.friend.id, 
+      friend_id: self.user.id, 
+      acceptance: true)
   end
 end
