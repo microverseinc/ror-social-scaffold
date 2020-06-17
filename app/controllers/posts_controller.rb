b43d37770b6011962153753f83bd35d883277603
rsnazario <rafael.s.nazario@gmail.com>
2020-06-17 18:40:04 -0300

    refactored timeline_posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   def timeline_posts
     @timeline_posts ||= Post.where( user: (current_user.friends))
                       .or(Post.where( user: (current_user))
-    )
+    ).ordered_by_most_recent
   end
 
   def post_params
