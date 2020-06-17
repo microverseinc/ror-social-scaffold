e9cb1085c5b01b9277d6f2d79b46a9524d1cbca4
SevlaMare <sevilamare@gmail.com>
2020-06-17 17:51:18 -0300

    display friend name on friend list

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,8 @@ class Post < ApplicationRecord
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def friends_plus_own_posts
+    Post.where(user: (self.friends + self))
+  end
 end
