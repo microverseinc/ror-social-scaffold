361c986a7b547916a9f6c3ab11b88e8a73f17063
shubhsk88 <shubham88singh@gmail.com>
2020-06-18 21:02:25 +0530

    adding the feature that only the user can see the post of their friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @posts = @user.friendships.where(status:true).posts.ordered_by_most_recent
+    @posts = @user.posts.ordered_by_most_recent
   end
 end
