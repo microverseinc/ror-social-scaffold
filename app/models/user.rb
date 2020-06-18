6e492c6e2d7b8b5cfea79cf8ff3535be4628f5e8
Rossiel Carranza <rossielcarranza@gmail.com>
2020-06-18 20:29:14 -0300

    Add friendship model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :received_friendships, foreign_key: 'friend_id', class_name: 'Friendship'
 end
