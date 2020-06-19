00d71560f41662530f45ec26a96299742b01c0c4
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-06-19 15:02:48 -0500

    added symetric friendships with tests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,7 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
   after_update :friendship_symmetry,
-               if: [proc { |c| c.friend.friendships.find_by(friend_id: c.user.id) }]
+               unless: [proc { |c| c.friend.friendships.find_by(friend_id: c.user.id) }]
 
   validate :cannot_friend_self
 
@@ -10,9 +10,7 @@ class Friendship < ApplicationRecord
     errors.add(:friend_id, 'cannot friend self') if user_id == friend_id
   end
 
-  def friendship_symmetry(_friendship_relation)
-    @friend = friendship_relation.friend
-    @user = friendship_relation.user
-    @friend.friendships.create(friend_id: @user.id)
+  def friendship_symmetry
+    friend.friendships.create(friend_id: user.id, status: true)
   end
 end
