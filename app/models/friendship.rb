ee37d6f1c5e05a3b0253dacbc80a8973ec732b68
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-06-19 16:01:35 -0500

    Revert "added symetric friendships with tests"
    This reverts commit 00d71560f41662530f45ec26a96299742b01c0c4.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,7 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
   after_update :friendship_symmetry,
-               unless: [proc { |c| c.friend.friendships.find_by(friend_id: c.user.id) }]
+               if: [proc { |c| c.friend.friendships.find_by(friend_id: c.user.id) }]
 
   validate :cannot_friend_self
 
@@ -10,7 +10,9 @@ class Friendship < ApplicationRecord
     errors.add(:friend_id, 'cannot friend self') if user_id == friend_id
   end
 
-  def friendship_symmetry
-    friend.friendships.create(friend_id: user.id, status: true)
+  def friendship_symmetry(_friendship_relation)
+    @friend = friendship_relation.friend
+    @user = friendship_relation.user
+    @friend.friendships.create(friend_id: @user.id)
   end
 end
