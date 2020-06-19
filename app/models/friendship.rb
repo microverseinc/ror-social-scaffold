458a76a386a27e56b303fdb50d7811d738f9f634
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-06-19 16:13:23 -0500

    revert commit

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,18 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
-  after_update :friendship_symmetry,
-               if: [proc { |c| c.friend.friendships.find_by(friend_id: c.user.id) }]
 
   validate :cannot_friend_self
 
   def cannot_friend_self
     errors.add(:friend_id, 'cannot friend self') if user_id == friend_id
-  end
-
-  def friendship_symmetry(_friendship_relation)
-    @friend = friendship_relation.friend
-    @user = friendship_relation.user
-    @friend.friendships.create(friend_id: @user.id)
   end
 end
