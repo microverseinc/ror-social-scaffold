da49c420c8f31d69bf45f81b55b951ac4a80e231
Rossiel Carranza <rossielcarranza@gmail.com>
2020-06-21 03:33:35 -0300

    Add count of comments and likes by post
    - Add gravatar as user image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,10 @@ module UsersHelper
     users
   end
   # rubocop:enable Style/InverseMethods
+
+  def gravatar_for(user, size: 60)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
 end
