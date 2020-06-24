c25944c1a0c3df4755b2eec3832b614ab89ef954
Bekhzod96 <mr.begzodjon96@mail.ru>
2020-06-25 01:32:32 +0300

    Test section

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,11 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-#   parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+
 
   # Add more helper methods to be used by all tests here...
 
-  def sign_in_as(user) 
-    post login_path, params: {session: { username: user.username } }
-  end
 end
