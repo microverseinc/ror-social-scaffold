ab13a1a22b9b132bae6b8f7862b6b9198dd06162
Mauricio <mauriciofow@gmail.com>
2020-06-26 14:51:57 -0500

    User methods for friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,38 @@
 class User < ApplicationRecord
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array += inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
-
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
