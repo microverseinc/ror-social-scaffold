923adbfdf11ff39de468c991d3699da13bf97db7
Javier Botero <javierbotero10271@gmail.com>
2020-06-26 17:22:21 -0500

    Fix some stickler issues

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,10 +5,7 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   # parallelize(workers: :number_of_processors)
-
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-
-
   # Add more helper methods to be used by all tests here...
-
 end
+
