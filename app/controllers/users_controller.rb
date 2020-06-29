f90ad2ec7fdc781b94998f97177f377823fd0614
Precious chiedu <udegbue69@gmail.com>
2020-06-29 17:40:09 +0100

    added user display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.all.where("id != ?", current_user.id)
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
