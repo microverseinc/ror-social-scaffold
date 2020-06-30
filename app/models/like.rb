44e00fae4c86d88497b4ceb5862e985e236cd892
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-06-30 14:41:30 +0530

    adding test for friendship and like models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,9 @@
 class Like < ApplicationRecord
   validates :user_id, uniqueness: { scope: :post_id }
 
+ 	# validates :user_id, presence: true
+  # validates :post_id, presence: true
+
   belongs_to :user
   belongs_to :post
 end
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'validations' do
+    it 'should validate presence of content' do
+      User.create!(email: 'abc@example.com', password: '123456', name: 'Example')
+      Post.create!(user_id: 2, content: 'ascascacascsacsac')
+      like = Like.create!(user_id: 2, post_id: 2)
+      expect(like.valid?).to be true
+    end
+
+    it { should validate_uniqueness_of(:user_id).scoped_to(:post_id) }
+
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+end
