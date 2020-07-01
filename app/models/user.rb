0178f1abe0a04ffac62a8a1ff05c313100ae6767
Pedro Donda <phfdonda@gmail.com>
2020-07-01 19:32:47 -0300

    add friends method to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
-
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def friends
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    friends_array + inverse_friendships.map { |friendship| friendship.friendship.user if friendship.confirmed }
+    friends_array.compact
+  end
+
 end
