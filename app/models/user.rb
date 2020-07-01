792ef77e5338b8e5812bdf74b6a397da7e963031
Precious chiedu <udegbue69@gmail.com>
2020-07-01 16:57:31 +0100

    fixed errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,13 +27,13 @@ class User < ApplicationRecord
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship = inverse_friendships.find { |friendship| friendship.user == user } # rubocop:disable Metrics/ShadowingOuterLocalVariable
     friendship.confirmed = true
     friendship.save
   end
 
   def decline_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship = inverse_friendships.find { |friendship| friendship.user == user } # rubocop:disable Metrics/ShadowingOuterLocalVariable
     friendship.confirmed
     friendship.destroy
   end
