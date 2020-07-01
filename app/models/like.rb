5f385f71dd0eaf9d8795888e90b1c81f755801d0
Azeem <azeemahmed101@gmail.com>
2020-07-01 17:33:44 +0200

    add like and comment counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,6 @@
 class Like < ApplicationRecord
   validates :user_id, uniqueness: { scope: :post_id }
 
- 	# validates :user_id, presence: true
-  # validates :post_id, presence: true
-
   belongs_to :user
   belongs_to :post
 end
