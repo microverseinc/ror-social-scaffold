3180fb5e7b494bd18234fe3c36a2e66aff3b365c
Osong Agberndifor <agberndifor7@gmail.com>
2020-07-02 11:01:36 +0100

    Add new Friendship association with status value = integer

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,41 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+
+
+
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.status == 1} 
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status == 1}
+    friends_array.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.friend if friendship.status == -1}.compact 
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if friendship.status == -1}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = 1
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
+  def reject_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = 0
+    friendship.save
+  end
 end
