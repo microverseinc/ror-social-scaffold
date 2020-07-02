dd19a5cfb20b4caf8a49cd1cfd95b0e331d44c12
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-02 19:24:36 +0530

    added controller test for users and posts

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  before(:each) do
+    @user = User.create(name: "Test", email: "test@xyz.com",
+      password: "123456")
+    sign_in @user
+    # request.env['HTTP_REFERER'] = '/posts'
+  end
+
+  describe '#create post' do
+    context 'with valid params' do
+      it 'creates a new post' do
+        post :create, params: { post: { content: "gdscscdscsc" }  }
+        expect(response).to have_http_status(302) 
+        expect(response).to redirect_to(posts_path)       
+        expect(flash[:notice]).to match(/Post was successfully created./)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'gives an error' do
+        post :create, params: { post: { content: 1 }  }
+        expect(response).to have_http_status(302) 
+      end
+    end
+  end
+end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  before(:each) do
+    @user = User.create(name: "Test", email: "test@xyz.com",
+      password: "123456")
+    sign_in @user
+  end
+
+  describe 'GET #index' do
+    it 'gets all the users and render index page' do
+      get :index
+      expect(response).to be_successful
+      expect(response).to render_template(:index)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'Renders user show page' do
+      get :show, params: { id: @user.id }
+      expect(response).to be_successful
+      expect(response).to render_template(:show)
+      expect(response).to have_http_status(200)
+    end
+
+    context 'If user does not exis' do
+      it 'is not a success' do
+        begin
+          get :show, params: { id: -1 }
+        rescue StandardError
+          ActiveRecord::RecordNotFound
+        end
+        expect(response).not_to render_template(:show)
+      end
+    end
+  end
+
+
+  describe 'GET #pending_friends' do
+    it 'gets all the users pending friends' do
+      get(:pending_friends, params: { user_id: @user.id })
+      expect(response).to be_successful
+      expect(response).to render_template(:pending_friends)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET #friend_requests' do
+    it 'gets all the users friend requests' do
+      get(:friend_requests, params: { user_id: @user.id })
+      expect(response).to be_successful
+      expect(response).to render_template(:friend_requests)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
