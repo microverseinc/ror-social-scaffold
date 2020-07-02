904200a59b5fa260c6bf4013fef8364e227eb0c8
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-02 21:01:24 +0530

    added test friendship feature

---
+++ spec/features/friendships_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+feature 'create a friendship' do
+  before(:each) do
+    @user1 = User.create!(name: "Test", 
+                            email: 'test@example.com', 
+                            password: 'cake,biscuitsandtea'
+                            )
+    @user2 = User.create!(name: "Test2", 
+                            email: 'test2@example.com', 
+                            password: 'cake,biscuitsandtea'
+                            )
+    sign_in @user1  
+    visit users_path
+  end
+
+  scenario 'create friendship between two users' do
+    expect(page).to have_content 'Action'
+  end
+
+  feature 'send a request' do
+    before(:each) do  
+      first(:link, 'Send Request').click    
+    end
+
+    scenario 'renders user page' do
+      expect(page).to have_content 'Request pending'
+    end
+  end
+
+  feature 'accept a request' do
+    before(:each) do
+     visit users_path
+
+     first(:link, 'Send Request').click    
+       sign_out @user1
+       sign_in @user2
+       visit users_path
+
+    end
+
+    scenario 'renders friend requests page' do
+      expect(page).to have_content "#{@user1.name}"
+    end
+
+
+    scenario 'renders Accept friend Request page' do
+      visit users_path
+      first(:link, 'Accept').click 
+      expect(page).to have_content "You have accepted #{@user1.name} request"
+    end
+
+
+    scenario 'renders unfriending a friend Request' do
+      visit users_path
+      first(:link, 'Reject').click 
+      expect(page).to have_content "You have unfriended #{@user1.name}"
+    end
+  end
+end
