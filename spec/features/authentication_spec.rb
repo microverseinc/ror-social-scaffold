98c2824399885eeedaf59e9fd51ed2c88e3e5794
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-02 20:02:08 +0530

    added test feature for comment

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -21,3 +21,27 @@ feature 'sign up process' do
     end
   end
 end
+
+feature 'sign in process' do
+  scenario 'has a new user session' do
+    visit new_user_session_path
+    expect(page).to have_content 'Sign in'
+  end
+
+  feature 'signing in a user' do
+      before(:each) do
+        @user = User.create!(name: "Test", 
+                              email: 'test@example.com', 
+                              password: 'cake,biscuitsandtea'
+                              )
+      visit new_user_session_path
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'Password', with: 'cake,biscuitsandtea'
+      click_on 'Log in'
+    end
+
+    scenario 'redirects to root path after signup' do
+      expect(page).to have_content 'Sign out'
+    end
+  end
+end
---
+++ spec/features/comments_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'create a comment' do
+  before(:each) do
+      @user = User.create!(name: "Test", 
+                              email: 'test@example.com', 
+                              password: 'cake,biscuitsandtea'
+                              )
+      sign_in @user
+      @posts = Post.create!(user_id: @user.id, content: "Post Content here")    
+      visit posts_path     
+  end
+
+  scenario 'has a create comment section' do
+    visit posts_path
+    expect(page).to have_content 'Recent posts'
+  end
+
+  feature 'create a new comment' do
+    before(:each) do  
+      fill_in 'comment_content', with: 'Comment content here'
+      click_on 'Comment'      
+    end
+
+    scenario 'renders comment section on the post' do
+      expect(page).to have_content 'Comment content here'
+    end
+  end
+
+  feature 'create a new post with invalid params' do
+    before(:each) do  
+      fill_in 'comment_content', with: ''      
+      click_on 'Save'      
+    end
+
+    scenario 'renders comments could not be saved error' do
+      expect(page).to have_content "Content can't be blank"
+    end
+  end
+end
