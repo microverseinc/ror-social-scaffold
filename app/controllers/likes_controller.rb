bc1b1694535f4ab31cf9e34c1b0d852780ebe2f4
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-07-02 16:33:57 +0530

    added integration test for Likes action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
     if @like.save
       redirect_to request.referrer, notice: 'You liked a post.'
     else
-      redirect_to posts_path, alert: 'You cannot like this post.'
+      redirect_to request.referrer, alert: 'You cannot like this post.'
     end
   end
 
@@ -15,7 +15,7 @@ class LikesController < ApplicationController
       like.destroy
       redirect_to request.referrer, notice: 'You disliked a post.'
     else
-      redirect_to posts_path, alert: 'You cannot dislike post that you did not like before.'
+      redirect_to request.referrer, alert: 'You cannot dislike post that you did not like before.'
     end
   end
 end
