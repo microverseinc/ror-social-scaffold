18b74567bb7ac8c8ed26ffc612b4b60ddf381cc2
Pedro Donda <phfdonda@gmail.com>
2020-07-03 17:52:09 -0300

    update doc img of ERB

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'User' do
   let(:frienship_send) { Friendship.create(user_id: 1, friend_id: 2, confirmed: false) }
 
   context 'doesn\'t exist' do
-    let(:user_params) { { name: 'Tester', email: 'test@testing.com', password: '123456'} }
+    let(:user_params) { { name: 'Tester', email: 'test@testing.com', password: '123456' } }
     let(:long_name) { 'a' * 25 }
 
     it 'can create a new user' do
@@ -28,35 +28,35 @@ RSpec.describe 'User' do
     end
   end
 
-  # context 'user already exist' do
-  #   user1 = User.create(user_params)
-  #   user2 = User.create()
-  #   user3 = User.create()
-  #   user4 = User.create()
-  #
-  #   it 'can send friendship request' do
-  #     expect
-  #   end
-  #
-  #   it 'can check pending friends' do
-  #     expect
-  #   end
-  #
-  #   it 'can check friend requests' do
-  #     expect
-  #   end
-  #
-  #   it 'can check friends' do
-  #     expect
-  #   end
-  #
-  #   it 'can check if a user is a friend' do
-  #     expect
-  #   end
-  #
-  #   it 'can confirm friends' do
-  #     expect
-  #   end
+  context 'user already exist' do
+    let(:user_params) { { name: 'Tester', email: 'test@testing.com', password: '123456' } }
+    user1 = User.create(user_params)
+    user2 = User.create
+    user3 = User.create
+    user4 = User.create
 
-  # end
+    it 'can send friendship request' do
+      expect
+    end
+
+    it 'can check pending friends' do
+      expect
+    end
+
+    it 'can check friend requests' do
+      expect
+    end
+
+    it 'can check friends' do
+      expect
+    end
+
+    it 'can check if a user is a friend' do
+      expect
+    end
+
+    it 'can confirm friends' do
+      expect
+    end
+  end
 end
