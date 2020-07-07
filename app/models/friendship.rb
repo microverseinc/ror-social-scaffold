7882adb64a4095dfa23af72e68bd81f6736be59e
jubaan <julio.ab@outlook.com>
2020-07-07 18:04:46 +0000

    Create self referential many-to-many
    Use callbacks in friendship model to reciprocate and destroy 'friendships'

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,15 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend
+  belongs_to :friend, class_name: 'User'
+
+  after_create :reciprocate_friendship
+  after_destroy :destroy_friendship
+
+  def reciprocate_friendship
+    friend.friends << user unless friend.friends.include?(user)
+  end
+
+  def destroy_friendship
+    friend.friends.delete(user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
 
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :friends, through: :friendships, source: :friend
 end
