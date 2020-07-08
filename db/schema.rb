56359b279032c577ce727c3f5bb9bcefc863d55f
jubaan <julio.ab@outlook.com>
2020-07-08 21:57:04 +0000

    Generate seed file to populate database

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_223937) do
   create_table "friendships", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "friend_id"
-    t.boolean "confirmed"
+    t.boolean "confirmed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create({name: "julio", email: "julio@julio.com", password: 123456, password_confirmation: 123456})
+
+User.create({name: "felipe", email: "felipe@felipe.com", password: 123456, password_confirmation: 123456})
+
+10.times do
+  User.create(
+    {
+      name: Faker::Name.unique.name,
+      email: Faker::Internet.unique.email,
+      password: '123456',
+      password_confirmation: '123456'
+    }
+  )
+end
+
+User.all.each do |user|
+  friendable = User.all.ids
+
+  3.times do
+    friend_id = friendable.delete(rand(0..User.all.length))
+
+    Friendship.create(
+      { user_id: user.id, friend_id: friend_id }
+    )
+  end
+end
