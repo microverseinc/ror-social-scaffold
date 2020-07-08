f0ee25ff1f93b4499777645b97dae31312953eb9
Pedro Donda <phfdonda@gmail.com>
2020-07-08 15:00:59 -0300

    add test for login user

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Users', type: :feature do
+
+  context 'Users exist'do
+  let(:user){ create(:random_user) }
+  let(:friend){ create(:random_friend) }
+  let(:frienship_not_accepted) { create(:unconfirmed_friendship) }
+
+    it 'successfully login the user' do
+      login_user(user)
+    end
+
+    it 'can\'t login with a blank password' do
+  end
+
+  end
+end
