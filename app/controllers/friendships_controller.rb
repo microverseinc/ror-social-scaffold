4611f210fc96422254d3746e0fd3b5a9b822be78
Maclenn77 <maclenn77@gmail.com>
2020-07-09 16:21:50 -0500

    Add specs for friendship. Create controller for friendship

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,33 @@
+class FriendshipsControllers < ApplicationController
+  def create
+    user = User.find_by(id: current_user)
+    friend = User.find(params[:id])
+    @friendship = Friendship.new(user_id: user.id, friend_id: friend.id, confirmed: false)
+    @inverse_friendship = Friendship.new(user_id: friend.id, friend_id: user.id, confirmed: false)
+    @inverse_friendship.save if @friendship.save
+    redirect_to users_path
+  end
+
+  def update
+    user = User.find_by(id: current_user)
+    friend = User.find(params[:id])
+    @friendship = Friendship.find_by(user_id: friend, friend_id: user)
+    @inverse_friendship = Friendship.find_by(user_id: user, friend_id: friend)
+    @friendship.confirm = true
+    @inverse_friendship.confirm = true
+    @inverse_friendship.save if @friendship.save
+  end
+
+  def destroy
+    user = User.find_by(id: current_user)
+    friend = User.find(params[:id])
+    @friendship = Friendship.find_by(user_id: friend, friend_id: user)
+    @inverse_friendship = Friendship.find_by(user_id: user, friend_id: friend)
+    @inverse_friendship.delete if @friendship.delete
+  end
+end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id, confirmed:)
---
+++ spec/features/friendships_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature 'Friendship' do
+  let!(:user) { create(:random_user) }
+  let!(:friend) { create(:random_user) }
+
+  context 'User add a friend' do
+    it 'friend request is created' do
+      Friendship.create
+      expect(Friendship.last).to exist
+    end
+
+    it 'two objects are created when you send a friend request' do
+      Friendship.create
+      friendship = Friendship.last
+      friendship_two = Friendship.first
+      expect(friendship.id).to eql(friendship_two.id + 1)
+    end
+
+    it 'friend request is accepted' do
+      Friendship.update
+      expect(Frienship.confirmed).to eql(true)
+    end
+
+    it 'friend request is rejected' do
+      expect(Friendship.confirmed).to eql(false)
+    end
+
+    it 'two objects are update when you accept a friend request' do
+      Friendship.update
+      friendship = user.friendships.where(friend_id: friend.id)
+      inverse_friendship = friend.friendships.where(friend_id: user.id)
+      expect(friendship.confirmed).to eql(inverse_friendship.confirmed)
+    end
+
+  end
+end
