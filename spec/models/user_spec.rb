c48f521e36cec05989cd894a2a82ca42b4b3f16c
Hemant soni <sonihemant4741@gmail.com>
2020-07-10 21:01:44 +0530

    testing for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../rails_helper'
 
+# rubocop:disable Metrics/BlockLength
+
 RSpec.describe User, type: :model do
   context 'creating user' do
     it 'name cannot be blank ' do
@@ -41,4 +43,53 @@ RSpec.describe User, type: :model do
       expect(u.macro).to eq(:has_many)
     end
   end
+
+  context 'user methods' do
+    let(:frodo) { User.create(name: 'Frodo', email: 'frodo@email.com', password: '123456') }
+    let(:bilbo) { User.create(name: 'Bilbo', email: 'bilbo@email.com', password: '123456') }
+    let(:gandalf) { User.create(name: 'Gandalf', email: 'gandalf@email.com', password: '123456') }
+    let(:friendship_1) { Friendship.create(user_id: frodo.id, friend_id: bilbo.id, status: true) }
+    let(:friendship_2) { Friendship.create(user_id: frodo.id, friend_id: gandalf.id, status: false) }
+
+    it 'returns an array of user who are already friends' do
+      frodo
+      bilbo
+      friendship_1
+      expect(frodo.friends).to include(bilbo)
+    end
+
+    it 'returns an array of user who are friends to be accepted or rejected' do
+      frodo
+      gandalf
+      friendship_2
+      expect(frodo.pending_friends).to include(gandalf)
+    end
+
+    it 'returns an array of users who have requested to be friends' do
+      frodo
+      gandalf
+      friendship_2
+      expect(gandalf.friend_requests).to include(frodo)
+    end
+
+    it 'confirms user as a friend' do
+      frodo
+      gandalf
+      bilbo
+      friendship_2
+      gandalf.confirm_friend(frodo)
+
+      expect(gandalf.confirm_friend(frodo)).to eql(true)
+    end
+
+    it 'checks if user is a friend' do
+      frodo
+      gandalf
+      bilbo
+      friendship_1
+
+      expect(frodo.friend?(bilbo)).to eql(true)
+    end
+  end
+  # rubocop:enable Metrics/BlockLength
 end
