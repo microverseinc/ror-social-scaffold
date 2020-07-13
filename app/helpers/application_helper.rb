385ae8d8de06839a6a68e139d96360660e0310bd
taiwocoker <taiwofcoker@gmail.com>
2020-07-13 14:03:23 +0100

    fix linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,5 +22,4 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
-
 end
