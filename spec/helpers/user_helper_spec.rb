1faefe78648004062c2e32465061f7f3a12f5615
fc-anjos <felipe.anjos@usp.br>
2020-07-14 19:31:14 -0300

    write tests for helpers

--- spec/helpers/user_helper_spec.rb
+++ spec/helpers/user_helper_spec.rb
@@ -11,5 +11,58 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UserHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:current_user) { create :user }
+  let(:other_user) { create :user }
+  let!(:friendship) { create :friendship, requester: current_user, receiver: other_user }
+
+  context '#find_friendships' do
+    it 'Finds the correct friendship passing the receiver' do
+      found_friendship = find_friendship(other_user)
+      expect(found_friendship).to eql(friendship)
+    end
+  end
+
+  context '#friendship_interaction' do
+    it 'returns nil if the user is the current_user' do
+      expect(friendship_interaction(current_user)).to eql(nil)
+    end
+  end
+
+  context 'Current user has received an invitation' do
+    let!(:friendship) { create :friendship, requester: other_user, receiver: current_user }
+    it 'Returns drop invitation message' do
+      interaction = friendship_interaction(other_user)
+      expect(interaction).to include('Accept Friendship')
+      expect(interaction).to include('patch')
+      expect(interaction).to include('Reject Friendship')
+      expect(interaction).to include('delete')
+    end
+  end
+
+  context 'Current user has sent an invitation' do
+    let!(:friendship) { create :friendship, requester: current_user, receiver: other_user }
+    it 'Returns drop invitation message' do
+      interaction = friendship_interaction(other_user)
+      expect(interaction).to include('Drop invitation')
+      expect(interaction).to include('delete')
+    end
+  end
+
+  context 'Current user and receiver are friends' do
+    let!(:friendship) { create :friendship, requester: current_user, receiver: other_user, confirmed: true }
+    it 'Returns unfriend message' do
+      interaction = friendship_interaction(other_user)
+      expect(interaction).to include('Unfriend')
+      expect(interaction).to include('delete')
+    end
+  end
+
+  context 'Current user and receiver are not related' do
+    it 'Returns unfriend message' do
+      friendship.destroy
+      interaction = friendship_interaction(other_user)
+      expect(interaction).to include('Invite friend')
+      expect(interaction).to include('post')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:current_user) { create :user }
-  let(:user) { create :user }
+  let(:requester) { create :user }
+  let(:receiver) { create :user }
+  let!(:friendship) { create :friendship, requester: requester, receiver: receiver }
 
-  context '#friendable?' do
-    it 'validates if a user CAN be a friend to the current_user' do
-      expected = user.friendable?(current_user)
-      expect(expected).to be_truthy
+  context 'Requested and received friendships' do
+    it 'Requester should be in received_friends' do
+      expect(receiver.received_friends).to include(requester)
+    end
+
+    it 'Receiver should be in requested_friends' do
+      expect(requester.requested_friends).to include(receiver)
     end
   end
 
-  context '#unfriendable?' do
-    let!(:friendship) { create :friendship, user_id: current_user.id, friend_id: user.id }
-    it 'validates if a user can be unfriend by the current user' do
-      expected = user.unfriendable?(current_user)
-      expect(expected).to be_truthy
+  context 'Confirmed friendships' do
+    let!(:friendship) { create :friendship, requester: requester, receiver: receiver, confirmed: true }
+    it 'Receiver should be in friends' do
+      expect(requester.friends).to include(receiver)
+    end
+
+    it 'Requester should be in friends' do
+      expect(receiver.friends).to include(requester)
     end
   end
 end
