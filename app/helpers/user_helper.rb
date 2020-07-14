de54a12274b03b35dbe5c7a0f56aba4135ba6847
jubaan <julio.ab@outlook.com>
2020-07-14 21:24:18 +0000

    Add comment counter in timeline and 'No pending friends' notification in user profile

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,59 +4,54 @@ module UserHelper
 
     new_friendship_path =
       friendships_path({ requester_id: current_user.id, receiver_id: user.id })
-    class_html = 'profile-link'
+    class_html = "profile-link"
 
     if current_user.friends.include?(user)
       render html: link_to(
-        'Unfriend',
+        "Unfriend",
         find_friendship(user),
-        method: :delete, class: class_html
+        method: :delete, class: class_html,
       )
-
     elsif current_user.received_friends.include?(user)
       id = find_friendship(user, true)
 
-      reject_friendship = link_to(
-        'Reject Friendship',
-        find_friendship(user),
-        method: :delete,
-        class: class_html
-      )
+      reject_friendship =
+        link_to(
+          "Reject Friendship",
+          find_friendship(user),
+          method: :delete, class: class_html,
+        )
 
-      accept_friendship = link_to(
-        'Accept Friendship',
-        friendship_path({ id: id, confirmed: true }),
-        method: :patch,
-        class: class_html
-      )
+      accept_friendship =
+        link_to(
+          "Accept Friendship",
+          friendship_path({ id: id, confirmed: true }),
+          method: :patch, class: class_html,
+        )
 
       render html: "#{accept_friendship} | #{reject_friendship} ".html_safe
-
     elsif current_user.requested_friends.include?(user)
       render html: link_to(
-        'Drop invitation',
+        "Drop invitation",
         find_friendship(user),
-        method: :delete, class: class_html
+        method: :delete, class: class_html,
       )
     else
       render html: link_to(
-        'Invite friend',
+        "Invite friend",
         new_friendship_path,
-        method: :post, class: class_html
+        method: :post, class: class_html,
       )
-
     end
   end
 
   def find_friendship(user, return_id = false)
-    friendship_a = Friendship.find_by_requester_id_and_receiver_id(current_user.id, user.id)
-    friendship_b = Friendship.find_by_receiver_id_and_requester_id(current_user.id, user.id)
+    friendship_a =
+      Friendship.find_by_requester_id_and_receiver_id(current_user.id, user.id)
+    friendship_b =
+      Friendship.find_by_receiver_id_and_requester_id(current_user.id, user.id)
 
     friendship = friendship_a || friendship_b
-    if return_id
-      friendship.id
-    else
-      friendship
-    end
+    return_id ? friendship.id : friendship
   end
 end
