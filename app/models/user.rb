b39105120055ac2529dd53c231066c905598a182
Pedro Donda <phfdonda@gmail.com>
2020-07-15 16:38:41 -0300

    fix bug displaying friends posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    # friends_array += inverse_friendships.map { |friendship| friendship.friendship.user if friendship.confirmed }
+    friends_array = friendships.map { |f| f.friend if f.confirmed }
+    friends_array += inverse_friendships.map { |f| f.user if f.confirmed }
     friends_array.compact
   end
 
