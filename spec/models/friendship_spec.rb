d42777155eb34fe91901b0f1724ad83d25ff748b
kotoku-code <kennedy.kotoku@gmail.com>
2020-07-20 12:13:45 +0200

    creaated rspec user

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Friendship, type: :model do
-  let(:user) { User.create(name: 'Temmarie', email: 'Tam@email.com', password: 'tamara') }
-  let(:friend) { User.create(name: 'Becca', email: 'Becca@email.com', password: 'rebecca') }
-  let(:friendship) { Friendship.create(user_id: user.id, friend_id: friend.id) }
+RSpec.describe Like, type: :model do
+  let(:user1) { User.create(name: 'test', email: 'email@email.com', password: 'abc123') }
+  let(:user2) { User.create(name: 'test2', email: 'email2@email.com', password: 'abc123') }
+  let(:friend1) { user1.friendships.create(friend_id: 2) }
 
+  context 'check associations' do
+    it 'frienship belongs to a user' do
+      user1
+      user2
+      friend1
+      expect(Friendship.first.user_id).not_to be_nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new }
+  let(:user1) { User.create(name: 'test', email: 'email@email.com', password: 'abc123') }
+  let(:post1) { user1.posts.create(content: 'abc') }
+
+  context 'Model' do
+    it 'should exist' do
+      expect(user).not_to be_nil
+    end
+  end
+
+  context 'Autofill id using association' do
+    it 'create a post' do
+      user1.posts.create(content: 'abc')
+      expect(user1.posts.first).not_to be_nil
+    end
+  end
+
+  context 'Autofill id using association' do
+    it 'create a comment' do
+      post1.comments.create(user_id: 1, content: 'abc')
+      expect(post1.comments).not_to be_nil
+    end
+  end
+
+  context 'user can leave a like' do
+    it 'leave a like' do
+      user1.likes.create(post_id: 1)
+      expect(post1.likes).not_to be_nil
+    end
+  end
 end
