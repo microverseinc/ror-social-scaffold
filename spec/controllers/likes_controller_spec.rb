291a25b06e57371c57f177743c27b81aa26272c7
teekaytech <taofeek.olalere@feghas.com>
2020-07-20 15:50:11 +0100

    Add test cases for likes feature

---
+++ spec/controllers/likes_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'likes_controller', type: :system do
+  before { User.create(name: 'Taofeek', email: 'taofeek@gmail.com', password: '123456', password_confirmation: '123456') }
+
+  it 'creates a new post' do
+    do_login('taofeek@gmail.com', '123456')
+    create_post("This is a valid post")
+    expect(page).to have_content('Post was successfully created')
+  end
+
+  it 'likes a particular post' do
+    like('taofeek@gmail.com', '123456', 'This is a valid post')
+    expect(page).to have_content('You liked a post')
+  end
+
+  it 'dislikes an already liked post' do
+    like('taofeek@gmail.com', '123456', 'This is a valid post')
+    click_link('Dislike!')
+    expect(page).to have_content('You disliked a post')
+  end
+end
