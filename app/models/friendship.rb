d7a92c50bb6ec192f5e07cad407efda37b92f679
Temmarie <temmar_whyte16@yahoo.com>
2020-07-20 13:19:45 +0100

    finished user_spec tests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend, :class_name => "User"
+  belongs_to :friend, class_name: 'User'
 
   scope :is_friend, -> { where('acceptance =?', true) }
   scope :pendant_add, -> { where('acceptance =?', false) }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,58 @@ RSpec.describe User, type: :model do
       expect(post1.likes).not_to be_nil
     end
   end
+  describe '#name' do
+    it 'doesnt validate user without the name' do
+      user = User.new
+      user.name = nil
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+
+      user.name = 'test'
+      user.valid?
+      expect(user.errors[:name]).to_not include("can't be blank")
+    end
+  end
+
+  describe '#email' do
+    it 'validates for presence of email adress' do
+      user = User.new
+      user.name = 'test'
+      user.email = ''
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+
+      user.email = 'email@gmail.com'
+      user.valid?
+      expect(user.errors[:email]).to_not include("can't be blank")
+    end
+
+    it 'validates for format of email adress' do
+      user = User.new
+      user.name = 'test'
+      user.email = 'email..com'
+      user.valid?
+      expect(user.errors[:email]).to include('is invalid')
+
+      user.email = 'email@gmail.com'
+      user.valid?
+      expect(user.errors[:email]).to_not include('is invalid')
+    end
+
+  end
+
+  describe '#password' do
+    it 'validates the presence of password' do
+      user = User.new
+      user.name = 'test'
+      user.email = 'email@email.com'
+      user.password = ''
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+
+      user.password = '123456'
+      expect(user.valid?).to eql(true)
+    end
+  end
+
 end
