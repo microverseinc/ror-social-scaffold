aa0de8134e61e93c893f12ee5f05362e900364fa
Temmarie <temmar_whyte16@yahoo.com>
2020-07-20 13:45:30 +0100

    fixing rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
+    friends_arrayb = inverse_friendships.map { |friendship| friendship.user if friendship.status }
+    friends_array.concat(friends_arrayb)
     friends_array.compact
   end
 
@@ -29,7 +30,7 @@ class User < ApplicationRecord
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship = inverse_friendships.find { |friend| friend.user == user }
     friendship.confirmed = true
     friendship.save
   end
