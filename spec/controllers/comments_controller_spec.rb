bca93a005a053105ff9fd1f6a2bffd59d193b2e4
teekaytech <taofeek.olalere@feghas.com>
2020-07-20 15:39:46 +0100

    Add test case for comments

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'comments_controller', type: :system do
+  before { User.create(name: 'Taofeek', email: 'taofeek@gmail.com', password: '123456', password_confirmation: '123456') }
+
+  it 'creates a new post' do
+    do_login('taofeek@gmail.com', '123456')
+    create_post("This is a valid post")
+    expect(page).to have_content('Post was successfully created')
+  end
+
+  it 'creates a comment on a post' do
+    do_login('taofeek@gmail.com', '123456')
+    create_post('Another post')
+    fill_in('comment_content', with: 'Awesome!')
+    click_button('Comment')
+    expect(page).to have_content('Comment was successfully created')
+  end
+end
