30115cd34d6f929e141aabe06a28dd9f2d9fc05f
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-24 20:17:57 +0300

    Remove pending_friendships association

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :friendships
-  has_many :pending_friendships, -> { where confirmed: nil }, class_name: 'Friendship', foreign_key: 'friend_id'
-
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
   has_many :inverse_friendships, -> { where confirmed: nil }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :inverse_friends, through: :inverse_friendships, source: :user
-
-  # def friends
-  #   sent_request = Friendship.where(user_id: id, confirmed: true).pluck(:friend_id)
-  #   received_request = Friendship.where(friend_id: id, confirmed: true).pluck(:user_id)
-  #   ids = sent_request + received_request
-  #   User.where(id: ids)
-  # end
 
   def friend_with?(user)
     Friendship.confirmed_record?(id, user.id)
