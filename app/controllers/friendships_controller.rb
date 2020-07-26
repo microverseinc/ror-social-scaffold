f9c7d537132119c52f6094b1126d24f77c3d0a3f
Hemant soni <sonihemant4741@gmail.com>
2020-07-26 20:11:47 +0530

    friendship request error fixed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,7 +6,7 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.new
     @friendship.user_id = current_user.id
-    @friendship.friend_id = friendship_params
+    @friendship.friend_id = params[:format]
     @friendship.status = false
     if @friendship.save
       redirect_to users_path, notice: 'Friend request sent.'
