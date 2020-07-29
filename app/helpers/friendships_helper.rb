5e8409d3e2e99836a01df2e6c29ade8a8185443d
edieatha <edieatha@gmail.com>
2020-07-30 01:57:43 +0300

    Fix linter errors

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -2,9 +2,4 @@ module FriendshipsHelper
   def friends
     @user.friends + @user.users
   end
-  
-  def adding_friend
-    signed_in? && !Friendship.reacted?(current_user.id, user.id) && current_user != user
-  end
-  
 end
