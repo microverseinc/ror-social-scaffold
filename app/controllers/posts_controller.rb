a5704c353f11044e2d271d8db0e88647c094cfd9
Roheem Olayemi <abdurroheem20@gmail.com>
2020-07-29 03:29:18 +0100

    updated the posts controller and views respectively

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,43 @@
 class PostsController < ApplicationController
+  before_action :find_post, except: %i[new create index]
   before_action :authenticate_user!
 
   def index
     @post = Post.new
-    timeline_posts
+    @users = User.all
+    @comment = Comment.create
+    @user = current_user
+    @friends = @user.friends
+  end
+
+  def new
+    @post = current_user.posts.build
   end
 
   def create
     @post = current_user.posts.new(post_params)
 
-    if @post.save
+    @post.user = current_user
+    if @post.valid?
+      @post.save
       redirect_to posts_path, notice: 'Post was successfully created.'
     else
-      timeline_posts
-      render :index, alert: 'Post was not created.'
+      render :new, alert: 'Post was not created.'
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
 
-  def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+  def find_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :user)
   end
 end
