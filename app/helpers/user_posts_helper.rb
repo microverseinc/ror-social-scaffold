15b2b4f41c36903098ed9f2e4802c8ef0f118a33
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-07-31 23:06:53 +0300

    Order user posts by most recent first

--- app/helpers/user_posts_helper.rb
+++ app/helpers/user_posts_helper.rb
@@ -3,7 +3,7 @@ module UserPostsHelper
     if signed_in? && Friendship.reacted?(current_user.id, @user.id)
       render @posts
     elsif signed_in? && @user == current_user
-      render @user.posts
+      render @user.posts.order(created_at: :desc)
     end
   end
 end
