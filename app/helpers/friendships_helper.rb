a2240ee974750ff40cd49598202e8f7a03e5a356
edieatha <edieatha@gmail.com>
2020-07-31 22:15:56 +0300

    Fixed friend_status links in views and helpers

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -7,11 +7,19 @@ module FriendshipsHelper
 
   def friend_status(user)
     cancel = ''
-    if signed_in? && !Friendship.reacted?(current_user.id, user.id) && current_user != user
-      link_to 'Add Friend', friendships_create_path(ids: { id1: current_user.id, id2: user.id }), class: 'friend btn btn-success btn-sm'
-    elsif signed_in? && !Friendship.confirmed_record?(current_user.id, user.id) && current_user != user
+    if signed_in? && 
+      !Friendship.reacted?(current_user.id, user.id) && 
+      current_user != user
+      link_to 'Add Friend', 
+      friendships_create_path(ids: { id1: current_user.id, id2: user.id }), 
+      class: 'friend btn btn-success btn-sm'
+    elsif signed_in? && 
+      !Friendship.confirmed_record?(current_user.id, user.id) && 
+      current_user != user
       user.pending_friendships.each do |friendship|
-        cancel = link_to 'Cancel Request', friendships_destroy_path(friendship_id: friendship.id), class: 'btn btn-danger btn-sm' if current_user
+        cancel = link_to 'Cancel Request', 
+        friendships_destroy_path(friendship_id: friendship.id), 
+        class: 'btn btn-danger btn-sm' if current_user
       end
       cancel.html_safe
     end
@@ -19,10 +27,14 @@ module FriendshipsHelper
 
   def reject_response(user)
     reject = ''
-    if signed_in? && !Friendship.confirmed_record?(current_user.id, user.id) && current_user = user
+    if signed_in? && 
+      !Friendship.confirmed_record?(current_user.id, user.id) && 
+      current_user = user
       user.pending_friendships.each do |friendship|
         if current_user
-          reject = link_to 'Reject', friendships_destroy_path(friendship_id: friendship.id), class: 'btn btn-danger btn-sm'
+          reject = link_to 'Reject', 
+          friendships_destroy_path(friendship_id: friendship.id), 
+          class: 'btn btn-danger btn-sm'
         end
       end
       reject.html_safe
@@ -31,10 +43,14 @@ module FriendshipsHelper
 
   def accept_response(user)
     accept = ''
-    if signed_in? && !Friendship.confirmed_record?(current_user.id, user.id) && current_user = user
+    if signed_in? && 
+      !Friendship.confirmed_record?(current_user.id, user.id) && 
+      current_user = user
       user.pending_friendships.each do |friendship|
         if current_user
-          accept = link_to 'Accept', friendships_update_path(friendship_id: friendship.id), class: 'btn btn-success btn-sm'
+          accept = link_to 'Accept', 
+          friendships_update_path(friendship_id: friendship.id), 
+          class: 'btn btn-success btn-sm'
         end
       end
       accept.html_safe
@@ -43,7 +59,9 @@ module FriendshipsHelper
 
   def display_name(user)
     name =''
-    if signed_in? && !Friendship.confirmed_record?(current_user.id, user.id) && current_user = user
+    if signed_in? && 
+      !Friendship.confirmed_record?(current_user.id, user.id) && 
+      current_user = user
       user.pending_friendships.each do |friendship|
         if current_user
           name = User.find(friendship.user_id).name
@@ -56,8 +74,15 @@ module FriendshipsHelper
   def unfriend(friend)
     un_friend = ''
     if @user == current_user
-      un_friend = link_to 'Un-friend', friendships_destroy_path(friendship_id: Friendship.find_request(@user.id, friend.id)), class: 'btn btn-danger btn-sm'
+      un_friend = link_to 'Un-friend', 
+      friendships_destroy_path(friendship_id: Friendship.find_request(@user.id, friend.id)), 
+      class: 'btn btn-danger btn-sm'
     end
     un_friend.html_safe
   end
+
+  def display_links(user)
+    friend_status(user) if Friendship.where(user_id: current_user.id)
+  end
+
 end
