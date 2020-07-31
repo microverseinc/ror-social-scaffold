adeac8c4449b224b0d107619561ffa1d22910d04
edieatha <edieatha@gmail.com>
2020-07-31 06:18:10 +0300

    Reset Files

--- app/models/like.rb
+++
@@ -1,6 +0,0 @@
-class Like < ApplicationRecord
-  validates :user_id, uniqueness: { scope: :post_id }
-
-  belongs_to :user
-  belongs_to :post
-end
--- db/migrate/20200728205631_add_default_value_to_confirmed.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueToConfirmed < ActiveRecord::Migration[6.0]
-  def change
-    change_column :friendships, :confirmed, :boolean, :default => false
-  end
-end
