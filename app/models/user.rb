ab923910e6573c483db6aa0419cbb225732d2e63
8Bts <mcmizze@yahoo.com>
2020-08-04 13:36:24 +0400

    Add User#confirm_friendship, User#friends? methods and fix lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,35 +13,30 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-
   def friends
-    friends_list = friendships.map{ |friendship| friendship.friend if friendship.status}
-    
-    friends_list + inverse_friendships.map{ |friendship| friendship.user if friendship.status}
+    friends_list = friendships.map { |friendship| friendship.friend if friendship.status }
+
+    friends_list += inverse_friendships.map { |friendship| friendship.user if friendship.status }
 
     friends_list.compact
   end
 
   def pending_friends
-
-    friendships.map {|friendship| friendship.friend unless friendship.status}.compact
-
+    friendships.map { |friendship| friendship.friend unless friendship.status }.compact
   end
 
   def friendship_requests
-
-    inverse_friendships.map { |friendship| friendship.user unless friendship.status}.compact
-
+    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
   end
 
   def confirm_friendship(user)
-    friendship = inverse_friendships.find{ |friendship| friendship.user == user }
+    friendship_record = inverse_friendships.find { |friendship| friendship.user == user }
 
-    friendship.status = true
-    friendship.save
+    friendship_record.status = true
+    friendship_record.save
   end
 
   def friend?(user)
     friends.include?(user)
-  end  
+  end
 end
