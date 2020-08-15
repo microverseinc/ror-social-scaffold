17c014c185f157242696ce0dbc0860e82a9a0229
SmithVyne <smithnkereuwem2@gmail.com>
2020-08-15 15:30:21 +0300

    fix lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,22 +14,21 @@ class User < ApplicationRecord
   has_many :invitations, class_name: 'Friendship', foreign_key: :invitee_id
 
   def friends
-    friends_array = friendships.map{|friendship| friendship.invitee if friendship.status}
+    friends_array = friendships.map { |friendship| friendship.invitee if friendship.status }
 
-    friends_array + invitations.map{|friendship| friendship.user if friendship.status}
+    friends_array + invitations.map { |friendship| friendship.user if friendship.status }
     friends_array.compact
   end
 
   def pending_friends
-    friendships.map{|friendship| friendship.invitee if !friendship.status}.compact
+    friendships.map { |friendship| friendship.invitee unless friendship.status }.compact
   end
 
   def friend_requests
-    friendships.map{|friendship| friendship.user if !friendship.status}.compact
+    friendships.map { |friendship| friendship.user unless friendship.status }.compact
   end
 
   def friend?(user)
     friends.include?(user)
   end
-
 end
