6d2d9ab69783788d9e907e098bd76b6b7f8533d0
Ignatius22 <igee54077@gmail.com>
2020-08-19 07:24:55 +0100

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,6 @@ class User < ApplicationRecord
   has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships
 
- 
-
   # Users who have yet to confirmed friend invites
   def pending_invites
     pending_friends
