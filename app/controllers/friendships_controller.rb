9f95475f1aeca660896cf620331e735f7d072480
Yahir Cardona <yahir.cardona@gmail.com>
2020-09-03 16:13:33 -0600

    rubocop ofeenses fixed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,17 +1,16 @@
 class FriendshipsController < ApplicationController
-
-    def create
-         @friendship = current_user.friendships.new(friend_id: params[:friend_id])
-         @friendship.confirmed = false
-         if @friendship.save
-            redirect_to users_path, notice: 'You send a friend request.'
-          else
-            redirect_to users_path, alert: @friendship.errors.full_messages.join('. ').to_s
-          end
+  def create
+    @friendship = current_user.friendships.new(friend_id: params[:friend_id])
+    @friendship.confirmed = false
+    if @friendship.save
+      redirect_to users_path, notice: 'You send a friend request.'
+    else
+      redirect_to users_path, alert: @friendship.errors.full_messages.join('. ').to_s
     end
+  end
 
+  private
 
-    private
   def friendships_params
     params.require(:friendship).permit(:friend_id, :user_id, :confirmed)
   end
