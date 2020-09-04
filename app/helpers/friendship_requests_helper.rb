7177595fb2efc1e72aedfaa4e8bd6689b580f5ef
Bluette1 <marylene.sawyer@gmail.com>
2020-09-04 12:35:58 +0200

    Refactor user views

--- app/helpers/friendship_requests_helper.rb
+++ app/helpers/friendship_requests_helper.rb
@@ -6,16 +6,21 @@ module FriendshipRequestsHelper
   end
 
   def accept_btn(user)
-    if pending_friend?(user)
-      sent_request = current_user.received_requests.find_by(sender_id: user.id)
-      button_to 'Accept', user_friendship_request_path(user.id, sent_request.id), method: :patch, class: 'btn add-friend btn-secondary', type: 'submit'
-    end
+    check_pending_request(user, 'Accept')
   end
 
   def reject_btn(user)
+    check_pending_request(user, 'Reject')
+  end
+
+  def set_user(user)
+    @user = user
+  end
+
+  def check_pending_request(user, action)
     if pending_friend?(user)
       sent_request = current_user.received_requests.find_by(sender_id: user.id)
-      button_to 'Reject', user_friendship_request_path(user.id, sent_request.id), method: :delete, class: 'btn add-friend btn-secondary', type: 'submit'
+      button_to action, user_friendship_request_path(user.id, sent_request.id), method: :patch, class: 'btn add-friend btn-secondary', type: 'submit'
     end
   end
 end
