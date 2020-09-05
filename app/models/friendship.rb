69e37a023f2fd5af755d16c1039a499dd7235719
Bluette1 <marylene.sawyer@gmail.com>
2020-09-05 07:28:43 +0200

    Add model specs for `friendship` model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   belongs_to :inverse_friend, class_name: 'User'
-  validates :friend_id, uniqueness: { scope: :inverse_friend_id }
+  validates :friend, uniqueness: { scope: :inverse_friend }
 end
--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe FriendRequest, type: :model do
   describe 'Validations' do
     let(:sender) do
-      User.create(name: 'sender')
+      User.create(name: 'sender', email: 'email@gm.com')
     end
     let(:receiver) do
-      User.create(name: 'receiver')
+      User.create(name: 'receiver', email: 'email1@gm.com')
     end
 
     subject do
@@ -25,6 +25,12 @@ RSpec.describe FriendRequest, type: :model do
     it 'is not valid without a receiver' do
       subject.receiver = nil
       expect(subject).to_not be_valid
+    end
+
+    it 'should validate uniqueness of sender -> scope receiver' do
+      described_class.create(sender: sender, receiver: receiver)
+      expect(subject).not_to be_valid
+      expect(subject.errors.full_messages).to eq ['Sender has already been taken']
     end
   end
 
