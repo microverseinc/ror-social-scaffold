13a51f68ccb90429aecba8a0179427fbc7a609f0
Bluette1 <marylene.sawyer@gmail.com>
2020-09-07 04:32:54 +0200

    Add friendship requests feature specs

--- spec/controllers/friendship_requests_controller_spec.rb
+++ spec/controllers/friendship_requests_controller_spec.rb
@@ -4,43 +4,42 @@ require 'support/devise_config'
 
 RSpec.describe FriendshipRequestsController, type: :controller do
   describe 'post friend request' do
-
     log_in_usr :sender, 'jane', 'jane@gmail.com'
-  
-    it 'successfully sends a friend request and sets the correct flash notice' do  
-      receiver  = create(:receiver, name: 'marylene', email: 'marylene@gmail.com')
-      sender  = User.find_by(name: 'jane', email: 'jane@gmail.com')
-      post :create, params: {:user_id => receiver.id}
+
+    it 'successfully sends a friend request and sets the correct flash notice' do
+      receiver = create(:receiver, name: 'marylene', email: 'marylene@gmail.com')
+      sender = User.find_by(name: 'jane', email: 'jane@gmail.com')
+      post :create, params: { user_id: receiver.id }
       expect(response.code).to eq '302'
       expect(response).to have_http_status(:found)
       expect(flash[:notice]).to eq "A friendship request was successfully sent to #{receiver.name}"
       expect(FriendRequest.find_by(sender: sender, receiver: receiver, status: 'pending')).not_to eq nil
     end
 
-    it 'fails to post a friend request and sets the correct flash notice' do  
-      receiver  = create(:receiver, name: 'marylene', email: 'marylene@gmail.com')
-      sender  = User.find_by(name: 'jane', email: 'jane@gmail.com')
+    it 'fails to post a friend request and sets the correct flash notice' do
+      receiver = create(:receiver, name: 'marylene', email: 'marylene@gmail.com')
+      sender = User.find_by(name: 'jane', email: 'jane@gmail.com')
       FriendRequest.create(sender: sender, receiver: receiver)
-      post :create, params: {:user_id => receiver.id}
+      post :create, params: { user_id: receiver.id }
       expect(response.code).to eq '302'
       expect(response).to have_http_status(:found)
       expect(flash[:notice]).to eq nil
-      expect(flash[:alert]).to eq "An error occurred while trying to request the friendship [\"Sent requests is invalid\"]"
+      expect(flash[:alert])
+        .to eq 'An error occurred while trying to request the friendship ["Sent requests is invalid"]'
     end
   end
 
   describe 'update friend request' do
-
     log_in_usr :receiver, 'marylene', 'marylene@gmail.com'
 
-    it 'successfully accepts a friend request, creates a friendship and sets the correct flash notice' do  
-      sender  = create(:sender, name: 'jane', email: 'jane@gmail.com')
-      receiver  = User.find_by(name: 'marylene', email: 'marylene@gmail.com')
+    it 'successfully accepts a friend request, creates a friendship and sets the correct flash notice' do
+      sender = create(:sender, name: 'jane', email: 'jane@gmail.com')
+      receiver = User.find_by(name: 'marylene', email: 'marylene@gmail.com')
 
       FriendRequest.create(sender: sender, receiver: receiver)
-      
-      sent_request = FriendRequest.find_by(receiver: receiver, status: 'pending', )
-      patch :update, params: {:user_id => sender.id, id: sent_request.id}
+
+      sent_request = FriendRequest.find_by(receiver: receiver, status: 'pending')
+      patch :update, params: { user_id: sender.id, id: sent_request.id }
       expect(response.code).to eq '302'
       expect(response).to have_http_status(:found)
       expect(flash[:notice]).to eq "You are now friends with #{sender.name}"
@@ -48,36 +47,36 @@ RSpec.describe FriendshipRequestsController, type: :controller do
       expect(Friendship.find_by(friend: receiver, inverse_friend: sender)).not_to eq nil
     end
 
-    it 'fails to accepts a friend request and sets the correct flash notice' do  
-      sender  = create(:sender, name: 'jane', email: 'jane@gmail.com')
-      receiver  = User.find_by(name: 'marylene', email: 'marylene@gmail.com')
+    it 'fails to accepts a friend request and sets the correct flash notice' do
+      sender = create(:sender, name: 'jane', email: 'jane@gmail.com')
+      receiver = User.find_by(name: 'marylene', email: 'marylene@gmail.com')
 
       FriendRequest.create(sender: sender, receiver: receiver)
-      
-      sent_request = FriendRequest.find_by(receiver: receiver, status: 'pending', )
-      patch :update, params: {:user_id => receiver.id, id: sent_request.id}
+
+      sent_request = FriendRequest.find_by(receiver: receiver, status: 'pending')
+      patch :update, params: { user_id: receiver.id, id: sent_request.id }
       expect(response.code).to eq '302'
       expect(response).to have_http_status(:found)
       expect(flash[:notice]).to eq nil
-      expect(flash[:alert]).to eq "An error occurred while trying to save the friendship [\"Friend has already been taken\"]"
+      expect(flash[:alert])
+        .to eq 'An error occurred while trying to save the friendship ["Friend has already been taken"]'
       expect(Friendship.find_by(friend: sender, inverse_friend: receiver)).to eq nil
       expect(Friendship.find_by(friend: receiver, inverse_friend: sender)).to eq nil
     end
   end
 
   describe 'destroy friend request' do
-
     log_in_usr :receiver, 'marylene', 'marylene@gmail.com'
 
-    it 'rejects a friend request, creates a friendship and sets the correct flash notice' do  
-      sender  = create(:sender, name: 'jane', email: 'jane@gmail.com')
-      receiver  = User.find_by(name: 'marylene', email: 'marylene@gmail.com')
+    it 'rejects a friend request, creates a friendship and sets the correct flash notice' do
+      sender = create(:sender, name: 'jane', email: 'jane@gmail.com')
+      receiver = User.find_by(name: 'marylene', email: 'marylene@gmail.com')
 
       FriendRequest.create(sender: sender, receiver: receiver)
-      
-      sent_request = FriendRequest.find_by(sender: sender, status: 'pending', )
-      
-      patch :destroy, params: {:user_id => sender.id, id: sent_request.id}
+
+      sent_request = FriendRequest.find_by(sender: sender, status: 'pending')
+
+      patch :destroy, params: { user_id: sender.id, id: sent_request.id }
       expect(response.code).to eq '302'
       expect(response).to have_http_status(:found)
       expect(flash[:notice]).to eq "The friendship request from #{sender.name} was successfully rejected"
---
+++ spec/features/friendship_requests_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe 'Friendship Management Features', type: :feature do
+  let(:mary) do
+    {
+      name: 'Mary', email: 'm@gmail.com', password: 'password', password_confirmation: 'password'
+    }
+  end
+  let(:jane) { { name: 'Jane', email: 'j@gmail.com', password: 'password', password_confirmation: 'password' } }
+
+  before :each do
+    visit new_user_registration_path
+    fill_in 'Name', with: mary[:name]
+    fill_in 'Email', with: mary[:email]
+    fill_in 'Password', with: mary[:password]
+    fill_in 'Password confirmation', with: mary[:password_confirmation]
+    click_button 'Sign up'
+    click_on 'Sign out'
+    # register_user jane
+    visit new_user_registration_path
+    fill_in 'Name', with: jane[:name]
+    fill_in 'Email', with: jane[:email]
+    fill_in 'Password', with: jane[:password]
+    fill_in 'Password confirmation', with: jane[:password_confirmation]
+    click_button 'Sign up'
+  end
+  scenario 'user can send a friendship request to another user' do
+    fill_in :post_content, with: 'Hello users, I am Jane.'
+    click_button 'Save'
+    click_on 'All users'
+    click_button 'Add Friend'
+
+    expect(page).to have_content("A friendship request was sent and is awaiting confirmation\nName: #{mary[:name]}\n")
+  end
+
+  scenario 'a user with a pending friendship request can accept it on the all users\' list' do
+    # Jane creates a post and sends a friend request to Mary
+    jane_post = 'Hello users, I am Jane.'
+    fill_in :post_content, with: jane_post
+    click_button 'Save'
+    click_on 'All users'
+    click_button 'Add Friend'
+    click_on 'Sign out'
+
+    # Sign in as Mary
+    visit new_user_session_path
+    fill_in 'Email', with: mary[:email]
+    fill_in 'Password', with: mary[:password]
+    click_button 'Log in'
+
+    # Mary accepts Jane's friend request
+    click_on 'All users'
+    click_button 'Accept'
+
+    expect(page).to have_content("You are now friends with #{jane[:name]}")
+    click_on 'Timeline'
+    expect(page).to have_content(jane_post)
+  end
+
+  scenario 'a user with a pending friendship request can accept it on the sender\'s profile page' do
+    # Jane creates a post and sends a friend request to Mary
+    jane_post = 'Hello users, I am Jane.'
+    fill_in :post_content, with: jane_post
+    click_button 'Save'
+    click_on 'All users'
+    click_button 'Add Friend'
+    click_on 'Sign out'
+
+    # Sign in as Mary
+    visit new_user_session_path
+    fill_in 'Email', with: mary[:email]
+    fill_in 'Password', with: mary[:password]
+    click_button 'Log in'
+
+    # Mary creates a post and accepts Jane's friend request
+    marylene_post = 'Hello users, I am Mary.'
+    fill_in :post_content, with: marylene_post
+    click_button 'Save'
+    click_on mary[:name]
+    click_button 'Accept'
+
+    expect(page).to have_content("You are now friends with #{jane[:name]}")
+    click_on 'Timeline'
+    expect(page).to have_content(jane_post)
+    click_on 'Sign out'
+
+    # Sign in as Jane
+    visit new_user_session_path
+    fill_in 'Email', with: jane[:email]
+    fill_in 'Password', with: jane[:password]
+    click_button 'Log in'
+    expect(page).to have_content(marylene_post)
+  end
+end
