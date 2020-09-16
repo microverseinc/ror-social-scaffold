eea8e0943698fa6bff350c456685649f69a993a7
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-09-16 15:17:06 +0300

    Add comment for friendship helper

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,5 +1,6 @@
 module FriendshipsHelper
   def friendship_action(current_user, user)
+    # render the form only for users different from the current_user
     render 'friendships/friendship_actions', user: user unless current_user == user
   end
 end
