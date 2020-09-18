ebec951446f778abf4e1b171e98762bfd4708aff
Yahir Cardona <yahir.cardona@gmail.com>
2020-09-18 16:46:28 -0600

    rubocop fixed

--- app/helpers/friensdhips_helper.rb
+++ app/helpers/friensdhips_helper.rb
@@ -2,6 +2,6 @@ module FriensdhipsHelper
   def accept_reject_friend(friendship)
     return if friendship.confirmed
 
-    link_to('Accept friend request', confirm_path(user_id: friendship.user), method: :patch, style: "margin-right: 20px",  class: 'profile-link') << link_to('Reject friend request', reject_path(user_id: friendship.user), method: :delete, class: 'profile-link')
+    link_to('Accept friend request', confirm_path(user_id: friendship.user), method: :patch, style: 'margin-right: 20px', class: 'profile-link') << link_to('Reject friend request', reject_path(user_id: friendship.user), method: :delete, class: 'profile-link')
   end
 end
