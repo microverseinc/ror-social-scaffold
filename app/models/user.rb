30463667aae544a3bbb5b8c75922149a8a54d276
Joseph Burke <joseph_burke@outlook.com>
2020-09-23 09:02:28 +0100

    Create Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships, foreign_key: :user_id
+  has_many :friendships, foreign_key: :friend_id
 end
