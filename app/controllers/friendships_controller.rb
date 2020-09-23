5835f211da5d97ee669fcc03c02718df6de1d12c
Joseph Burke <joseph_burke@outlook.com>
2020-09-23 16:03:49 +0100

    Implement "Accept Friendship Request" button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,11 +3,21 @@ class FriendshipsController < ApplicationController
   def create
     Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], status: 'pending')
   end
-  
+
+  def update
+    Friendship.find(params[:id]).update(status: 'accepted')
+  end
+
+
+  def destroy
+    Friendship.find(params[:id]).destroy
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:status)
+  end
+
 end
-
-# The button is working, it activates the FriendshipsController#create
-# FriendshipsController#create is also working, creating a new item in the table.
-
-# It is saving the current_user.id as the user_id for the Friendship it makes.
 
