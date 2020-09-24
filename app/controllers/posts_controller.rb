9190867464ee25f1f1a713426004bf6f74927033
Joseph Burke <joseph_burke@outlook.com>
2020-09-24 13:47:34 +0100

    Adjust Posts#index to only show posts from connected users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,8 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    connected_user_ids = current_user.friends.ids << current_user.id
+    @timeline_posts ||= Post.where(user_id: connected_user_ids).ordered_by_most_recent.includes(:user)
   end
 
   def post_params
