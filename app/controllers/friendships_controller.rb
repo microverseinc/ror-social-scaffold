0dcfb2a63cee2bc45870b727ef12808c3c774d90
Joseph Burke <joseph_burke@outlook.com>
2020-09-24 13:58:20 +0100

    Add redirect methods to Friendship actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], status: 'pending')
+    redirect_back(fallback_location: root_path)
   end
 
   def update
@@ -9,10 +10,12 @@ class FriendshipsController < ApplicationController
       friendship.update(status: 'accepted')
       Friendship.create(user_id: friendship[:friend_id], friend_id: friendship[:user_id], status: 'accepted')
     end
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     Friendship.find(params[:id]).destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private
