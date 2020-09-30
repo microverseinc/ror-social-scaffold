24b3c733bc4d5abe800d2658c88a097bd912e7e1
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-30 14:58:21 +0100

    Add total friendship method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,13 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, class_name: :Friendship, foreing_key: :friend_id 
+  has_many :inverse_friendships, class_name: :Friendship, foreing_key: :friend_id
+
+    def total_friends
+      invitation_sent = friendships.each{|friendship| friendship.friend if friendship.comfirmed ==true }
+      invitation_received = friendship.each{|friendship| friendship.user if friendship.confirmed==true }
+      my_friends = invitation_sent + invitation_received
+    end
+
+
 end
