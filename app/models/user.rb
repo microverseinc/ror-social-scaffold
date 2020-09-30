0e60b52a810d8d7437a7c898f2f823e83a1942c2
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-30 15:50:17 +0100

    Add association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,26 @@ class User < ApplicationRecord
 
     def total_friends
       invitation_sent = friendships.each{|friendship| friendship.friend if friendship.comfirmed ==true }
-      invitation_received = friendship.each{|friendship| friendship.user if friendship.confirmed==true }
+      invitation_received = inverse_friendship.each{|friendship| friendship.user if friendship.confirmed==true }
       my_friends = invitation_sent + invitation_received
+    end
+
+    def pending_friends
+      friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    end
+
+    def friend_requests
+      inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    end
+
+    def confirm_friend(user)
+      friendship = inverse_friendships.find{|friendship| friendship.user == user }
+      frienship.comfirmed = true
+      friendship.save 
+    end
+
+    def friend?(user)
+      friends.include?(user)
     end
 
 
