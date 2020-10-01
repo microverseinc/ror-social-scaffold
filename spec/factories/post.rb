f66dcaba9f0049dd169e50237898c15ec4c9b620
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-01 16:52:26 +0100

    Add test for posts model

---
+++ spec/factories/post.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+    factory :post do
+      content  { Faker::Lorem.paragraph}
+    end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+    before{ @post = FactoryGirl.build(:post) }
+
+    context "Check presence on content in post" do
+        it { is_expected.to validate_presence_of(:content) }
+    end
+
+    context "should belong to user model" do
+        it { should belong_to(:user) }
+    end
+
+    context "has many comments and likes" do
+        it { should have_many(:comments) }
+        it { should have_many(:likes) }
+    end
+end
