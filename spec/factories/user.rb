bcca04e9594710f210e9a9b2b67dba5395f9673c
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-01 15:19:42 +0100

    Add test to user model password

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
     factory :user do
       name     { Faker::Name.name }
       email    { Faker::Internet.email }
+      password   { Faker::Internet.password }
+
     end
-  end
+end 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#  let(:user) {build(:User)}
-
-#  context "when name is blank" do 
-#   before {user.name =""}
-#   it {expect(user).not_to be_valid }
-#  end
-# end
 RSpec.describe User, type: :model do
 
   before{ @user = FactoryGirl.build(:user) }
 
+  context "when name is blank" do 
+    it {expect(@user).to be_valid }
+   end
+
  it { expect(@user).to respond_to(:name)}
  it { expect(@user).to respond_to(:email)}
+ it { expect(@user).to respond_to(:password)}
+
  end
