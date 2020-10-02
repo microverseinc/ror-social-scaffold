5c5b3557d8fd47ac10c4c0146102eaa26f3be51b
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-02 11:32:09 +0100

    Fix rubocop errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,8 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 if Rails.env.production?
   abort('The Rails environment is running in production mode!')
+else
 end
-
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
