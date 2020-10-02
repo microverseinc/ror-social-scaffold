b80c4b85f302c048a2127e29e0327a1c20f97587
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-02 10:19:41 +0100

    Add test for friendship

---
+++ spec/factories/friendship.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+    factory :friendship do
+      confirmed  { Faker::Boolean.boolean }
+    end
+end
---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+    context "should return boolean(T|F)" do      
+        it { should allow_value(true).for(:confirmed) }
+        it { should allow_value(false).for(:confirmed) }   
+    end
+
+    
+    context "Friendship belong to" do
+        it { should belong_to(:user) }
+        it { should belong_to(:friend) }
+    end
+end
