efc1c823dd6b51695d0044b597bfa8cb858d2232
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-02 10:48:21 +0100

    Fix rubocop error

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     { Faker::Name.name }
-    email    { Faker::Internet.email }
-    password   { Faker::Internet.password }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 end
