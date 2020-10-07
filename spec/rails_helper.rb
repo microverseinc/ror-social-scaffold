9b7eac378bb5950cbcbb90e84f7c4122bc0adff2
Ramin Mammadzada <remki-design@Hotmail.com>
2020-10-07 21:36:13 +0300

    Update friendship & user testing files.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -89,4 +89,11 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
