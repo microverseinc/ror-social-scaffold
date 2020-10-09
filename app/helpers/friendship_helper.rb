53e80081a63fb3e4cad189dda3d0a62c5fdf933c
jurgen1c <jurgen1c@gmail.com>
2020-10-09 14:13:30 -0600

    Add routes to friendships resources &and update helper

--- app/helpers/friendship_helper.rb
+++ app/helpers/friendship_helper.rb
@@ -1,37 +1,41 @@
 module FriendshipHelper
   def not_user_display(current_user, n_user)
     return unless n_user != current_user
+    friendship = Friendship.find_by(user_id: n_user.id, friend_id: current_user.id)
 
     if current_user == n_user
-      content_tag(:td, (link_to 'Unfriend', unfriend_path(user_id: current_user.id, friend_id: n_user.id)))
+      content_tag(:td, (link_to 'Unfriend', friendship_path(user_id: current_user.id, friend_id: n_user.id friendship_id: friendship.id)))
     elsif current_user.friend?(n_user)
-      content_tag(:td, (link_to 'Unfriend', unfriend_path(user_id: current_user.id, friend_id: n_user.id)))
+      content_tag(:td, (link_to 'Unfriend', friendship_path(user_id: n_user.id, friend_id: current_user.id, friendship_id: friendship.id)))
     elsif n_user.friend_requests.include?(current_user) || n_user.pending_friends.include?(current_user)
       content_tag(:td, 'Pending')
     else
-      content_tag(:td, (link_to 'Add Friend', addfriend_path(confirmed: false, user_id: current_user.id,
-                                                             friend_id: n_user.id)))
+      content_tag(:td, (link_to 'Add Friend', friendships_path(confirmed: false, user_id: current_user.id,
+                                                             friend_id: n_user.id,), method: :post))
     end
   end
 
   def user_display?(current_user, a_user)
     return unless a_user == current_user
+    friendship = Friendship.find_by(user_id: a_user.id, friend_id: current_user.id)
 
     if current_user == a_user
-      content_tag(:td, (link_to 'Unfriend', unfriend_path(user_id: current_user.id, friend_id: a_user.id)))
+      content_tag(:td, (link_to 'Unfriend', friendship_unfriend_path(user_id: current_user.id, friend_id: a_user.id, friendship_id: friendship.id)))
     elsif current_user.friend?(a_user)
-      content_tag(:td, (link_to 'Unfriend', unfriend_path(user_id: current_user.id, friend_id: a_user.id)))
+      content_tag(:td, (link_to 'Unfriend', friendship_unfriend_path(user_id: current_user.id, friend_id: a_user.id, friendship_id: friendship.id)))
     else
-      content_tag(:td, (link_to 'Add Friend', addfriend_path(confirmed: false, user_id: current_user.id,
-                                                             friend_id: a_user.id)))
+      content_tag(:td, (link_to 'Add Friend', friendship_addfriend_path(confirmed: false, user_id: current_user.id,
+                                                             friend_id: a_user.id, friendship_id: friendship.id)))
     end
   end
 
   def show_approve(user, p_friend)
-    link_to 'Approve', approve_friendship_path(user_id: user.id, friend_id: p_friend.id) if user == current_user
+    friendship = Friendship.find_by(user_id: p_friend.id, friend_id: user.id)
+    link_to 'Approve', friendship_approve_friendship_path(user_id: user.id, friend_id: p_friend.id, friendship_id: friendship.id) if user == current_user
   end
 
   def show_decline(user, d_friend)
-    link_to 'Decline', unfriend_path(user_id: user.id, friend_id: d_friend.id) if user == current_user
+    friendship = Friendship.find_by(friend_id: user.id, user_id: d_friend.id)
+    link_to 'Decline', friendship_unfriend_path(user_id: user.id, friend_id: d_friend.id, friendship_id: friendship.id) if user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,14 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-  resources :friendships, only: [:create, :destroy, :approve]
-  post '/addfriend',  to:'friendships#create'
-  get '/addfriend',  to:'friendships#create'
-  get '/unfriend', to:'friendships#destroy'
-  post '/unfriend', to:'friendships#destroy'
+  resources :friendships, only: [:create, :destroy, :approve] do
+    #post '/addfriend',  to:'friendships#create'
+    #get '/addfriend',  to:'friendships#create'
+    get '/unfriend', to:'friendships#destroy'
+    post '/unfriend', to:'friendships#destroy'
 
-  get '/approve_friendship', to:'friendships#approve'
-  post '/approve_friendship', to:'friendships#approve'
+    get '/approve_friendship', to:'friendships#approve'
+    post '/approve_friendship', to:'friendships#approve'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
