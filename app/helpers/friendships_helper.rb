0c961f01650acf861d50c5f6c3b377636d252e75
vanheaven <vanheaven6@gmail.com>
2020-10-11 02:32:06 +0300

    Add helper function for view logic

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,15 +1,24 @@
 module FriendshipsHelper
   def friendship_stack(user)
-    if user == current_user
-      content_tag(:span, '(Current User)', class: 'text-secondary text-bold')
-    elsif current_user.friend? user
-      link_to('Remove')
+    if current_user.friend? user
+      link_to('Cancel friend', cancel_friend_path(user_id: user.id),
+                               method: :delete,
+                               data: { confirm: "Continue and cancel #{user.name}?" }
+             )
     elsif current_user.pending_friends.to_a.include? user
-      content_tag(:span, 'Friend Request Sent', class: 'text-success')
+      content_tag(:span, 'Friend Request Sent', class: 'text-success text-bold')
     elsif current_user.friend_invites.to_a.include? user
-      link_to('Accept', accept_request_path(invitor_id: user.id))
+      content_tag :span do
+        content_tag(:span, link_to('Accept', accept_request_path(invitor_id: user.id))) +
+        content_tag(:span, link_to('Reject', 
+                                    decline_request_path(invitor_id: user.id),
+                                    method: :delete,
+                                    class: 'ml-5'
+                                  )
+                   )
+      end
     else
-      link_to('Send Invite', send_request_path(invitee_id: user.id), class: 'text-primary')
+      link_to('Send Invite', send_request_path(invitee_id: user.id))
     end
   end
 end
