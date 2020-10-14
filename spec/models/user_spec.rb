dda25234047f145874a4ba20f25da01b20745d96
Samuel Almao Herrera <samuelalmaoherrera@gmail.com>
2020-10-13 23:37:44 -0300

    Updated test descriptions.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,35 +12,31 @@ RSpec.describe User, type: :model do
   let!(:friend_real2) { Friendship.create(user_id: 2, friend_id: 1, confirmed: false) }
   let!(:friend_real3) { Friendship.create(user_id: 3, friend_id: 1, confirmed: false) }
 
-  it 'checks for the user friends' do
+  it 'Checks if the user has friends' do
     expect(pedro.friends).not_to be_nil
   end
 
-  it 'checks for the user friends' do
+  it 'Checks for the instance where the users has no friends.' do
     expect(tomas.friends).to be_empty
   end
 
-  it 'checks for the user pending friendships' do
+  it 'Checks when the user has no pending friendships' do
     expect(tomas.pending_friends).not_to be_empty
   end
 
-  it 'checks for the user pending friends' do
+  it 'Checks for the to have user pending friends' do
     expect(pedro.pending_friends).to be_empty
   end
 
-  it 'checks for the user pending request' do
+  it 'Checks for the pending request friend request sent by the user' do
     expect(pedro.friend_requests).not_to be_empty
   end
 
-  it 'checks for the user pending request' do
-    expect(tomas.friend_requests).to be_empty
-  end
-
-  it 'checks if users are friends' do
+  it 'Checks if users are not friends' do
     expect(tomas.friend?(pedro)).to be false
   end
 
-  it 'checks if users are friends' do
+  it 'Checks if users are friends' do
     expect(pedro.friend?(tomas)).to be true
   end
 end
