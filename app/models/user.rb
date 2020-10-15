f371db070b7c88e82783ab6c23ce7e6e1dba5da9
Zubenna <emelunna@gmail.com>
2020-10-15 13:24:36 +0100

    Modefied feature methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,31 +10,31 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-
-  def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed }
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed }
+  has_many :inverse_friendships, class_name: :Friendship, foreign_key: :friend_id
+  
+  def freinds
+    friends_array = friendships.map { |friendship| friendship.freind if freindship.status }
+    freinds_array += inverse_friendships.map { |freindship| friendship.user if freindship.status }
     friends_array.compact
   end
-  
-  def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed }.compact
-  end
 
+  def pending_freinds
+    friendships.map { |freindship| freindship.friend unless friendship.status }.compact
+  end
   
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    inverse_friendships.map { |freindship| friendship.user unless friendship.status }.compact
   end
 
-  def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
-    friendship.confirmed = true
-    friendship.save
+  def confirm_freind(user)
+    freindship = inverse_freindships.find{ |freindship| friendship.user == user }
+    freindship.status = true
+    freindship.save
   end
 
   def friend?(user)
     friends.include?(user)
   end
+  
 end
 
