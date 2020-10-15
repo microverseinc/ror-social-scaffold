3d5669b262232f4e87056164f7b0b51b81a1b296
Umair Arshad <Umair6622@gmail.com>
2020-10-15 22:09:49 +0500

    Update friendship controller, routes and navbar

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,24 @@
 class FriendshipsController < ApplicationController
+    def new
+        @friendship = Friendship.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+
+
+    private 
+    def friendship_params
+        params.require(friendship).permit(:user_id, :friend_id, :confirmed)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
