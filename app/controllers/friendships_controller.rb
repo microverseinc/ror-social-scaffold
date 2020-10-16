f7d6aa1cbb16373127707b8a223295c0f855c524
Zubenna <emelunna@gmail.com>
2020-10-16 16:37:31 +0100

    Implemented actions for user and friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,24 +1,26 @@
 class FriendshipsController < ApplicationController
-    def new
-        @friendship = Friendship.new
+
+    def index
+      @friends = current_user.friendships.all
     end
 
     def create
-    end
-
-    def show
+      @friendship = current_user.friendships.build(friend_id: params[:id], confirmed: false)
+      if @friendship.save
+        redirect_to users_path, notice: 'You have sent a friendship request!'
+      else
+        redirect_to users_path, alert: 'Friendship request Sent already!'
+      end
     end
 
     def destroy
-    end
-
-    def update
-    end
-
-
-    private 
-    def friendship_params
-        params.require(friendship).permit(:user_id, :friend_id, :confirmed)
+      @friendship = Friendship.find_by(params[:user_id], friend_id: params[:id])
+      if @friendship
+        @friendship.destroy
+        redirect_to users_path, notice: 'Friend has been removed'
+      else
+        redirect_to root_path, alert: 'You are not allowed to do this'
+      end
     end
 
 end
