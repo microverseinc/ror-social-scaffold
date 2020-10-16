8c9103a0b2bf14cc76b3d952c08ee0a64683495e
Zubenna <emelunna@gmail.com>
2020-10-16 16:50:07 +0100

    Added user and friendship helper methods

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,12 @@
 module FriendshipsHelper
-end
+    def friendship_button(friend)
+      if friend.friend_requests.any?(current_user)
+        link_to 'Pending request', '#', class: 'profile-link btn btn-secondary'
+      elsif !current_user.friend?(friend)
+        link_to 'Add friend', request_friendship_user_path(friend.id), method: :post,                                                                    class: 'profile-link btn btn-secondary'
+      else
+        link_to 'Unfriend', unfriend_user_path(friend), method: :delete,                                                   class: 'profile-link btn btn-secondary'
+      end
+    end
+  end
+  
