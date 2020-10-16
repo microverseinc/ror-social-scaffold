d9ee920562e1472fa754b682f458f5d4a8768fbd
Umair Arshad <Umair6622@gmail.com>
2020-10-16 23:22:03 +0500

    minor changes

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -5,12 +5,10 @@ module FriendshipsHelper
         class: 'profile-link btn btn-secondary'
       elsif !current_user.friend?(friend)
         link_to 'Add friend', 
-        request_friendship_user_path(friend.id), 
-        method: :post, class: 'profile-link btn btn-secondary'
+        request_friendship_user_path(friend.id), method: :post, class: 'profile-link btn btn-secondary'
       else
         link_to 'Unfriend', 
-        unfriend_user_path(friend), 
-        method: :delete, class: 'profile-link btn btn-secondary'
+        unfriend_user_path(friend), method: :delete, class: 'profile-link btn btn-secondary'
       end
     end
 end
