02058fce3148d300ed8b70b9560e743d44d3f7e6
Zubenna <emelunna@gmail.com>
2020-10-18 17:59:12 +0100

    Removed unnecessary space in post_spec.rb

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'Post', type: :feature do
   it 'User can make and save comment' do
     User.create(name: 'Ibe Precious', email: 'i.email@example.com', password: 'secretpass',
                 password_confirmation: 'secretpass')
-    visit  visit new_user_session_path
+    visit visit new_user_session_path
     fill_in 'user_email', with: 'i.email@example.com'
     fill_in 'user_password', with: 'secretpass'
     find("input[type='submit']").click
