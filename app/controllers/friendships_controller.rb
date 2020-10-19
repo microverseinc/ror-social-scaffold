248bc652fb0f21a0f994708c1783d3d43ac9d23a
Gui Recordon <guilherme.recordon@gmail.com>
2020-10-19 12:56:11 -0300

    first change milestone_5

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,17 @@
+class FriendshipsController < ApplicationController
+  def new
+    # @friendship = Friendship.new
+  end
+
+  def create
+    friend = User.find(params[:friend_id])
+    friendship = current_user.friendships.build(friend_id: friend.id, confirmed: true)
+    
+    if friendship.save
+      flash.now[:success] = "Invitation sent"
+    end
+      redirect_to friend
+  end
+
+
+end
