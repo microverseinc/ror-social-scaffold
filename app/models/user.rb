fbb925af392b57d0b89a3a153b997d571723a730
Mahmoud Bakr <mbakr6821@gmail.com>
2020-10-19 11:45:44 +0200

    setup user model associations and add helper methods for them

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,31 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+
+
+  def friends
+    friends_array = friendships.map {|f| f.friend if f.confirmed}
+    friends_array + inverse_friendships.map {|f| f.user if f.confirmed}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map {|f| f.friend if !f.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map {|f| f.user if !f.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|f| f.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
