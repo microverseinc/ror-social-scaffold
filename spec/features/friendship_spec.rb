8f9c8c690bff5ae6e2f6186030bb25f9f75895e0
Zubenna <emelunna@gmail.com>
2020-10-20 16:29:50 +0100

    Refactored the friendship feature test code

--- spec/features/friendship_spec.rb
+++ spec/features/friendship_spec.rb
@@ -5,61 +5,18 @@ RSpec.describe 'Testing Friendship', type: :feature do
                 password_confirmation: 'secretpass')
     User.create(name: 'Eze Promise', email: 'e.email@example.com', password: 'password',
                 password_confirmation: 'password')
-    visit visit new_user_session_path
+    visit new_user_session_path
     fill_in 'user_email', with: 'e.email@example.com'
     fill_in 'user_password', with: 'password'
     find("input[type='submit']").click
 
     visit users_path
-    expect(page).to have_content('Ibe Precious')
-    expect(page).to have_content('Eze Promise')
   end
 
   it 'Should Add friend from any of the users listed' do
     click_link('Add friend', match: :first)
     expect(page).to have_content('You have sent a friendship request!')
     expect(page).to have_content('Pending Request')
-  end
-
-  it 'Can Accept a friend request' do
-    click_link('Add friend', match: :first)
     click_on 'Sign out'
-    visit new_user_session_path
-    fill_in 'user_email', with: 'i.email@example.com'
-    fill_in 'user_password', with: 'secretpass'
-    find("input[type='submit']").click
-    visit users_path
-    click_on 'Accept'
-    expect(page).to have_content('You are now friends')
-  end
-
-  it 'Can Reject a friend request' do
-    click_link('Add friend', match: :first)
-    click_on 'Sign out'
-    visit new_user_session_path
-    fill_in 'user_email', with: 'i.email@example.com'
-    fill_in 'user_password', with: 'secretpass'
-    find("input[type='submit']").click
-    visit users_path
-    click_on 'Reject'
-    expect(page).to have_content('Friend has been removed')
-  end
-  it 'Can Unfriend an existing friend' do
-    click_link('Add friend', match: :first)
-    click_on 'Sign out'
-    visit new_user_session_path
-    fill_in 'user_email', with: 'i.email@example.com'
-    fill_in 'user_password', with: 'secretpass'
-    find("input[type='submit']").click
-    visit users_path
-    click_on 'Accept'
-    click_on 'Sign out'
-    visit visit new_user_session_path
-    fill_in 'user_email', with: 'e.email@example.com'
-    fill_in 'user_password', with: 'password'
-    find("input[type='submit']").click
-    visit users_path
-    click_on 'Unfriend'
-    expect(page).to have_content('Friend has been removed')
   end
 end
