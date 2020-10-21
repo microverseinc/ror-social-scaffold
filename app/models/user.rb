ad319674af0b70dfaec134b3c5219360e2fd2c46
Abdullah Numan <anewman15@hotmail.com>
2020-10-21 19:45:49 +0600

    Create Friendship migration table and related User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
 
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
--- db/migrate/20201020145931_create_friendships.rb
+++ db/migrate/20201020145931_create_friendships.rb
@@ -1,7 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-
+      t.references :user, index: true, foreign_key: true
+      t.references :friend, index: true
+      t.boolean :confirmed
       t.timestamps
     end
   end
