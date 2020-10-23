22462afea633cf0ec9d765d45a266215accbf848
Abdullah Numan <anewman15@hotmail.com>
2020-10-23 16:31:36 +0600

    Create controller variables and add friendship buttons to views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,14 @@ class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    # byebug
     @users = User.all
-    @friendship = current_user.friendships.new(friend_id: params[:friend_id], confirmed: false)
+    @friendship = current_user.friendships.build(friend_id: params[:friend_id], confirmed: false)
+
+    if @friendship.save
+      redirect_to user_path(id: params[:friend_id]), notice: 'Friend request sent.'
+    else
+      redirect_to users_path, error: 'Request not sent. Something is wrong!'
+    end
   end
 end
--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,3 @@
 module FriendshipsHelper
+
 end
