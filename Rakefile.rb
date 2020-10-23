a9cb46a14a6395fee2bd3a233114e0ffddaeccc7
Zubenna <emelunna@gmail.com>
2020-10-24 00:00:23 +0100

    Added rake file

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,5 +2,6 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require_relative 'config/application'
+# require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
