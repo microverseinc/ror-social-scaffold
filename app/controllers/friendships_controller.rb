18f212ac5cca1e0aa7bf444ccf7231f9b00bc0b3
Zubenna <emelunna@gmail.com>
2020-10-24 07:46:22 +0100

    modified database.yml, upgraded rails to v6.0.3.3

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -13,12 +13,8 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    @friendship = Friendship.find_by(params[:user_id], friend_id: params[:id])
-    if @friendship
-      @friendship.destroy
-      redirect_to users_path, notice: 'Friend has been removed'
-    else
-      redirect_to root_path, alert: 'You are not allowed to do this'
-    end
+    @friendship = current_user.confirmed_friendships.find_by(friend_id: params[:id])
+    @friendship.destroy
+    redirect_to users_path, notice: 'Friend has been removed'
   end
 end
