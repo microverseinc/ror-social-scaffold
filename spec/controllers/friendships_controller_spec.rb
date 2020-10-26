afd299f6b3628ffc380cd72071e5de3e15d15c2f
Abdullah Numan <anewman15@hotmail.com>
2020-10-26 16:25:13 +0600

    Update friendship controller test

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -3,13 +3,26 @@ require 'rails_helper'
 RSpec.describe FriendshipsController, type: :controller do
   describe 'POST #create' do
     context 'with valid parameters' do
-      it 'redirects to user login page' do
+      it "redirects to friend's page" do
         user = build(:user)
         sign_in user
         post :create, params: { user_id: user.id, friend_id: 2 , confirmed: false }
-        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to redirect_to(user_path(id: 2))
         expect(response).to have_http_status(302)
       end
     end
   end
+
+  # describe 'PUT #update' do
+  #   context 'with valid parameters' do
+  #     it "redirects to friend's page" do
+  #       user = build(:user)
+  #       sign_in user
+  #       friendship = build(:friendship)
+  #       put :update, params: { id: friendship.id, confirmed: true }
+  #       expect(response).to redirect_to(new_user_session_path)
+  #       expect(response).to have_http_status(302)
+  #     end
+  #   end
+  # end
 end
--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :friendship do
-    # user_id { 1 }
-    # friend_id { 2 }
-    # confirmed { true}
+    user_id { 1 }
+    friend_id { 2 }
+    confirmed { false }
   end
 end
