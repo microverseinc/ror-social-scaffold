76fcefc7af3fde80d404f8497b149c1e2ba9a7f1
ezeilo-su <ezeilo.su@gmail.com>
2020-10-28 20:05:02 +0100

    Add default scope to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable
   # :validatable
 
+  default_scope -> { order(created_at: :desc) }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
