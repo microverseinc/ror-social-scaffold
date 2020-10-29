bb36761bcf2f4a264e70256960a3215ad6216fcc
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-10-29 17:45:08 -0400

    Add validations tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,13 +8,25 @@ RSpec.describe User, type: :model do
   #   end
   # end
 
-  context 'Association test for event models' do
-    it 'should have many attendeees' do
+  context 'Association test for User model' do
+    it 'should have many friendships' do
       t = User.reflect_on_association(:friendships)
       expect(t.macro).to eq(:has_many)
     end
-    it 'should have many invitations' do
+    it 'should have many inverse_friendships' do
       t = User.reflect_on_association(:inverse_friendships)
+      expect(t.macro).to eq(:has_many)
+    end
+    it 'should have many posts' do
+      t = User.reflect_on_association(:posts)
+      expect(t.macro).to eq(:has_many)
+    end
+    it 'should have many comments' do
+      t = User.reflect_on_association(:comments)
+      expect(t.macro).to eq(:has_many)
+    end
+    it 'should have many likes' do
+      t = User.reflect_on_association(:likes)
       expect(t.macro).to eq(:has_many)
     end
   end
