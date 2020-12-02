2140bc6376333028ccc206d06949d4c64c4673f4
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-02 14:59:41 +0500

    fix linters issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  # devise_for :users
+  devise_for :users
 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
