8ac4e4e155c91df6e4aa05bf5da216ec66a73a22
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-04 20:32:20 +0500

    add friendship view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  
   root 'posts#index'
   devise_for :users
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
-  resources :comments, only: [:create]
-  resources :likes, only: [:create, :destroy]
-  resources :friendships, only:[:create]
+    resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
+
+  resources :friendships , only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
