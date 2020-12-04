5e00a53ceda0973acb9ddeb36916f334674f7932
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-12-04 11:51:38 +0100

    improve styling, add hirb, add freinship controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,27 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  has_many :pending_friendships, -> { where confirmed: nil }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_requests, through: :pending_friendships, source: :friend
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.status}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
+    friends_array.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
