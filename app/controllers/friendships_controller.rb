75ed5b2e38aefe2d33922cdb1113e3394fefa378
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-08 13:42:49 +0500

    user can send request once and receive signle request from a certain user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,6 +6,6 @@ class FriendshipsController < ApplicationController
 
   def create
     friend_id = params[:friend_id]
-    @friendship = Friendship.create(user_id: current_user.id, friend_id: friend_id)
+    @friendship = Friendship.find_or_create_by(user_id: current_user.id, friend_id: friend_id)
   end
 end
