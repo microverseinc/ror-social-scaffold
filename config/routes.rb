153e9eeea94c94518de0060e5a619372dba57ae0
AlejandroToledo <atoledofr@gmail.com>
2020-12-08 18:07:55 -0500

    fixlinter issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-
   root 'posts#index'
 
   devise_for :users
 
-  resources :users, only: [:index, :show] do
+  resources :users, only: %i[index show] do
     resources :frienships, only: [:index]
     member do
       put 'add_friend', to: 'users#add_friend'
       post 'request_friendship', to: 'friendships#create'
       delete 'unfriend', to: 'friendships#destroy'
-      end
+    end
   end
-  resources :posts, only: [:index, :create] do
+  resources :posts, only: %i[index create] do
     resources :comments, only: [:create]
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: %i[create destroy]
   end
 
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
