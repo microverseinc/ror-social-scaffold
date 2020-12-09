c7298a53ac68f43669aa07754aeb2c35182e239f
AlejandroToledo <atoledofr@gmail.com>
2020-12-09 15:19:42 -0500

    fix rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-
   root 'posts#index'
 
   devise_for :users
 
-  resources :users, only: [:index, :show] do
-  resources :friendships, only: [:index]
+  resources :users, only: %i[index show] do
+    resources :friendships, only: [:index]
     member do
       put 'add_friend', to: 'users#add_friend'
       post 'request_friendship', to: 'friendships#create'
       delete 'unfriend', to: 'friendships#destroy'
     end
-  end  
-    resources :posts, only: [:index, :create] do
-      resources :comments, only: [:create]
-      resources :likes, only: [:create, :destroy]
-    end
+  end
+  resources :posts, only: %i[index create] do
+    resources :comments, only: [:create]
+    resources :likes, only: %i[create destroy]
+  end
 end
