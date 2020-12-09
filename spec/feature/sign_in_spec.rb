eccd7bac873831349d105959304945c4f11c7715
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-09 18:48:50 +0500

    add feature specs for sign in and sign out

---
+++ spec/feature/sign_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+RSpec.describe 'Login a user', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'abc', email:'a@a.com', password:'123456')
+  end
+  scenario 'valid inputs' do
+    visit user_session_path
+    fill_in 'user[email]', with: 'a@a.com'
+    fill_in 'user[password]', with: '123456'
+    click_on 'Log in'
+    visit root_path
+    expect(page).to have_content('Recent posts')
+  end
+
+  scenario 'Invalid inputs' do
+    visit user_session_path
+    fill_in 'user[email]', with: 'a@a.co'
+    fill_in 'user[password]', with: '123456'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+end
