2bf53000741e41c9a6273455c50292240bbe3abb
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-09 18:05:30 +0500

    add feature specs for friendship

---
+++ spec/feature/invite_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'Invite a user for friendship', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'somename', email: 'a@a.com', password:'123456')
+  end
+  scenario 'invite a user' do
+    visit users_path
+    page.should have_css('form input[type="submit"]')
+  end
+end
