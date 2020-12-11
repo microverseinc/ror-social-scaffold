af06b0011cdff382e1afb914b20442f881da97f2
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-11 19:32:13 +0500

    change destroy method to destroy both rows that a single friendship has

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,7 +6,8 @@ class FriendshipsController < ApplicationController
 
   def create
     friend_id = params[:friend_id]
-    @friendship = Friendship.find_or_create_by(user_id: current_user.id, friend_id: friend_id)
+    @friendship = Friendship.find_or_create_by(user_id: current_user.id, friend_id: friend_id, requestor_id: current_user.id)
+    @friendship = Friendship.find_or_create_by(user_id: friend_id, friend_id: current_user.id, requestor_id: current_user.id)
   end
 
   def update
@@ -15,7 +16,10 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    friendship = Friendship.find_by(user_id: params[:user_id], friend_id: params[:friend_id])
-    friendship.destroy
+    user_id = params[:user_id]
+    friend_id =  params[:friend_id]
+    requestor_id = current_user.id
+    friendship = Friendship.where(user_id == requestor_id).or(Friendship.where(friend_id == requestor_id))
+    friendship.destroy_all
   end
 end
