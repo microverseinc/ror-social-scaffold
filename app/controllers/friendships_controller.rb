fe093b95d04dbda3fe8a5278a647ec59c0e0562a
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-14 19:39:19 +0500

    update create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,13 +4,13 @@ class FriendshipsController < ApplicationController
 
   def create
     friend_id = params[:friend_id]
-    @friendship = Friendship.find_or_create_by(user_id: current_user.id, friend_id: friend_id, requestor_id: current_user.id)
-    @friendship = Friendship.find_or_create_by(user_id: friend_id, friend_id: current_user.id, requestor_id: current_user.id)
+    @friendship = Friendship.find_or_create_by(user_id: current_user.id, friend_id: friend_id)
+    @friendship = Friendship.find_or_create_by(user_id: friend_id, friend_id: current_user.id)
   end
 
   def update
     user = User.find(params[:user_id])
-    friend =  User.find(current_user.id)
+    friend = User.find(current_user.id)
     @friendship = Friendship.find_by_user_id_and_friend_id(user, friend)
     @friendship.confirmed = true
     @friendship.save
@@ -22,7 +22,7 @@ class FriendshipsController < ApplicationController
 
   def destroy
     user = User.find(params[:user_id])
-    friend =  User.find(params[:friend_id])
+    friend = User.find(params[:friend_id])
     Friendship.find_by_user_id_and_friend_id(user, friend).destroy
     Friendship.find_by_user_id_and_friend_id(friend, user).destroy
     redirect_to users_path
