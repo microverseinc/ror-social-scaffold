0dbf8016ee1f503233f25f8e483300db961e4b07
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-14 13:55:44 +0100

    add integration test to like and post model in rspec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Association Test' do
+    it 'belongs to users' do
+      expect(Like.reflect_on_association(:user).macro).to be :belongs_to
+    end
+
+    it 'belongs to posts' do
+      expect(Like.reflect_on_association(:post).macro).to be :belongs_to
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Association Test' do
+    it 'has many comments' do
+      expect(Post.reflect_on_association(:comments).macro).to be :has_many
+    end
+
+    it 'has many likes' do
+      expect(Post.reflect_on_association(:likes).macro).to be :has_many
+    end
+
+    it 'belongs to users' do
+      expect(Post.reflect_on_association(:user).macro).to be :belongs_to
+    end
+  end
 end
