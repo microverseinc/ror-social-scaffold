c1da95a508eda75a4fb3f092ce8ccfbf5715e98e
daviidy <yaodavidarmel@gmail.com>
2020-12-14 16:27:02 +0000

    adapt gems and db for local environment

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_02_04_165841) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
