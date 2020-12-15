e754e8b069c6cbe8359bbd860bb7b38aa886d92c
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-15 19:18:45 +0100

    add control logic to time-line mthd in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,10 +20,14 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    all_posts = Post.where(user: current_user.friends)
+    all_posts += current_user.posts
+    @timeline_posts ||= all_posts.sort { |a, b| b.created_at <=> a.created_at }
   end
 
   def post_params
     params.require(:post).permit(:content)
   end
 end
+
+    
