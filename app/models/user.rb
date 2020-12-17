59aadf5e2af8297330853c724a333ea6c772ae8a
Khristian Rojas <khristianrojas15@gmail.com>
2020-12-17 16:48:31 -0300

    Change confirmed method to status

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,17 +20,17 @@ class User < ApplicationRecord
 
    # Users who have yet to confirme friend requests
    def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    inverse_friendships.map{|friendship| friendship.user if !friendship.status}.compact
   end
 
   def confirm_friend(user)
     friendship = inverse_friendships.find{|friendship| friendship.user == user}
-    friendship.confirmed = true
+    friendship.status = true
     friendship.save
   end
 
