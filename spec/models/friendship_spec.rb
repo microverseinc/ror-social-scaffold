1517692e486df9e3b568d768ee6596d9e7251b76
Rocio01 <zrmartinezg@gmail.com>
2020-12-16 20:25:40 -0500

    Add frindships_spec.rb, tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -4,16 +4,17 @@ RSpec.describe Friendship, type: :model do
 
   context 'Associations' do
     
-    it 'Belong to User' do
-      t = Friendship.reflect_on_association(:user)
-      expect(t.macro).to eq(:belongs_to)
+    it 'Belongs to User' do
+      w = Friendship.reflect_on_association(:user)
+      expect(w.macro).to eq(:belongs_to)
     end
 
-    it ' Belong to Friend' do
-      t = Friendship.reflect_on_association(:friend)
-      expect(t.macro).to eq(:belongs_to)
+    it ' Belongs to Friend' do
+      w = Friendship.reflect_on_association(:friend)
+      expect(w.macro).to eq(:belongs_to)
     end
 
+   
 
   end
 end
