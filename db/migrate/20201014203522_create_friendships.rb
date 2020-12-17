dd1e3e1f2a55ff0d4274c8533871f563b8d8f3d8
AlejandroToledo <atoledofr@gmail.com>
2020-12-17 14:31:26 -0500

    add foreign key :true to friend references from friendship model

--- db/migrate/20201014203522_create_friendships.rb
+++ db/migrate/20201014203522_create_friendships.rb
@@ -2,7 +2,7 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true
+      t.references :friend, index: true, foreign_key: true
       t.boolean :confirmed
 
       t.timestamps
