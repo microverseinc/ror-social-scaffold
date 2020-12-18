7b113adea1b49ce0d11d3beae1e693d428158029
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2020-12-18 10:59:07 -0600

    Styling: Add basic styling to friend requests

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -2,17 +2,17 @@ module FriendshipsHelper
 
   def invitation_action_links(user)
     if current_user == user
-      content_tag(:span, 'Your profile')
+      content_tag(:span, 'Your profile', class: 'profile-link')
     elsif current_user.friend?(user)
-      content_tag(:span, 'Is your friend')
+      content_tag(:span, 'Is your friend', class: 'profile-link')
     elsif current_user.friend_requests.include?(user)
-      link_to('Accept invitation', friendship_path(user), method: :patch) +
+      link_to('Accept invitation', friendship_path(user), method: :patch, class: 'profile-link') +
         content_tag(:span, ' | ') +
-        link_to('Reject invitation', friendship_path(user), method: :delete)
+        link_to('Reject invitation', friendship_path(user), method: :delete, class: 'profile-link')
     elsif current_user.pending_friends.include?(user)
-      content_tag(:span, 'You already sent an invitation')
+      content_tag(:span, 'You already sent an invitation', class: 'profile-link')
     else
-      link_to 'Send friendship invitation', friendships_path(receiving_user_id: user.id), method: :post
+      (link_to 'Send friendship invitation', friendships_path(receiving_user_id: user.id), method: :post, class: 'profile-link')
     end
   end
 end
