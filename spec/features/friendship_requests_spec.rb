4f6ed1dcdc2a07feb765d8b56dacf0796205573a
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2020-12-18 17:18:13 -0600

    Test: Add tests for features and User model methods

--- spec/features/friendship_requests_spec.rb
+++ spec/features/friendship_requests_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Friend Requests ", type: :feature do
+
   before do
-    user = create(:user)
-    user2= create(:user)
+    @user = create(:user)
+    @user2 = create(:user)
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
     click_button 'Log in'
   end
 
@@ -24,10 +25,24 @@ RSpec.describe "Friend Requests ", type: :feature do
   end
 
   it 'Send a Friend Request from the User\'s personal page' do
-    visit users_path
-    click_link 'See Profile'
+    visit user_path(@user2)
     click_link 'Send friendship invitation'
     expect(page).to have_content('Friendship request sent')
     expect(page).to have_content('You already sent an invitation')
+  end
+
+  it 'Reject a friend request' do
+    visit users_path
+    click_link 'Send friendship invitation'
+    expect(page).to have_content('Friendship request sent')
+    expect(page).to have_content('You already sent an invitation')
+    click_link('Sign out')
+    visit new_user_session_path
+    fill_in 'Email', with: @user2.email
+    fill_in 'Password', with: @user2.password
+    click_button 'Log in'
+    visit friendships_path
+    click_link('Accept invitation')
+    expect(page).to have_content('Friend Added')
   end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -17,6 +17,60 @@ RSpec.describe User, type: :model do
     it { should have_many(:friendships).with_foreign_key('requesting_user_id') }
 
     it { should have_many(:friendship_requests).with_foreign_key('receiving_user_id').class_name('Friendship') }
+  end
+
+  describe '#Friendships' do
+    let(:sender) { create(:user) }
+
+    let(:receiver) { create(:user) }
+
+    let(:true_friendship) do
+      Friendship.new(
+        requesting_user: sender,
+        receiving_user: receiver,
+        request_status: true
+      )
+    end
+
+    let(:false_friendship) do
+      Friendship.new(
+        requesting_user: sender,
+        receiving_user: receiver,
+        request_status: false
+      )
+    end
+
+    it '#friends' do
+      true_friendship.save
+      expect(sender.friends).to include(receiver)
+    end
+
+    it '#friend?' do
+      true_friendship.save
+      expect(sender.friend?(receiver)).to be(true)
+    end
+
+    it '#pending_friends' do
+      false_friendship.save
+      expect(sender.pending_friends).to include(receiver)
+    end
+
+    it '#friend_request' do
+      false_friendship.save
+      expect(receiver.friend_requests).to include(sender)
+    end
+
+    it '#confirm_friend' do
+      false_friendship.save
+      receiver.confirm_friend(sender)
+      expect(sender.friend?(receiver)).to be(true)
+    end
+
+    it '#reject_friend' do
+      false_friendship.save
+      receiver.reject_friend(sender)
+      expect(sender.friend?(receiver)).to be(false)
+    end
 
   end
 end
