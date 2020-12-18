3699db1f652a8d1f911e10c559b095ea19ed2791
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-12-18 08:48:57 +0400

    attach bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,20 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
 end
