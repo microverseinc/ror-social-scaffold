41b4297dd6ea3403fbff15594de5f59e2feaa74a
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-12-23 08:37:36 +0400

    remove comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,19 +13,14 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  # confirm friends
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
 
-  # pending_friends
   has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
-  # friend_requests
   has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships, source: :user
-
-  # ------------------------------------
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
