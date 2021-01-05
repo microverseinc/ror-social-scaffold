364df442770ab0667bd73b02f8a05d49445995f4
Khristian Rojas <khristianrojas15@gmail.com>
2021-01-05 12:23:50 -0300

    Hide add friend for not duplicate friendship

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
   def add_friend(user)
-    if current_user.id == user.id || current_user.friend?(user) || current_user.not_friend?(user)
+    if current_user.id == user.id || current_user.friend?(user) || current_user.not_friend?(user) || user.friend?(current_user)
       nil
     else
       link_to 'Add Friend',
