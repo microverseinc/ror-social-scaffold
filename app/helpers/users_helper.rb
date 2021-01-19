f1bf70ae968893f45e13d3263d04b4b3325fbebf
Jocyline Magero <jmagero2017@gmail.com>
2021-01-19 20:19:53 +0300

    Add follow_form helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,18 @@ module UsersHelper
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
+
+  def follow_form
+    unless @user == current_user
+      if current_user.friend?(@user)
+        render 'unfollow' 
+      elsif @user.pending_friends.include?(current_user) 
+        render 'friendships/accept_request'
+      elsif current_user.pending_friends.include?(@user)
+        render 'friendships/cancel_request'
+      else
+      render 'follow'
+      end 
+    end 
+  end
 end
