bff9c3eb2cb4704acd53e985f7433c22c1502e6b
che <che.blancoo58@gmail.com>
2021-01-20 14:19:35 +0100

    Add rspec to verify if user friends include other user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,18 @@ RSpec.describe User, type: :model do
     end
   end
   context 'creating and saving friendships' do
-    it ' ensures name not blank' do
-      u1 = User.create(name: 'one', email: 'one@user.com', password: '1234567')
-      u2 = User.create(name: 'two', email: 'two@user.com', password: '1234567')
+    let(:u1){ User.create(name: 'one', email: 'one@user.com', password: '1234567')}
+    let(:u2){ User.create(name: 'two', email: 'two@user.com', password: '1234567')}
+    it 'testing user friendship and inverse frienship' do
       u1.friendships.create(friend_id: u2.id)
       expect(u1.friendships).not_to  eql(u2.friendships)
     end
+    it 'expects confirm friendship to make users friends' do
+      u1.friendships.create(friend_id: u2.id)
+      u2.confirm_friend(u1)
+      expect(u1.friends.include?(u2)).to eql(true)
+    end
+
   end
   
 end
