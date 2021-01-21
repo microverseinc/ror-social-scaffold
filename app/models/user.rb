4d2a4f8f408feb8c2ee50f34e1939834359bd9d9
smcommits <sm.anas@outlook.com>
2021-01-21 15:44:08 +0530

    Implement logic to manupulate friend requests
    - Added logic to display all the friends by adding instance method in
      User model which returns the combined record of friends who accepted
      the user friendship requests and the requests that user has accepted
    
    - Added logic to accepted friendship by implementing an instance method
      in user model that changes the status of any friendship instance to
      confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,30 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
 
+  scope :accepted_requests, -> {sent_friend_requests.where("status = ?", :accepted)}
+
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :sent_friend_requests, class_name: 'Friendship', foreign_key: :user_id
   has_many :recieved_friend_requests, class_name: 'Friendship', foreign_key: :friend_id
+
+
+  def friends 
+    accepted_requests = sent_friend_requests.map { |request| request.friend if request.status = :accepted }
+    requests_accepted = recieved_friend_requests.map { |request| request.user if request.status = :accepted }
+
+    accepted_requests + requests_accepted
+  end
+
+  def pending_friend_requests
+    recieved_friend_requests.where('status: ?', :pending )
+  end
+
+  def accept_request(user)
+    request = recieved_friend_requests.where(user_id: user.id)
+    request.update(status: :accepted)
+  end
+
 
 end
