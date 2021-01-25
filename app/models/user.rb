8b877c248acc2f55caf52344be708e9ee1dfaae7
delicelydia <Delydia84@gmail.com>
2021-01-25 12:20:23 +0200

    Add friendship model
    In the friendship model i added
    
    - User and friend references
    - Status as boolean
    
    In the user model i added
    
    - User has many friendships association
    - User has many inverse relationships
    - friend, pending_friends, and friend_request methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", 
+  :foreign_key => "friend_id"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -9,4 +11,23 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def friend
+    friend_arr = friendships.map{|friendship| friendship.friend 
+    if friendship.status}
+
+      friend_arr + inverse_friendships.map{|friendship| 
+      friendship.user if friendship.status}
+      friend_arr.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend 
+    if !friendship.status}.compact
+  end
+
+  def friend_request
+    inverse_friendships.map{|friendship| friendship.user
+    if !friendship.status }.compact
+  end
 end
