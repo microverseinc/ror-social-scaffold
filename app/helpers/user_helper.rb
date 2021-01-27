a5d2148ba145ccbb95ccfd76f2c1b9132d98c6ef
delicelydia <Delydia84@gmail.com>
2021-01-27 18:29:43 +0200

    Add view files
    - Added _user.html.erb in the user view  folder
    - Added index.html.erb in the user view  folder
    - Added pending.html.erb in the user view  folder
    - Added show.htm.erb in the user view  folder
    
    Added Routes in the route.rb

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,24 +1,24 @@
 module UserHelper
   def pending_friends_list
     if @pending_friends.empty?
-      notice: 'No pending friends'
+      content_tag :p, 'No pending friends.'
     else
-      content_tag :ul, class : "user-lists" do
-        @pending_friends.each do |f|
+      content_tag :ul, class: 'users-list' do
+        @pending_friends.each do |friend|
           link = link_to(friend.name.to_s, user_path(friend), class: 'profile-link')
           content = content_tag(:span, link)
           concat content_tag(:li, content)
         end
       end
+    end
   end
-end
-  def invited_links(user)
+  def invite_links(user)
     if current_user.friend_requests.include?(user)
       concat link_to 'Accept', confirm_request_path(user), class: 'profile-link accept'
       concat ' '
       concat link_to 'Reject', reject_request_path(user), class: 'profile-link reject'
     elsif !current_user.pending_friends.include?(user) && current_user != user && !current_user.friend?(user)
-      concat link_to 'Invite to friendship', request_friendship_path(user), class: 'profile-link invite'
+      concat link_to 'Request Friendship', request_friendship_path(user), class: 'profile-link invite'
   end
  end
 end
