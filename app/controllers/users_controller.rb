364e0f1eeb43b1167a7223f2629b16f845c1eeb6
delicelydia <Delydia84@gmail.com>
2021-02-01 19:44:20 +0200

    Implimented feedback
    - removed duplicated request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,20 +12,20 @@ class UsersController < ApplicationController
   end
 
   def request_friendship
-    @user = User.find(params[:id])
-    Friendship.create(user_id: current_user.id, friend_id: @user.id, confirmed: false)
+    user = User.find(params[:id])
+    Friendship.create(user_id: current_user.id, friend_id: user.id, confirmed: false)
     redirect_to root_path, notice: 'Friendship invite sent'
   end
 
   def confirm_request
-    @user = User.find(params[:id])
-    current_user.confirm_friend(@user)
-    redirect_to @user, notice: 'friend request accepted'
+    user = User.find(params[:id])
+    current_user.confirm_friend(user)
+    redirect_to user, notice: 'friend request accepted'
   end
 
   def reject_request
-    @user = User.find(params[:id])
-    current_user.reject_friendship(@user)
+    user = User.find(params[:id])
+    current_user.reject_friendship(user)
     redirect_to root_path, notice: 'request rejected'
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,10 @@ class User < ApplicationRecord
     friendships.map { |friendship| friendship.friend if friendship.confirmed }.compact
   end
 
-  # Users who have yet to confirme friend requests
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
-  # Users who have requested to be friends
   def friend_requests
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
