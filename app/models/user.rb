223d5933e93000abcd6cc8a16168b6b41bf0b9af
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-02-08 14:28:49 +0200

    fix confirm friend method error

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,9 @@ class User < ApplicationRecord
   end
 
   def confirm_friend(user)
-    inverse_friendships.find { |friendship| friendship.user == user }
-    friendsship.confirmed = true
-    friendship.save
+    friendships = inverse_friendships.find { |friendship| friendship.user == user }
+    friendships.confirmed = true
+    friendships.save
   end
 
   def friend?(user)
