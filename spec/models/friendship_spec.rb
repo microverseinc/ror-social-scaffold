7bb894d26d496535686843144c1ab7b29c0e3a40
abeidahmed <abeidahmed92@gmail.com>
2021-02-10 15:14:06 +0530

    Fix rubocop errors

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe Friendship, type: :model do
   end
 
   describe 'validations' do
-    it { is_expected.to define_enum_for(:status).with_values(accepted: 'accepted', pending: 'pending').backed_by_column_of_type(:string) }
+    it { is_expected.to define_enum_for(:status).with_values(accepted: 'accepted', pending: 'pending').backed_by_column_of_type(:string) } # rubocop:disable Layout/LineLength
   end
 end
