ec880d5058f8238439d19f9229de74c211c873ae
abeidahmed <abeidahmed92@gmail.com>
2021-02-11 15:05:01 +0530

    Test post scope for displaying posts by user and user's friends

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,4 +16,37 @@ RSpec.describe Post, type: :model do
 
     it { is_expected.to have_many(:comments) }
   end
+
+  describe '::by_me' do
+    it 'should list all the posts written by the user' do
+      post = create(:post)
+
+      expect(Post.includes(:user).by_me(post.user).map(&:id)).to match_array([post.id])
+    end
+  end
+
+  describe '::by_my_friends' do
+    it 'should list all the posts written by my friends' do
+      user = create(:user)
+      friend = create(:user)
+      friend.send_friend_request_to(user)
+      user.accept_friend_request_of(friend)
+      post = create(:post, user: friend)
+
+      expect(Post.includes(:user).by_my_friends(user).map(&:id)).to match_array([post.id])
+    end
+  end
+
+  describe '::by_known_people' do
+    it 'should list all the posts written by me and my friends' do
+      user = create(:user)
+      friend = create(:user)
+      friend.send_friend_request_to(user)
+      user.accept_friend_request_of(friend)
+      post = create(:post, user: friend)
+      another_post = create(:post, user: user)
+
+      expect(Post.includes(:user).by_known_people(user).map(&:id)).to match_array([post.id, another_post.id])
+    end
+  end
 end
