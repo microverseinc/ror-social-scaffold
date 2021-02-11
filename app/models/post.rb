69c9114bfdf8c1aba5286b4bfe281d46a104f614
abeidahmed <abeidahmed92@gmail.com>
2021-02-11 15:00:09 +0530

    Show posts by me and from my friend list

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   belongs_to :user
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  scope :by_me, ->(user) { where(user_id: user.id) }
+  scope :by_my_friends, ->(user) { where(user_id: user.friend_lists.pluck(:id)) }
+  scope :by_known_people, ->(user) { by_me(user).or(by_my_friends(user)) }
+
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
