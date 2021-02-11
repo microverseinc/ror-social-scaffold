9723689540ffe074a26d926f725311d33a29614c
abeidahmed <abeidahmed92@gmail.com>
2021-02-11 14:35:14 +0530

    Fix request actionable button url page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,7 +5,7 @@ class FriendshipsController < ApplicationController
     @user = User.find(params[:user_id])
     current_user.send_friend_request_to(@user)
 
-    redirect_back fallback_location: root_path, notice: 'Accepted friend request'
+    redirect_back fallback_location: root_path, notice: 'Invited to friend request'
   end
 
   def destroy
--- spec/features/request_actionable_spec.rb
+++ spec/features/request_actionable_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'RequestActionables', type: :feature do
-  it 'should accept the friend request' do
+  it 'should send the friend request' do
     user = create(:user)
-    another_user = create(:user)
-    another_user.send_friend_request_to(user)
+    create(:user)
     feature_sign_in(user)
 
     visit users_path
-    click_button 'Accept'
+    click_button 'Invite'
 
-    expect(page).to have_text('Accepted friend request')
+    expect(page).to have_text('Invited to friend request')
   end
 
   it 'should be able to reject the friend request' do
