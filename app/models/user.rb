701c3c90b6d2bd12dd2dd1d015bc4eb6a1a86025
helman101 <helmanandresortegon@gmail.com>
2021-02-12 10:51:25 +0100

    connect likes model to the users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
