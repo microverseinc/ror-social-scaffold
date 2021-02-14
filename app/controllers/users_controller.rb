ff0c2ea9a93e2514e8b19919b2e4cb2f94d32766
abeidahmed <abeidahmed92@gmail.com>
2021-02-14 13:34:10 +0530

    Refactor mutual_friends and pending_invitations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @mutual_friends = current_user.mutual_friends_with(@user)
+    @pending_invitations = @user.pending_friend_lists
   end
 end
