63ecb2d7da334cf19fa450a3301693754eb9dde9
helman101 <helmanandresortegon@gmail.com>
2021-02-16 11:57:39 +0100

    add helper methods for friendship, and be able to send a friend request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,33 @@
 class FriendshipsController < ApplicationController
+  include FriendshipsHelper
+
   def create
-    @friend
+    friendship = Friendship.new(requestor_id: params[:requestor_id], requested_id: params[:requested_id], status: false)
+    if friendship.save
+      redirect_to users_path, notice: 'You send a friend request'
+    else
+      redirect_to users_path, alert: 'You already send a friend request to this user'
+    end
+  end
+
+  def update
+    new_friendship = Friendship.new(requestor_id: current_user.id, requested_id: params[:requested_id], status: true)
+    friendship = Friendship.find(params[:id])
+    friendship.status = true
+    if new_friendship.save && friendship.save
+      redirect_to users_path, notice: 'Now you\'re friends'
+    else
+      redirect_to users_path, alert: 'This user is already your friend'
+    end
+  end
+
+  def destroy
+    friendship = Friendship.find(params[:id])
+    if friendship
+      friendship.destroy
+      redirect_to users_path, notice: current_user.id == params[:requested_id]? 'You reject this friendship' : 'You cancel the friend request'
+    else
+      redirect_to users_path, alert: 'You cannot reject this user'
+    end
   end
 end
--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,44 @@
 module FriendshipsHelper
+  def friendship_exist?(user, friend)
+    Friendship.where(requestor_id: user, requested_id: friend, status: false).first
+  end
+
+  def friendship(user, friend)
+    Friendship.where(requestor_id: user, requested_id: friend).first
+  end
+
+  def status?(user, friend)
+    friendship = Friendship.where(requestor_id: user, requested_id: friend).first
+    return false if friendship.nil?
+
+    friendship.status
+  end
+
+  def requestor?(requestor)
+    current_user.requestors.exists?(requestor)
+  end
+
+  def make_request(current, user)
+    if current != user
+      if friendship_exist?(current, user)
+        'Waiting for friend'
+      elsif status?(current, user)
+        'You are friends'
+      elsif !friendship_exist?(current, user)
+        link_to 'Friend request', friendships_path(requestor_id: current, requested_id: user), method: :create
+      end
+    end
+  end
+
+  def accept_request(current, user)
+    if requestor?(user) && !status?(current, user)
+      link_to 'Accept request', friendship_path(id: friendship(user, current).id, requested_id: user), method: :patch
+    end
+  end
+
+  def cancel_request(current, user)
+    if requestor?(user) && !status?(current, user)
+      link_to 'Reject request', friendship_path(id: friendship(user, current).id), method: :delete
+    end
+  end
 end
