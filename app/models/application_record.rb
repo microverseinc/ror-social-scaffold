0d3c32021322bd1a9e999846d69de8f9bfecc0fa
helman101 <helmanandresortegon@gmail.com>
2021-02-16 15:58:52 +0100

    fix approved scope

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :approved, -> { where('status = ?', true) }
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,4 @@ class Friendship < ApplicationRecord
   belongs_to :requestor, class_name: 'User'
   belongs_to :requested, class_name: 'User'
 
-  scope :approved, -> { where('status = ?', true) }
 end
