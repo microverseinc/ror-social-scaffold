f85e19de44a50189f4a59a95aba423710031aff6
Naqhid <mnaqhid@gmail.com>
2021-02-17 12:05:44 +0530

    add second-milestone-stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,25 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friends
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
+
+  def pending?(user)
+    pending_friends.include?(user) || friend_requests.include?(user)
+  end
 end
