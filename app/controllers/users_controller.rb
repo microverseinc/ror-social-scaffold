6ff10129d21a546000240f6cfcf2ee4a3addacdb
helman101 <helmanandresortegon@gmail.com>
2021-02-17 14:48:13 +0100

    add that only friends can see posts in user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  helper_method :are_friends?
 
   def index
     @users = User.all
@@ -8,5 +9,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+  end
+
+  def are_friends?(user, friend, posts)
+    if Friendship.exists?(requestor_id: user,
+                          requested_id: friend) && Friendship.exists?(requestor_id: friend,
+                                                                      requested_id: user) || user == friend
+      render posts
+    else
+      'Only friends can see posts'
+    end
   end
 end
