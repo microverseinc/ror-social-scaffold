e8812ac68b29843954f94401380bb6ea7811f2d8
wandji20 <wbertrand10@gmail.com>
2021-02-23 17:29:35 +0100

    Add another way to verify existence before creating a new Friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,9 +4,26 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @new_friendship = Friendship.where( 'friend_id = ? AND user_id=?', params[:friend_id], current_user.id).or(Friendship.where('friend_id = ? AND user_id = ?', current_user.id, params[:friend_id]))
+    # @new_friendship = Friendship.where( 'friend_id = ? AND user_id=?', params[:friend_id], current_user.id).or(Friendship.where('friend_id = ? AND user_id = ?', current_user.id, params[:friend_id]))
 
-    if @new_friendship.count.zero?
+    # if @new_friendship.count.zero?
+    #   @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    #   if @friendship.save
+    #     redirect_to users_path,
+    #     notice: 'Sent Friend Request'
+    #   else
+    #     redirect_to users_path,
+    #     alert: 'Friend Request Not Sent'
+    #   end
+
+    # else
+    #    redirect_to users_path,
+    #   alert: 'Friendship is Pending'
+    # end
+
+    @friendship = current_user.friendships.find_by_friend_id(params[:friend_id])
+    @inverse_friendship = current_user.inverse_friendships.find_by_user_id(params[:friend_id])
+    if !(@friendship || @inverse_friendship)
       @friendship = current_user.friendships.build(friend_id: params[:friend_id])
       if @friendship.save
         redirect_to users_path,
@@ -17,10 +34,10 @@ class FriendshipsController < ApplicationController
       end
 
     else
-       redirect_to users_path,
-      alert: 'Friendship is Pending'
+        redirect_to users_path,
+        alert: 'Friendship is Pending'
     end
-    
+
   end
 end
 
