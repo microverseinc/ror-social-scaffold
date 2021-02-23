ddd508845a7bae5698924f994bee0fe3cffcf15c
wandji20 <wbertrand10@gmail.com>
2021-02-23 22:35:36 +0100

    Update user show querries

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
-  belongs_to :user, class_name: 'User'
-  belongs_to :friend, class_name: 'User'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
   scope :confirmed, -> { where('status = true') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :friendships
-  has_many :friends, through: :friendships
-  has_many :inverse_friendships, class_name: 'Friendship'
+  has_many :friendships, foreign_key: 'user_id'
+  has_many :friends, class_name: 'User', through: :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :inverse_friends, through: 'inverse_friendships', source: 'user'
 
  
