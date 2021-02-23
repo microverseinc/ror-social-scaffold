f06241b7d660c3162c53f8ef78b9e1a110946a7f
wandji20 <wbertrand10@gmail.com>
2021-02-23 16:35:46 +0100

    Add check before friendship create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,13 +4,23 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
-    if @friendship.save
-      redirect_to users_path,
-      notice: 'Sent Friend Request'
+    @new_friendship = Friendship.where( 'friend_id = ? AND user_id=?', params[:friend_id], current_user.id).or(Friendship.where('friend_id = ? AND user_id = ?', current_user.id, params[:friend_id]))
+
+    if @new_friendship.count.zero?
+      @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+      if @friendship.save
+        redirect_to users_path,
+        notice: 'Sent Friend Request'
+      else
+        redirect_to users_path,
+        alert: 'Friend Request Not Sent'
+      end
+
     else
-      redirect_to users_path,
-      alert: 'Friend Request Not Sent'
+       redirect_to users_path,
+      alert: 'Friendship is Pending'
     end
+    
   end
 end
+
