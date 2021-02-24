c47020b64b4a3667e80dada9247ba7f065a2a9b3
taiwo2 <adetonat@ymail.com>
2021-02-24 06:54:43 +0100

    Add comment spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject(:comment) { build(:comment) }
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:post) }
+
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_length_of(:content).is_at_most(200).with_message('200 characters in comment is the maximum allowed.') } # rubocop:disable Layout/LineLength
+  end
+end
