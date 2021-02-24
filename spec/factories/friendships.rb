bf73de3488e90f621880e40e170dfc79166c4d6e
taiwo2 <adetonat@ymail.com>
2021-02-24 07:08:24 +0100

    Add friendships factory

---
+++ spec/factories/friendships.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory :friendship do
+      requester
+      receiver
+      status { 'accepted' }
+  
+      trait :pending do
+        status { 'pending' }
+      end
+    end
+  end
