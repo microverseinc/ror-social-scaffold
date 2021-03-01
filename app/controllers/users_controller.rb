2bd683267bcc557c8b6f4e05198e3b7370b556d4
helciodev <helcioandre@live.com>
2021-03-01 13:30:06 +0100

    Remove comments from lines of code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,11 @@ class UsersController < ApplicationController
     @pending_friendships = current_user.inverse_friendships
     @sent_friendships = current_user.friendships.unconfirmed
 
-    # @friends = current_user.friendships.confirmed + current_user.inverse_friendships.confirmed
-
-    # This array is made up of user.name from inverse_friendships and friend.name from
-
-    # @friends_array = current_user.all_friends
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
 
     @inverse_friends = current_user.inverse_friendships.confirmed.map(&:user)
     @friends = current_user.friendships.confirmed.map(&:friend)
 
-    # @friends = (@inverse_friends + @friends).compact
-    # @friends = all_friends
   end
 end
