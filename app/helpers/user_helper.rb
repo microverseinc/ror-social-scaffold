bd23e37f1531c2bff0068cc52bb616310b766bcc
wandji20 <wbertrand10@gmail.com>
2021-03-02 18:40:59 +0100

    Update friend helper method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,16 +1,27 @@
 module UserHelper
+  def all_friends
+    @inverse_friends = current_user.inverse_friendships.confirmed.map(&:user)
+    @friends = current_user.friendships.confirmed.map(&:friend)
+
+    (@inverse_friends + @friends).compact
+  end
+
+  def friend(user)
+    current_user.friendships.where('friend_id = ?', user.id)
+      .or(current_user.inverse_friendships.where('user_id = ?', user.id))
+  end
+
   def confirmed_friend(user)
     friendship = friend(user)
     friendship.confirmed.count.positive? ? friendship.first : nil
   end
 
-  def friend(user)
-    @friends = current_user.friendships.where('friend_id = ?', user.id)
-      .or(current_user.inverse_friendships.where('user_id = ?', user.id))
-
-    if @friends.count.zero?
+  def invite_friendship(user)
+    if friend(user).count.zero?
       link_to('Invite to friendship', friendships_path(friend_id: user),
               method: :post, class: 'btn btn-info w-25')
+    elsif friend(user).first.status == false
+      link_to('Request Pending', user_path(user), class: 'btn btn-info w-25')
     end
   end
 end
