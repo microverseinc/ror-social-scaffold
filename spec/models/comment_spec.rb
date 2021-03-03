8c724369fbea06e634013f85d15a60fde586113a
helciodev <helcioandre@live.com>
2021-03-03 14:13:17 +0100

    Fix error on comment validation test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,13 +2,59 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   context "validations tests" do
+   
     it 'ensures content presence' do
-      comment = Comment.new().save
-      expect(comment).to eq(false) 
+      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user.save
+
+      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post.user = user
+      post.save
+
+      comment = Comment.new()
+      comment.post = post
+      comment.user = user
+
+      expect(comment.valid?).to eq(false) 
     end
+
+    it 'ensures content presence' do
+      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user.save
+      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post.user = user
+      post.save
+      comment = Comment.new(content:'lorem ipsum')
+      comment.post = post
+      comment.user = user
+      comment.save
+      expect(comment.valid?).to eq(true)  
+    end
+
     it 'ensures content lenght to be maximum 200' do
-      comment = Comment.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vitae bibendum sapien.').save
-      expect(comment).to eq(false) 
+      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user.save
+      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post.user = user
+      post.save
+      comment = Comment.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vitae bibendum sapien.')
+      comment.post = post
+      comment.user = user
+      comment.save
+      expect(comment.valid?).to eq(false) 
+    end
+
+    it 'ensures content lenght to be maximum 200' do
+      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user.save
+      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post.user = user
+      post.save
+      comment = Comment.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vit.')
+      comment.post = post
+      comment.user = user
+      comment.save
+      expect(comment.valid?).to eq(true) 
     end
   end
   
