b3b4451a1fb834359e0e34259446ce4b11291d0b
helciodev <helcioandre@live.com>
2021-03-03 14:36:52 +0100

    Delete unused file

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context "validations tests" do
    let(:user_params) { { name:'user', email:'user@email.com', password:'UseRQWERty' } }
+   let(:post_params) { { content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita' } }
 
     it 'ensures content presence' do
       user = User.new(user_params)
       user.save
 
-      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post = Post.new(post_params)
       post.user = user
       post.save
 
@@ -20,9 +21,9 @@ RSpec.describe Comment, type: :model do
     end
 
     it 'ensures content presence' do
-      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user = User.new(user_params)
       user.save
-      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post = Post.new(post_params)
       post.user = user
       post.save
       comment = Comment.new(content:'lorem ipsum')
@@ -33,9 +34,9 @@ RSpec.describe Comment, type: :model do
     end
 
     it 'ensures content lenght to be maximum 200' do
-      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user = User.new(user_params)
       user.save
-      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post = Post.new(post_params)
       post.user = user
       post.save
       comment = Comment.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vitae bibendum sapien.')
@@ -46,9 +47,9 @@ RSpec.describe Comment, type: :model do
     end
 
     it 'ensures content lenght to be maximum 200' do
-      user = User.new(name:'user', email:'user@email.com', password:'UseRQWERty')
+      user = User.new(user_params)
       user.save
-      post = Post.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vita')
+      post = Post.new(post_params)
       post.user = user
       post.save
       comment = Comment.new(content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie lobortis diam, ac luctus massa lacinia non. Curabitur cursus elementum sem, fermentum porta lectus tristique sed. Phasellus vit.')
