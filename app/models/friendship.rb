65de4cfceff06fb4f1bc27be739dc8a938e96ffc
Oever González <software@notengobattery.com>
2021-03-04 14:52:29 -0600

    Make the test for listing all friends pass

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
+  ACCEPT = 1
+  REJECT = -1
+  PENDING = 0
+
   after_initialize :default_status
 
   validates :status, :user_id, :friend_id, presence: true
@@ -6,27 +10,29 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
+  scope :all_friends_of, ->(u) { where('(user_id=? OR friend_id=?) AND status=?', u.id, u.id, Friendship::ACCEPT) }
+
   def accept
-    self.status = 1
+    self.status = Friendship::ACCEPT
     save!
   end
 
   def accepted?
-    status == 1
+    status == Friendship::ACCEPT
   end
 
   def reject
-    self.status = -1
+    self.status = Friendship::REJECT
     save!
   end
 
   def rejected?
-    status == -1
+    status == Friendship::REJECT
   end
 
   private
 
   def default_status
-    self.status ||= 0
+    self.status ||= Friendship::PENDING
   end
 end
