47c33e1d705159d07f673b7122f1837059cb2275
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-04 12:41:09 -0400

    Create test for the validations and asssociations

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Friendship, type: :model do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:friend_id) }
   end
+
+  context 'association' do
+    subject { FactoryBot.build(:friendship) }
+    it { should belong_to(:user) }
+
+    it { should belong_to(:friend) }
+  end
 end
