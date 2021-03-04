c8a678fb87b88836082872af1324a71cdb88d6f3
Oever González <software@notengobattery.com>
2021-03-04 09:51:23 -0600

    Add unit tests for Friendship's validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,3 @@
 class Friendship < ApplicationRecord
+  validates_presence_of :status, :user_id, :friend_id
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    subject { FactoryBot.build(:friendship) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:friend_id) }
+  end
 end
