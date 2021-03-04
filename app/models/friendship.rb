886d6c2e902ffe554f5906ae964f5e7ead1b9996
Oever González <software@notengobattery.com>
2021-03-04 13:45:39 -0600

    Add a default status to the Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,14 @@
 class Friendship < ApplicationRecord
+  after_initialize :default_status
+
   validates :status, :user_id, :friend_id, presence: true
 
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  private
+
+  def default_status
+    self.status ||= 0
+  end
 end
