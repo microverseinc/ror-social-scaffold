1728d4b91b42fdbc0a86bd8eae80268a84b33756
ZawadiSumba66 <rosesumba98@gmail.com>
2021-03-05 16:00:35 +0300

    Create tests for user model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+  enum status: { accepted: 'accepted', pending: 'pending' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
+  validates :email, uniqueness: true
 
   has_many :posts
   has_many :comments, dependent: :destroy
