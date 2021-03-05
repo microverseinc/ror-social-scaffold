03e6fa0ad95f4f0011e6a69bd5d5dbf7c21be478
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-03-05 09:08:18 +0100

    Add rubocop gem

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2021_02_26_090155) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
