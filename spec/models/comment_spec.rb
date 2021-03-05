949a9f3050a9899fa24b861af448304a38ce0e1e
ZawadiSumba66 <rosesumba98@gmail.com>
2021-03-05 18:06:13 +0300

    Add tests for like and comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Comment', type: :model do
+    it 'is valid if the comment is present' do
+        user = User.create(name: 'Rose', email: 'rosesumba6@yahoo.mail', password: "sertot")
+        post = Post.create(content: 'Where is Uhuru Kenyatta?', user_id: user.id)
+        comment= Comment.create(content: 'I get so bored in the afternoon',user_id: user.id, post_id: post.id)
+        expect(comment).to be_valid
+    end
+    it 'is invalid if the comment is not present' do
+        user = User.create(name: 'Rose', email: 'rosesumba6@yahoo.mail', password: "sertot")
+        post = Post.create(content: 'Where is Uhuru Kenyatta?', user_id: user.id)
+        comment= Comment.create(content: '',user_id: user.id, post_id: post.id)
+        expect(comment).not_to be_valid
+    end
+
+    describe 'associations' do
+        it 'belongs to post' do
+            comment =  Comment.reflect_on_association(:user)
+            expect(comment.macro).to eql(:belongs_to)
+        end
+        it 'belongs to user' do
+            comment =  Like.reflect_on_association(:post)
+            expect(comment.macro).to eql(:belongs_to)
+        end
+    end
+end
