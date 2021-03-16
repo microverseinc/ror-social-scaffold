df7b72a29ea6632cb39e9a60b74407a022169a6f
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-16 18:36:18 +0300

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,27 +13,31 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
 
-# status = confirmed and status == 'confirmed'
+  # status = confirmed and status == 'confirmed'
   def friends
-    friends_array = friendships.map{ |friendship| friendship.friend if friendship.status}
-    friends_array + inverse_friendships.map{ |friendship| friendship.user if friendship.status}
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
+    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.status }
     friends_array.compact
   end
 
   # Users who have yet to confirme friend requests
   def pending_friends
-    friendships.map { |friendship| friendship.friend if 
-      !friendship.status }.compact
+    friendships.map do |friendship|
+      friendship.friend unless
+      friendship.status
+    end.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{ |friendship| friendship.user if 
-      !friendship.confirmed }.compact
+    inverse_friendships.map do |friendship|
+      friendship.user unless
+      friendship.confirmed
+    end.compact
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{ |friendship| friendship.user == user }
+    friendship = inverse_friendships.find { |friend| friend.user == user }
     frienship.status = true
     friendship.save
   end
