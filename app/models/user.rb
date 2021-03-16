15f0f7995e188aff971dbc3096029d3fd17648b5
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-16 17:49:35 +0300

    Add helper methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,26 @@ class User < ApplicationRecord
     friends_array + inverse_friendships.map{ |friendship| friendship.user if friendship.status}
     friends_array.compact
   end
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map { |friendship| friendship.friend if 
+      !friendship.status }.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{ |friendship| friendship.user if 
+      !friendship.confirmed }.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{ |friendship| friendship.user == user }
+    frienship.status = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
