02c8fddf78622f8fd25e10b6736a2c4f953e57a7
chrystalme <afam_ifeanyi@live.com>
2021-03-16 16:16:14 +0200

    Add user association to friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
+
+# status = confirmed and status == 'confirmed'
+  def friends
+    friends_array = friendships.map{ |friendship| friendship.friend if friendship.status}
+    friends_array + inverse_friendships.map{ |friendship| friendship.user if friendship.status}
+    friends_array.compact
+  end
 end
