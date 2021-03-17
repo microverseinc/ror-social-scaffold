9812404c4d5a509c7a43f55179400106741593e4
chrystalme <afam_ifeanyi@live.com>
2021-03-17 17:38:37 +0200

    Add request button to profile page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,15 +2,18 @@ class FriendshipsController < ApplicationController
  
 
   def create
-    current_user.friend_requests
+    # request(current_user, @friend)
     flash[:notice] = "Friend request has been sent #{user.name}."
     redirect_to users_path
   end
+ 
 
   private
 
   def user
     @friend = User.find(params[:user_id])
   end
+
+  
 
 end
