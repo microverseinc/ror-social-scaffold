da5139ade58569122e0aa77bcef9f5ac2f876077
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-18 16:50:03 +0300

    Add friend request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
-    # User.friend_requests
     @friendship = current_user.friendships.new(friend_id: params[:user_id])
     flash[:notice] = "Friend request has been sent to #{user.name}."
- 
     redirect_to users_path(@friend)
   end
 
   def accept
     if @friendship.save
+      status = true
       flash[:notice] = "#{user.name} accept your request."
     end
     redirect_to users_path(@friend)
