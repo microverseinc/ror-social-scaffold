09eafe0eb8391357c2f305b0cf97b4a37e6315cc
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-03-19 09:36:54 -0700

    fixed database error

--- db/migrate/20210315184252_create_friendships.rb
+++ db/migrate/20210315184252_create_friendships.rb
@@ -1,4 +1,4 @@
-class CreateFriendships < ActiveRecord::Migration
+class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, index: true, foreign_key: true
