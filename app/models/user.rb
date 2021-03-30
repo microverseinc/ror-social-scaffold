b4eb578716ba275cb7867289edd00214fb31811c
Nihal Mohammed <mnihal64@outlook.com>
2021-03-30 18:47:59 +0530

    Create friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_mant :inverse_frienships, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  def friends
+    friends_array1 = friendships.map{|friendship| friendship.friend if friendship.status}
+    friends_array2 = inverse_friendships.map{|friendship| friendship.user if friendship.status}
+    [friends_array1, friends_array2].flatten
+  end
 end
