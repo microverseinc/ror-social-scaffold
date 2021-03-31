7efe56d5eef89ff95867797c040d37d16451bcbb
oluwadare <wintan1418@gmail.com>
2021-03-31 10:38:38 +0100

    add foreign keys and modify model files

--- db/migrate/20210330100806_create_friendies.rb
+++ db/migrate/20210330100806_create_friendies.rb
@@ -1,11 +1,12 @@
 class CreateFriendies < ActiveRecord::Migration[5.2]
   def change
     create_table :friendies do |t|
-      t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.references :friend, index: true
       t.boolean :confirmed
 
-      t.timestamps
+      t.timestamps null: false
     end
+    add_foreign_key :friendies, :users, column: :friend_id
   end
 end
