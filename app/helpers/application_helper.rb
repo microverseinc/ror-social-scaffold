e7266a8ecbc446519cae397ff5f36932004784a2
arikarim <arikarim146@gmail.com>
2021-03-31 17:52:45 +0300

    Add ERD

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,17 @@ module ApplicationHelper
   def friend?(user); end
 
   def already_sended?(user)
-    if current_user.friendships.any? { |friendship| friendship.friend == user }
-      redirect_to root_path
-      flash[:notice] = 'you already sent a request to this person'
-    end
+    return unless current_user.friendships.any? { |friendship| friendship.friend == user }
+
+    redirect_to root_path
+    flash[:notice] = 'you already sent a request to this person'
   end
 
   def sended_to_us?(_user)
-    if current_user.inverse_friendships.any? { |friendship| friendship.user == current_user }
-      redirect_to root_path
-      flash[:notice] = 'this person already sent a request to you'
-    end
+    return unless current_user.inverse_friendships.any? { |friendship| friendship.user == current_user }
+
+    redirect_to root_path
+    flash[:notice] = 'this person already sent a request to you'
   end
 
   def menu_link_to(link_text, link_path)
@@ -33,8 +33,8 @@ module ApplicationHelper
   end
 
   def btn_send(user)
-    if current_user.friendships.none? { |friendship| friendship.friend == user }
-      (link_to 'Send request', user_friendships_path(user), method: :post)
-    end
+    return unless current_user.friendships.none? { |friendship| friendship.friend == user }
+
+    (link_to 'Send request', user_friendships_path(user), method: :post)
   end
 end
