2479476c6d68143b175cc70aa35938281bd43057
arikarim <arikarim146@gmail.com>
2021-03-31 16:46:26 +0300

    Remove button send request when already sended

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,14 @@ module ApplicationHelper
   def friend?(user); end
 
   def already_sended?(user)
-    if current_user.friendships.any? { |friendship| friendship.friend = user }
+    if current_user.friendships.any? { |friendship| friendship.friend == user }
       redirect_to root_path
       flash[:notice] = 'you already sent a request to this person'
     end
   end
 
   def sended_to_us?(_user)
-    if current_user.inverse_friendships.any? { |friendship| friendship.user = current_user }
+    if current_user.inverse_friendships.any? { |friendship| friendship.user == current_user }
       redirect_to root_path
       flash[:notice] = 'this person already sent a request to you'
     end
@@ -33,7 +33,7 @@ module ApplicationHelper
   end
 
   def btn_send(user)
-    if current_user.friends.include?(user) || current_user != user
+    if current_user.friendships.none? { |friendship| friendship.friend == user }
       (link_to 'Send request', user_friendships_path(user), method: :post)
     end
   end
