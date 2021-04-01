b8a39aa87fdcf50144411517ea0706868ab4a0f5
oluwadare <wintan1418@gmail.com>
2021-04-01 10:12:16 +0100

    fix linter issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,32 +10,31 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendies
-  has_many :inverse_friendies, :class_name => "Friendie", :foreign_key => "friend_id"
+  has_many :inverse_friendies, class_name: 'Friendie', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendies.map{|friendie| friendie.friend if friendie.confirmed}
-    friends_array + inverse_friendies.map{|friendie| friendie.user if friendie.confirmed}
+    friends_array = friendies.map { |friendie| friendie.friend if friendie.confirmed }
+    friends_array + inverse_friendies.map { |friendie| friendie.user if friendie.confirmed }
     friends_array.compact
   end
 
   # Users who have yet to confirme friend requests
   def pending_friends
-    friendies.map{|friendie| friendie.friend if !friendie.confirmed}.compact
+    friendies.map { |friendie| friendie.friend unless friendie.confirmed }.compact
   end
- # Users who have requested to be friends
- def friend_requests
-  inverse_friendies.map{|friendie| friendie.user if !friendie.confirmed}.compact
-end
 
-def confirm_friend(user)
-  friendie = inverse_friendies.find{|friendie| friendie.user == user}
-  friendie.confirmed = true
-  friendie.save
-end
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendies.map { |friendie| friendie.user unless friendie.confirmed }.compact
+  end
 
-def friend?(user)
-  friends.include?(user)
-end
+  def confirm_friend(user)
+    friendie = inverse_friendies.find { |friendie| friendie.user == user }
+    friendie.confirmed = true
+    friendie.save
+  end
 
+  def friend?(user)
+    friends.include?(user)
+  end
 end
-
