e9820cc015e779110f2fcd8a66ed455b9c7727ed
Carlos Ospina <carlossospinag@gmail.com>
2021-04-02 16:09:58 -0500

    update user model with methods for friend requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,24 @@ class User < ApplicationRecord
     friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
     friends_array.compact
   end
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
