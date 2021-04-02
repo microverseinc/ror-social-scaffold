8e0dfecd2a5c6152adce69353f3deadf91b7c673
Nihal Mohammed <mnihal64@outlook.com>
2021-04-02 17:45:06 +0530

    Remove unnecessary comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -19,26 +17,8 @@ class User < ApplicationRecord
   has_many :inverse_friendships, -> { where status: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverse_friendships, source: :user
 
-  # has_many :friendships
-  # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-
-  # def friends
-  #   friends_array1 = friendships.map { |friendship| friendship.friend if friendship.status == true }
-  #   friends_array2 = inverse_friendships.map { |friendship| friendship.user if friendship.status == true }
-  #   [friends_array1, friends_array2].flatten
-  # end
-
-  # def pending_requests
-  #   friendships.map { |friendship| friendship.friend unless friendship.status }.compact
-  # end
-
-  # def friend_requests
-  #   inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
-  # end
-
   def friends_and_own_posts
     Post.where(user: (friends.to_a << self))
-    # This will produce SQL query with IN. Something like: select * from posts where user_id IN (1,45,874,43);
   end
 
   def friend?(user)
