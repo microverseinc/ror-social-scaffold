d3860651c9b20ab3f3f4bb92501dc57f3eaffc09
ybadmus <yusif.badmus@gmail.com>
2021-04-06 01:57:03 +0800

    add view and functionality for all pending friend invitations

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,9 @@
 class FriendshipsController < ApplicationController
+  def index 
+    @friendships = current_user.friend_requests
+  end
+
   def create
-    puts "Yusif"
-    puts params[:id]
     user = current_user.friendships.build(friend: User.find(params[:id].to_i));
     if user.save
       redirect_to request.referrer, alert: 'Friendship request successfully sent.'
