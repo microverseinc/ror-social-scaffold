56044350e05124f83ff977334aa45d4c86d3e1e8
ybadmus <yusif.badmus@gmail.com>
2021-04-06 07:57:44 +0800

    fix rubocop erros

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,10 @@
 class FriendshipsController < ApplicationController
-  def index 
+  def index
     @friendships = current_user.friend_requests
   end
 
   def create
-    user = current_user.friendships.build(friend: User.find(params[:id].to_i));
+    user = current_user.friendships.build(friend: User.find(params[:id].to_i))
     if user.save
       redirect_to request.referrer, alert: 'Friendship request successfully sent.'
     else
@@ -21,7 +21,7 @@ class FriendshipsController < ApplicationController
     end
   end
 
-  def destroy 
+  def destroy
     friendship = Friendship.find_by(user_id: params[:id].to_i, friend_id: current_user.id.to_i)
     if friendship.destroy
       current_user.friend_requests.delete(friendship)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all.select { |user| user != current_user }
+    @users = User.all.reject { |user| user == current_user }
   end
 
   def show
