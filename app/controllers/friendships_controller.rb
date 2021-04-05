a526ca2651943693d8b0e56a039b10c9e016f3ca
Carlos Ospina <carlossospinag@gmail.com>
2021-04-05 18:05:01 -0500

    update controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,11 @@
 class FriendshipsController < ApplicationController
 
     def create
-        @friendship = Friendship.create(friend_id: params[:friend_id], user_id: current_user.id, confirmed: false)
+        @friendship = Friendship.create!(friend_id: params[:friend_id], user_id: current_user.id, confirmed: false)
 
         redirect_to "/users/#{params[:friend_id]}" if @friendship.save
     end
-      
+
     private    
     # Only allow a list of trusted parameters through.
     def friendships_params
@@ -30,7 +30,7 @@ end
 #         render 'new'
 #       end
 #     end
- 
+
 #     def update
 #       @friendship = Friendship.find(params[:id])
 #       @friendship.update_attribute(:confirmed, true)
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :friendships
   post 'users/:id', to:'friendships#create'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
