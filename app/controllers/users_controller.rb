089426ac4eb5a6b116bb747c3723db1ffed40934
unknown <paulclue20@gmail.com>
2021-04-06 16:43:32 -0500

    update users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @use = User.all
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
     @friendships = Friendship.all
