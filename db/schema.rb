6b18fd47ed3488a4bd8ede89f39033163e449739
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-04-07 15:03:06 -0600

    change status default to false and add null to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_191831) do
+ActiveRecord::Schema.define(version: 2021_04_07_205830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_191831) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.boolean "status"
+    t.boolean "status", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "inviter_id"
