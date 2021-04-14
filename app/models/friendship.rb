a807fe82a796fb024c48aa4d8724b4f03c4fd2ae
oluwadare <wintan1418@gmail.com>
2021-04-14 10:34:43 +0100

    fix fubocop errors

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,12 +2,8 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
 
-
-
   def mirror?(friendship)
     true if user.id == friendship.friend.id && friend.id == friendship.user.id
     false
   end
-
 end
-
