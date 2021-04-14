0b0ed8de453046955b045784638dc17567876b22
Alejandro Contreras <kdta15@outlook.com>
2021-04-13 19:24:44 -0500

    Update the Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,11 @@ class User < ApplicationRecord
     friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
     friends_array.compact
   end
-  # Users who have yet to confirme friend requests
+
   def pending_friends
     friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
   end
 
-  # Users who have requested to be friends
   def friend_requests
     inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
   end
