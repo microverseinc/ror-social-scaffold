660967b40d7bb996d3476497f1f31f2986cc3a44
oluwadare <wintan1418@gmail.com>
2021-04-14 10:26:54 +0100

    add functionalities for  mirror friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
+
+
+
+  def mirror?(friendship)
+    true if user.id == friendship.friend.id && friend.id == friendship.user.id
+    false
+  end
+
 end
+
