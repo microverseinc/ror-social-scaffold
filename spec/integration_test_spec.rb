fd3c795ad7d5e1889fa69e2fbb58afa8318435ce
Alexis Varela <alexisbec13@hotmail.com>
2021-04-15 13:07:03 -0500

    add test to post features

--- spec/integration_test_spec.rb
+++ spec/integration_test_spec.rb
@@ -22,10 +22,21 @@ RSpec.describe 'User Authentication', type: :system do
       click_link "Sign out"
       expect(page).to have_content('Sign in')
     end
+  end
+end
 
-    xit 'allows user to sign out from session' do
-      click_link "SIGN OUT"
-      expect(page).to have_content('Sign In')
-    end
+
+RSpec.describe 'Post Features', type: :system do
+  it 'allows user to create a new post' do
+    visit root_path
+    click_link "Sign up"
+    fill_in "Name", with: 'John Smith'
+    fill_in "Email", with: 'john.smith@email.com'
+    fill_in "Password", with: 'abc123'
+    fill_in "Password confirmation", with: 'abc123'
+    click_button "Sign up"
+    fill_in "Content", with: 'This is a test'
+    click_button "Save"
+    expect(page).to have_content('This is a test')
   end
 end
