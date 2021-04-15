df8f2ba7c72b6b80f8da6ca41bfc22b41a86dc2a
Alexis Varela <alexisbec13@hotmail.com>
2021-04-15 17:40:51 -0500

    add pending requests and confirmed friendships labels for user#index and user#show

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,6 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @friendships = Friendship.joins(:inviter).where(invitee_id: current_user.id)
   end
