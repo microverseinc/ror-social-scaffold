1a3ec17c639ab119bf947327ae92f9337742b222
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-04-17 15:19:22 -0400

    Added friendship features to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,28 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :recieved_friendships, foreign_key: :friend_id, class_name: 'Friendship'
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + recieved_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    recieved_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = recieved_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
