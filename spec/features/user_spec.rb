b023f8da4ee8f6d175ba78f51a897b7a7c36e08c
ybadmus <yusif.badmus@gmail.com>
2021-04-20 00:06:55 +0800

    remove unnecessary test cases

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -33,14 +33,6 @@ RSpec.describe 'User session actions', type: :feature do
     expect(page).to have_content('Signed in successfully.')
   end
 
-  scenario 'User fails trying to log in' do
-    visit user_session_path
-    fill_in 'Email', with: 'a@mail.com'
-    fill_in 'Password', with: '123123'
-    click_on 'Log in'
-    expect(page).to have_content('Invalid Email or password.')
-  end
-
   scenario 'user logs out' do
     visit user_session_path
     fill_in 'Email', with: 'a@mail.com'
