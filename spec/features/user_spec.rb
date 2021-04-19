46c86d5c5847ffaea696b5bc2dcf9c641665dfb6
ybadmus <yusif.badmus@gmail.com>
2021-04-20 00:09:58 +0800

    add user login actions to separate test cases for sign up and login in

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -25,12 +25,29 @@ RSpec.describe 'User session actions', type: :feature do
     expect(page).to have_content('Email has already been taken')
   end
 
+  
   scenario 'user logs in successfully' do
     visit user_session_path
     fill_in 'Email', with: 'a@mail.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
     expect(page).to have_content('Signed in successfully.')
+  end
+end
+
+
+RSpec.describe 'User login actions', type: :feature do
+
+  before :each do
+    User.create(name: 'Oscar', email: 'a@mail.com', password: '123456')
+  end
+  
+  scenario 'User fails trying to log in' do
+    visit user_session_path
+    fill_in 'Email', with: 'a@mail.com'
+    fill_in 'Password', with: '123123'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
   end
 
   scenario 'user logs out' do
