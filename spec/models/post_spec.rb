5f60d6e3dbb765361678f3b7b76b5fa3f22c447a
marijanbrvar <marijan.brvar@gmail.com>
2021-04-20 14:38:31 +0200

    Add post test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:content) }
+
+    it { should_not allow_value('').for(:content) }
+  end
+
   describe 'Post' do
     it { should belong_to(:user) }
     it { should have_many(:comments).dependent(:destroy) }
