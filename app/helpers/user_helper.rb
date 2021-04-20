198275a9d6cd8a3d99714a125e376af03fa53d5e
Zil Norvilis <zilasino27@gmail.com>
2021-04-20 12:26:23 +0200

    Add gravatar to user index

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -14,4 +14,10 @@ module UserHelper
   def all_users
     User.select{ |u| u.id != current_user.id}
   end
+
+  def gravatar_for(user, size='32')
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
 end
