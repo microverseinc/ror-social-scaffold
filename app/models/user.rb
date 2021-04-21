755b504238126009309ad09f870879a8ef62c50f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-04-20 19:23:08 -0600

    fix typo in Google_auth2 name in User Model-Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github, :googl_oauth2]
+         :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
