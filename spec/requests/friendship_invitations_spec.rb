773851b8a2f55b3b50d736ce3fe7811d973c3387
Hubert MUGABO <mugberto@gmail.com>
2021-04-22 22:26:14 +0200

    Fix linter errors

--- spec/requests/friendship_invitations_spec.rb
+++ spec/requests/friendship_invitations_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "FriendshipInvitations", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/friendship_invitations/create"
+RSpec.describe 'FriendshipInvitations', type: :request do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/friendship_invitations/create'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/friendship_invitations/destroy"
+  describe 'GET /destroy' do
+    it 'returns http success' do
+      get '/friendship_invitations/destroy'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/views/friendship_invitations/create.html.erb_spec.rb
+++ spec/views/friendship_invitations/create.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "friendship_invitations/create.html.erb", type: :view do
+RSpec.describe 'friendship_invitations/create.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
