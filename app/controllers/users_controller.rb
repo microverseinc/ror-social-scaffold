efd39923036a12f1b2a57072b7be1004477a12d9
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-04-25 01:18:59 +0100

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
-    @friendship = Friendship.new
+    @users = current_user.potential_friends(current_user)
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.ordered_by_most_recent
-    @user_friends = @user.friends
-    @friend_requests = @user.friend_requests
-    @friendship = Friendship.new
+    @user = User.friendly.find(params[:id])
   end
+
+  def edit; end
 end
