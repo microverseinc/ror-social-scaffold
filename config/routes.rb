8bce4b6cd9f946c2d4df563cb33ae6fa9f0e9b88
ixboy <ismaelixboy@gmail.com>
2021-04-27 23:39:41 +0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'friendships/create'
-  get 'friendships/destroy'
   root 'posts#index'
 
   devise_for :users
-
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :friendships, only: %i[create] do
+      collection do
+        get 'accept_friend'
+        get 'decline_friend'
+      end
+    end
+  end
   resources :posts, only: %i[index create] do
     resources :comments, only: [:create]
     resources :likes, only: %i[create destroy]
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
