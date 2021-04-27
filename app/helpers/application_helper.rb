04fdcbb049f19feb264e68fd952f0785755b903f
ixboy <ismaelixboy@gmail.com>
2021-04-27 23:39:25 +0300

    add friendship helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,20 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+  def friend_request_sent?(user)
+    current_user.friend_sent.exists?(friend_id: user.id, status: false)
+  end
+  
+  def friend_request_received?(user)
+    current_user.friend_request.exists?(user_id: user.id, status: false)
+  end
+  
+  def possible_friend?(user)
+    request_sent = current_user.friend_sent.exists?(friend_id: user.id)
+    request_received = current_user.friend_request.exists?(user_id: user.id)
+    
+    return true if request_sent != request_recieved    
+    return true if request_sent == request_recieved && request_sent == true    
+    return false if request_sent == request_recieved && request_sent == false
+  end
 end
