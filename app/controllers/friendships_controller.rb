a77cb0edf45ce09638529db6355f8eb27e4a80ac
ixboy <ismaelixboy@gmail.com>
2021-04-27 23:38:20 +0300

    add friendship actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,40 @@
 class FriendshipsController < ApplicationController
   def create
+    return if current_user.id == params[:user_id]
+  return if friend_request_sent?(User.find(params[:user_id]))
+  return if friend_request_received?(User.find(params[:user_id]))
+
+  @friendship = current_user.friend_sent.build(friend_id: params[:user_id])
+
+  if @friendship.save
+    flash[:success] = 'Friend Request Sent!'
+  else
+    flash[:danger] = 'Friend Request Failed!'
+  end
+  redirect_back(fallback_location: root_path)
   end
 
-  def destroy
+  def accept_friend
+    @friendship = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id, status: false)
+    return unless @friendship # return if no record is found
+
+    @friendship.status = true
+    if @friendship.save
+      flash[:success] = 'Friend Request Accepted!'
+      @friendship2 = current_user.friend_sent.build(sent_to_id: params[:user_id], status: true)
+      @friendship2.save
+    else
+      flash[:danger] = 'Friend Request could not be accepted!'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def decline_friend
+    @friendship = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id, status: false)
+    return unless @friendship # return if no record is found
+
+    @friendship.destroy
+    flash[:success] = 'Friend Request Declined!'
+    redirect_back(fallback_location: root_path)
   end
 end
