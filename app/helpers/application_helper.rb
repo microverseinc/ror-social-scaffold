7168f727d8449df7a1b854bf218db814b84aa466
Rashad-Muntar <muntartoure@gmail.com>
2021-04-27 16:13:05 +0000

    display comment counter and like counter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,13 @@ module ApplicationHelper
   end
 
   def like_or_dislike_btn(post)
+    counter = 0
     like = Like.find_by(post: post, user: current_user)
     if like
+      counter -= 1
       link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete)
     else
+      counter += 1
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
