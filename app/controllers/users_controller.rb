e26f9694f7d9294818dbecb2f8677350d45d1014
ixboy <ismaelixboy@gmail.com>
2021-04-27 23:38:50 +0300

    edit user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @friends = current_user.friends
+    @pending_requests = current_user.pending_requests
+    @friend_requests = current_user.received_requests
   end
 
   def show
