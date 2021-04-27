35c6bd8cd75f18b49ab8d03782003ef9022fd2a2
Rashad-Muntar <muntartoure@gmail.com>
2021-04-27 17:47:03 +0000

    add test for user, friendship and likes

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+
+    describe 'validations' do
+        it { should validate_presence_of(:content) }
+      end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:comments).dependent(:destroy) }
+    it { should have_many(:likes).dependent(:destroy) }
+
+    
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+    describe 'validations' do
+        it { should validate_presence_of(:name) }
+      end
+
+  describe 'associations' do
+    it { should have_many(:posts) }
+    it { should have_many(:comments).dependent(:destroy) }
+    it { should have_many(:likes).dependent(:destroy) }
+    it { should have_many(:friendships) }
+    it { should have_many(:inverse_friendships).class_name('Friendship') }
+  end
+
+end
