0cd2c3627fc82dbfab52353da21ae4f8bdcc049a
ixboy <ismaelixboy@gmail.com>
2021-04-30 08:43:19 +0300

    edit friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,40 +1,35 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :def_user, only: %i[create accept decline destroy]
+
   def create
-    return if current_user.id == params[:user_id]
-    return if friend_request_sent?(User.find(params[:user_id]))
-    return if friend_request_received?(User.find(params[:user_id]))
-
-    @friendship = current_user.friend_sent.build(friend_id: params[:user_id])
-
-    if @friendship.save
-      flash[:success] = 'Friend Request Sent!'
-    else
-      flash[:danger] = 'Friend Request Failed!'
-    end
-    redirect_back(fallback_location: root_path)
+    Friendship.request(@user1, @friend)
+    flash[:notice] = "Friend request has been sent to #{@friend.name}."
+    redirect_to user_path(@friend)
   end
 
-  def accept_friend
-    @friendship = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id, status: false)
-    return unless @friendship # return if no record is found
-
-    @friendship.status = true
-    if @friendship.save
-      flash[:success] = 'Friend Request Accepted!'
-      @friendship2 = current_user.friend_sent.build(sent_to_id: params[:user_id], status: true)
-      @friendship2.save
-    else
-      flash[:danger] = 'Friend Request could not be accepted!'
-    end
-    redirect_back(fallback_location: root_path)
+  def accept
+    Friendship.accept(@user1, @friend)
+    flash[:notice] = "Friend request from #{@friend.name} has been accepted."
+    redirect_to user_path(@user1)
   end
 
-  def decline_friend
-    @friendship = Friendship.find_by(sent_by_id: params[:user_id], sent_to_id: current_user.id, status: false)
-    return unless @friendship # return if no record is found
+  def decline
+    Friendship.breakup(@user1, @friend)
+    flash[:notice] = "Friend request from #{@friend.name} has been declined."
+    redirect_to user_path(@user1)
+  end
 
-    @friendship.destroy
-    flash[:success] = 'Friend Request Declined!'
-    redirect_back(fallback_location: root_path)
+  def destroy
+    Friendship.breakup(@user1, @friend)
+    flash[:notice] = "#{@friend.name} has been successfully removed from your friends list."
+    redirect_to user_path(@user1)
+  end
+
+  private
+
+  def def_user
+    @user1 = current_user
+    @friend = User.find(params[:id])
   end
 end
