49b9c5381b339e714963f2a9273327577d1c47c6
ixboy <ismaelixboy@gmail.com>
2021-04-30 08:43:38 +0300

    edit user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @friends = current_user.friends
-    @pending_requests = current_user.pending_requests
-    @friend_requests = current_user.received_requests
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @friends = @user.friends
+    @my_friends = current_user.friends.uniq
+    @pending_requests = current_user.pending_friends.uniq
+    @requested_friends = current_user.requested_friends.uniq
   end
 end
