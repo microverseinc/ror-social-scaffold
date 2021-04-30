1fce960dcd2bd985ab7a9334ec44e9bcc021ade3
ixboy <ismaelixboy@gmail.com>
2021-04-30 08:47:30 +0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,25 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: %i[index show] do
-    resources :friendships, only: %i[create] do
-      collection do
-        get 'accept_friend'
-        get 'decline_friend'
-      end
+    member do
+      get :friends
     end
   end
+  # resources :users, only: %i[index show] do
+  #   resources :friendships, only: %i[create] do
+  #     collection do
+  #       # get 'accept_friend'
+  #       # get 'decline_friend'
+
+  #     end
+  #   end
+  # end
+  get 'friends', to: 'friendships#index', as: 'friends'
+  post 'friends/create/:id', to: 'friendships#create', as: 'add_friend'
+  put 'friends/accept.:id', to: 'friendships#accept', as: 'accept_request'
+  delete 'friends/decline/:id', to: 'friendships#decline', as: 'decline_request'
+  delete 'friends/delete/:id', to: 'friendships#destroy', as: 'delete_friend'
+
   resources :posts, only: %i[index create] do
     resources :comments, only: [:create]
     resources :likes, only: %i[create destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,6 @@
 # rubocop:disable Metrics/BlockLength
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_423_164_436) do
+ActiveRecord::Schema.define(version: 20_210_429_033_011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -28,7 +17,7 @@ ActiveRecord::Schema.define(version: 20_210_423_164_436) do
   create_table 'friendships', force: :cascade do |t|
     t.bigint 'user_id'
     t.bigint 'friend_id'
-    t.boolean 'status'
+    t.string 'status'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['friend_id'], name: 'index_friendships_on_friend_id'
@@ -66,5 +55,5 @@ ActiveRecord::Schema.define(version: 20_210_423_164_436) do
 
   add_foreign_key 'friendships', 'users'
   add_foreign_key 'friendships', 'users', column: 'friend_id'
-  # rubocop:enable Metrics/BlockLength
 end
+# rubocop:enable Metrics/BlockLength
