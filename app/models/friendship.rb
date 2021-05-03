79aaa05753fd75e10149ebdc83d102960fa22972
ixboy <ismaelixboy@gmail.com>
2021-05-03 22:04:58 +0300

    edit routes

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,10 +1,9 @@
 class Friendship < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
   def self.request(user, friend)
     return if user == friend
-
     transaction do
       create(user: user, friend: friend, status: 'pending')
       create(user: friend, friend: user, status: 'requested')
@@ -16,13 +15,6 @@ class Friendship < ApplicationRecord
       updated_at = Time.now
       accept_one_side(user, friend, updated_at)
       accept_one_side(friend, user, updated_at)
-    end
-  end
-
-  def self.breakup(user, friend)
-    transaction do
-      destroy(find_by_user_id_and_friend_id(user.id, friend.id))
-      destroy(find_by_user_id_and_friend_id(friend.id, user.id))
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,7 @@ Rails.application.routes.draw do
       get :friends
     end
   end
-  # resources :users, only: %i[index show] do
-  #   resources :friendships, only: %i[create] do
-  #     collection do
-  #       # get 'accept_friend'
-  #       # get 'decline_friend'
-
-  #     end
-  #   end
-  # end
+ 
   get 'friends', to: 'friendships#index', as: 'friends'
   post 'friends/create/:id', to: 'friendships#create', as: 'add_friend'
   put 'friends/accept.:id', to: 'friendships#accept', as: 'accept_request'
