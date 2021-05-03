6177c490dc0dabe36ce10f0e77c12ebc7752272f
ixboy <ismaelixboy@gmail.com>
2021-05-04 01:53:23 +0300

    add helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,24 @@
 module UserHelper
   def friend_button(current_user, user)
-    if current_user.friends.include?(user) || current_user.pending_friends.include?(user) || current_user.requested_friends.include?(user)
+    if current_user.friends.include?(user) ||
+       current_user.pending_friends.include?(user) || current_user.requested_friends.include?(user)
       ''
     else
-      link_to 'Send Friend Request', add_friend_path(user), method: :post, class: 'btn btn-primary'
+      unless current_user.id == user.id
+        link_to 'Send Friend Request', add_friend_path(user), method: :post,
+                                                              class: 'btn btn-primary'
+      end
+    end
+  end
+
+  def friendship_table(my_friends, requested_friends, current_user, user)
+    if current_user.id == user.id && my_friends.count.positive? || requested_friends.count.positive?
+      render 'shared/myfriends'
+    elsif current_user.id == user.id && !my_friends.count.positive? && !requested_friends.count.positive?
+      content_tag :p, "You don't have any friends yet. make some friends.", style: 'align = center;', class: 'lead'
+
+    else
+      ''
     end
   end
 end
