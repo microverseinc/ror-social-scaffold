d4205ac6c9872cd88be60049eb7d0d02a4dbc241
ixboy <ismaelixboy@gmail.com>
2021-05-03 22:02:21 +0300

    edit user helpers

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
-  def friend_button(_current_user, user)
-    if @my_friends.include?(user) || @pending_requests.include?(user) || @requested_friends.include?(user)
+  def friend_button(current_user, user)
+    if current_user.friends.include?(user) || current_user.pending_friends.include?(user) || current_user.requested_friends.include?(user)
       ''
     else
       link_to 'Send Friend Request', add_friend_path(user), method: :post, class: 'btn btn-primary'
