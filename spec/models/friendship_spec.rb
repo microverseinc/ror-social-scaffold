279b9f88d213a07042d74116e926bb8ec29c6e94
ixboy <ismaelixboy@gmail.com>
2021-05-05 03:14:31 +0300

    add model tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user1 = User.create(id: 1, name: 'Ismael', email: 'ismael@gmail.com', password: '123456', password_confirmation: '123456')
+    @friend = User.create(id: 2, name: 'Ixboy', email: 'ixboy@gmail.com', password: '123456', password_confirmation: '123456')
+    @friendship = Friendship.create(user_id: 1, friend_id: 2, status: 'accepted')
+  end
+  it 'should have valid attributes' do
+    expect(@friendship).to be_valid
+  end
+
+
+  it 'should update status to accepted' do
+    @friendship.update(status: 'accepted')
+    expect(Friendship.find_by_status('accepted')).to eq(@friendship)
+  end
+
+  it 'should update status to reject' do
+    @friendship.update(status: 'accepted')
+    expect(Friendship.find_by_status('accepted')).to eq(@friendship)
+  end
+
+  describe 'Associations' do
+     it { should belong_to(:user) }
+    it { should belong_to(:friend) }
+  end
+
+
+  context 'friendship' do
+    let(:sender) {@user1 }
+    let(:receiver) {@friend }
+
+    it 'can request a frienship' do
+      invitation = Friendship.request(@user1, @friend)
+      @request1 = Friendship.find(2)
+      @request2 = Friendship.find(3)
+      expect(@request1.status).to eq('pending')
+      expect(@request2.status).to eq('requested')
+      expect(invitation.status?).to be_truthy
+    end
+  end
 end
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before(:all) do
+    @user5 = User.create!(id:100, name: 'bruno', email: 'bruno@gmail.com', password: '123456', password_confirmation: '123456')
+  end
+
+  it 'should have valid attributes' do
+    expect(@user5).to be_valid
+  end
+
+  it 'should find user by email' do
+    expect(User.find_by_email('isma@gmail.com')).to eq(@user)
+  end
+
+  it 'is not valid without an email' do
+    user = User.new(email: nil)
+    expect(user).to_not be_valid
+  end
+
+  describe 'Associations' do
+    it { should have_many(:friendships)}
+  end
+
+  describe 'Associations' do
+    it { should have_many(:friends)}
+  end
+end
