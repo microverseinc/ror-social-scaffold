895f9a70f851c2c4c78a71ea9c46fdb6f9842692
ixboy <ismaelixboy@gmail.com>
2021-05-06 00:02:32 +0300

    edit tests comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,17 +5,14 @@ RSpec.describe Comment, type: :model do
     @user4 = User.create!(id: 97, name: 'user4', email: 'user4@gmail.com', password: '123456',
                           password_confirmation: '123456')
     @user5 = User.create!(id: 96, name: 'user5', email: 'user5@gmail.com', password: '123456',
-                            password_confirmation: '123456')
+                          password_confirmation: '123456')
     @post = Post.create(user_id: @user4.id, content: 'I really need this tests to pass :)')
     @comment = Comment.create(user_id: @user5.id, post_id: @post.id, content: 'okay dude!')
-    
   end
-
 
   it 'should have valid attributes' do
     expect(@comment).to be_valid
   end
-
 
   it 'should not be valid without post id' do
     comment = Comment.new(post_id: nil)
@@ -27,11 +24,9 @@ RSpec.describe Comment, type: :model do
     expect(comment).to be_a_kind_of(Integer)
   end
 
-
   it 'should find comment by user id' do
     expect(Comment.find_by(user_id: 96)).to eq(@comment)
   end
-
 
   describe 'Associations' do
     it { should belong_to(:user) }
@@ -40,5 +35,4 @@ RSpec.describe Comment, type: :model do
   describe 'Associations' do
     it { should belong_to(:post) }
   end
-
 end
