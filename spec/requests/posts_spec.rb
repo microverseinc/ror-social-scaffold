4a15feb1d5a11cdacb6fe6385d8a5bf7b9dfbf7b
ixboy <ismaelixboy@gmail.com>
2021-05-06 00:08:21 +0300

    fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-
   describe 'GET /index' do
     it 'renders a successful response' do
       get '/posts'
@@ -9,10 +8,10 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
-  describe "GET posts#create" do
-    it "should get create" do
+  describe 'GET posts#create' do
+    it 'should get create' do
       post '/posts'
-  expect(response).to have_http_status(302)
+      expect(response).to have_http_status(302)
     end
   end
 end
