f416075496727608b80b526f990207a813ed768b
EroAuditore <alfa.fenrir@gmail.com>
2021-05-07 18:29:23 -0500

    test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
   let(:user2) { User.create(name:'user_test2', email:'mail2@mail.com', password: 'password') } 
   let(:user3) { User.create(name:'user_test3', email:'mail2@mail.com', password: 'password') } 
   let(:invalid_user) { User.create(name:nil, email:'mail3@mail.com', password: 'password') }
+  let(:post_1) { user1.posts.create(content: "Post test")}
+  let(:comment_1 ) { Comment.new(content: "Test comment") }
 
   describe 'An User can be created it has all the validations' do
     it 'Has email, name and password' do
@@ -23,6 +25,17 @@ RSpec.describe User, type: :model do
         post = user1.posts.new()
         post.save
         expect(post).to_not be_valid 
+      end
+      it 'can comment post'do
+        comment_1.user = user1;
+        post_1.comments << comment_1
+        post_1.save
+        expect(post_1).to be_valid
+      end
+      it 'can Like post'do
+
+        like = Like.new(user: user1, post: post_1)
+        expect(like).to be_valid
       end
 
   end
