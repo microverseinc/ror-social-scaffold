31921421854134f863c32870e5b4ced5363fd52c
nna nna 19 <uchennaongit@gmail.com>
2021-05-07 13:46:52 +0100

    Modify the user model and friendship model with functions for third milestone

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -16,3 +16,35 @@ class Friendship < ApplicationRecord
     # Code goes here
   end
 end
+
+def confirm_friend
+  update_attributes(confirmed: true)
+  Friendship.create!(friend_id: user_id,
+                     user_id: friend_id,
+                     confirmed: true)
+end
+
+def set_defaults
+  self.confirmed = false if confirmed.nil?
+end
+
+def create_inverse
+  Friendship.create(user: friend, friend: user, confirmed: confirmed)
+end
+
+def destroy_inverse
+  ir = inverse_record
+  return if ir.nil?
+
+  ir.destroy
+end
+
+def inverse_record_nil?
+  inverse_record.nil?
+end
+
+def inverse_record
+  Friendship.where(user: friend, friend: user).first
+end
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,48 @@ class User < ApplicationRecord
 
   has_many :sent_requests, class_name: 'Friendship', dependent: :destroy, foreign_key: :user_id
   has_many :received_requests, class_name: 'Friendship', dependent: :destroy, foreign_key: :friend_id
+
+  def friends_and_own_posts
+    Post.where(user: (friends.to_a << self))
+  end
+
+  def friends
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  end
+
+  def friend_requests
+    Friendship.where(friend_id: id, confirmed: false)
+  end
+
+  def pending_requests
+    friend_requests.map(&:user).compact
+  end
+
+  def pending_friendship(user)
+    friend_requests.find { |friendship| friendship if friendship.user == user }
+  end
+
+  def pending_friendship?(user)
+    !pending_friendship(user).nil?
+  end
+
+  def confirm_friend(user)
+    friendship = friend_requests.find { |f| f.user == user }
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def reject_friend(user)
+    friendship = friend_requests.find { |f| f.user == user }
+    friendship.destroy
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
