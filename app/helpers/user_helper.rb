e37082b93804a823ab5c517b71b52db7d063e172
Hubert MUGABO <mugberto@gmail.com>
2021-05-12 00:43:53 +0200

    Fix #invite_to_priendship_btn helper method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,7 +2,7 @@ module UserHelper
   def invite_to_friendship_btn(user)
     return if user == current_user or current_user.friend? user
 
-    if current_user.pending_invitations.include? user
+    if current_user.pending_friends.include? user
       content_tag :span, 'Pending request', class: 'pending_badge'
     elsif current_user.invited_by? user
       render partial: 'shared/confirm_reject_btn', locals: { user: user }
