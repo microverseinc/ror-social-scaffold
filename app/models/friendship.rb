b859a13ac3e4655eb49ad781cce74ab8fc58b871
EroAuditore <alfa.fenrir@gmail.com>
2021-05-11 13:25:06 -0500

    Model frindship updated to mutal friendship work

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,30 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  after_initialize :set_defaults, unless: :persisted?
+  after_create :create_inverse, if: :inverse_record_nil?
+  after_update :update_inverse
+  after_destroy :destroy_inverse
+
+  def set_defaults
+    self.confirmed = false if self.confirmed.nil?
+  end
+
+  def create_inverse
+     Friendship.create(user: friend, friend: user, confirmed: confirmed)
+  end
+
+  def inverse_record_nil?
+      inverse_record.nil?
+  end
+  def inverse_record
+    Friendship.where(user: user, friend: friend).first
+  end
+
+  def update_inverse
+    ir = inverse_record
+    return if ir.confirmed == confirmed
+    ir.confirmed = confirmed
+    ir.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,11 +21,13 @@ class User < ApplicationRecord
 
   # Users who have yet to confirme friend requests
   def pending_friends
+    
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
+    #Friendship.where(friend_id: id, confirmed: false)
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
