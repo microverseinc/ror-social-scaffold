7b1d9a5157522952ac6a40fed0f123fa86069d92
emmy <emmy66418@gmail.com>
2021-05-11 17:03:00 +0200

    Generate the model table for the friendships actions

---
+++ db/migrate/20210511150139_create_friendships.rb
@@ -0,0 +1,11 @@
+class CreateFriendships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: true
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
