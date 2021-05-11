81828704382beb305eb02636bf5234f4f17c87a8
emmy <emmy66418@gmail.com>
2021-05-11 17:04:44 +0200

    Migrate friendship table to be used

--- db/migrate/20210511150139_create_friendships.rb
+++ db/migrate/20210511150139_create_friendships.rb
@@ -1,11 +1,12 @@
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :friend, null: false, foreign_key: true
-      t.boolean :confirmed
+      t.references :user, foreign_key: true
+      t.references :friend, foreign_key: { to_table: 'users' }
+      t.string :status
 
       t.timestamps
     end
+    add_index :friendships, [:user_id, :friend_id], :unique => true
   end
 end
