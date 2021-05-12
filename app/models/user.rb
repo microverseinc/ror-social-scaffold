1d7f6e5710cc417d8a5a2fcb186c0df82ca441b5
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-12 22:39:09 +0200

    Add associations and required methods for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,33 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships, :class_name  => "Friendship", :foreign_key => "sender_id" 
+  has_many :inverse_friendships, :class_name  => "Friendship", :foreign_key => "receiver_id"
+
+  def friends
+     friends_array = 
+     friendships.map{|friendship| friendship.sender if friendship.status} + 
+     friendships.map{|friendship| friendship.receiver if friendship.status}
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.receiver if !friendship.status} 
+  end
+
+  def friend_requests
+    friendships.map{|friendship| friendship.sendedr if !friendship.status} 
+  end
+  
+  def conirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.sender == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
