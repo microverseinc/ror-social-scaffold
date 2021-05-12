e308180e11dcae672de500d4aa64b9d7e967aa7a
Hubert MUGABO <mugberto@gmail.com>
2021-05-12 02:59:14 +0200

    Add pending_friendships and pending_friends assoctions

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,8 @@ class User < ApplicationRecord
   has_many :friendships, class_name: 'Friendship', foreign_key: 'user_id', dependent: :destroy
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
-
-  def pending_friends
-    friendships.map { |invitation| invitation.friend if invitation.confirmed == false }.compact
-  end
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
 
   def friendship_requests
     inverted_friendships.map { |invitation| invitation.user if invitation.confirmed == false }.compact
