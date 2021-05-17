db3aa65da5706fb5874df38399240fd631b642e4
Shaher-11 <shahershamroukh@gmail.com>
2021-05-17 20:06:36 +0200

    Add the helper methods for for the buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,51 @@ module ApplicationHelper
       link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
+
+  def friends_names(user)
+    result = '<b>There are no friends</b>'
+    unless user.friends.empty?
+      result = ''
+      user.friends.each do |friend|
+        result += friend.name.to_s
+      end
+    end
+    result.html_safe
+  end
+
+  def pending_requests(user)
+    result = '<b>There are no friend requests</b>'
+    unless user.friend_requests.empty?
+      result = ''
+      user.friend_requests.each do |request|
+        result += request.name.to_s
+        if user == current_user
+          result += (button_to 'Accept', accept_user_path(request.id), method: :get).to_s
+          result += (button_to 'Decline', decline_user_path, method: :get, params: { data: request.id }).to_s
+        end
+        result += ' '
+      end
+    end
+    result.html_safe
+  end
+
+  def accept_decline_buttons(user)
+    result = ''
+    current_user.friend_requests.each do |request|
+      if requster == user
+        result += (button_to 'Accept', accept_user_path(request.id), method: :get).to_s
+        result += (button_to 'Decline', decline_user_path(request.id), method: :get).to_s
+      end
+    end
+    result.html_safe
+  end
+
+  def invitation_button(user)
+    result = ''
+    if !user.friends.include?(current_user) && user != current_user && !user.pending?(current_user)
+      result += (button_to 'Invite to friendship', create_friendship_user_path(user.id),
+                           method: :get).to_s
+    end
+    result.html_safe
+  end
 end
