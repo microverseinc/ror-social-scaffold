a4ac04c8f905306355ea5476050a9f32c0cd76fa
Niko Escobal <niko.escobal@gmail.com>
2021-05-17 22:32:49 +0800

    Add accept and decline methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def accept
+    current_user.confirm_friend(@user)
+    redirect_to request.referrer, notice: "Confirmed friend request"
+  end
+
+  def decline
+    user = user.find(params[:data])
+    friendship = @user.inverse_friendshiips.find { |f| f.user == user } 
+    friendship.destroy
+    redirect_to request.referrer, notice: "Declined friend request"
+  end
+
 end
