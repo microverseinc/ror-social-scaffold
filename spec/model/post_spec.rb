dc283b2efcdf21f2e336fef888f71c54c0cfcd60
CollinsTatang <makungongcollins@gmail.com>
2021-05-18 11:18:06 +0100

    add post test

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe Post, type: :model do
       .is_at_most(1000)
       .with_message('1000 characters in post id the maximum allowed.')
   end
-  
+  it 'should save new post successfully' do
+    post1 = Post.new(content: 'soft vibes', user_id: 1).save
+    expect(post1) == true
+  end
 
 end 
