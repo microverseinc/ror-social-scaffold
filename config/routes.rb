6a050ac7ee0e0a98f494ac7bd04b697cc2a4bb4e
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-18 16:53:34 +0200

    Add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    post 'request', to:'users#send_request'
+    put '/approve/:sender_id', to: 'users#confirm_request'
+    delete '/reject/:sender_id', to: 'users#reject_request'
+  end
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
