f2428fbc2e5c9daa470bfddb3886536f45fc48c7
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-18 16:55:04 +0200

    Add tests to Friendship

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+
+	let(:u1) { User.create(name: 'Bishoy', email: 'Bishoy@gmail.com', password: '246888') }
+  	let(:u2) { User.create(name: 'Felix', email: 'Felix@gmail.com', password: '246888') }
+
+	it 'It check if the friendship already exist' do
+		Friendship.create(u1.id, u2.id)
+	    expect(Friendship.requested_before?(u1.id,u2.id)).to eql(true)
+	    expect(Friendship.requested_before?(u1.id,3)).to eql(false)
+  	end
+
+end
+
