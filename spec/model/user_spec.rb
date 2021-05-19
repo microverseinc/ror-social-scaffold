14a325edca3d208714a8ccea6cc833b7bec80be2
crappypapa <devsolaoluwa@gmail.com>
2021-05-19 11:47:09 +0100

    Add more test for user model

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -33,4 +33,42 @@ RSpec.describe User, type: :model do
   it 'check if inverse_friendships association is correct' do
     expect(inverse_friendships).to eq(:has_many)
   end
+
+  context 'validation test' do
+    it 'user name is present' do
+      user = User.new(email: 'user@example.com', password: 'password').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures minimum length of name to be at least 3' do
+      user = User.new(name: 'user', email: 'user@example.com', password: 'password').save
+      expect(user).to eq(true)
+    end
+
+    it 'should save successfully' do
+      user = User.new(name: 'User1', email: 'user@gmail.com', password: 'password').save
+      expect(user) == true
+    end
+  end
+
+  context 'helper methods test' do
+    before(:each) do
+      @user1 = User.create(name: 'User1', email: 'user1@example.com', password: 'password')
+      @user2 = User.create(name: 'User2', email: 'user2@example.com', password: 'password')
+    end
+
+    it 'should return empty friends' do
+      expect(@user1.friends.empty?).to eq(true)
+    end
+
+    it 'should return one incoming friend request' do
+      @user1.friendships.new(friend_id: @user2.id, status: false).save
+      expect(@user2.friend_requests.length).to eq(1)
+    end
+
+    it 'should return one pending friend request' do
+      @user1.friendships.new(friend_id: @user2.id, status: false).save
+      expect(@user1.pending_friends.length).to eq(1)
+    end
+  end
 end
