d04db813f614924cb3a24c1b3845f0deac63c93a
samroodAli <devsamrood@gmail.com>
2021-05-20 20:54:26 +0530

    Feat:Add associations between confirmed and unconfirmed requests send and accepted

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,44 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  # outgoing  unconfirmed
+  has_many :requests_send, 
+            class_name: :Friendship, 
+            foreign_key: :user_id
 
-  # friendships
+  has_many :requestees, 
+            through: :requests_send, 
+            source: :friend
 
-  has_many :requests_send, class_name: :Friendship, foreign_key: :user_id
-  has_many :requestees, through: :requests_send, source: :friend
+  # outgoing  confirmed
+  has_many :requests_send_confirmed,
+              ->{where(confirmed: true)},
+            class_name: :Friendship,
+            foreign_key: :user_id
 
-  has_many :requests_recieved, class_name: :Friendship, foreign_key: :friend_id
-  has_many :requesters, through: :requests_recieved, source: :user
+  has_many :friends_sent_accepted,
+            through: :requests_send_confirmed,
+            source: :friend
+
+
+  # incoming unconfirmed
+
+  has_many :requests_recieved, 
+            class_name: :Friendship, 
+            foreign_key: :friend_id
+
+  has_many :requesters,
+            through: :requests_recieved, 
+            source: :user
+
+  # incoming confirmed
+
+  has_many :requests_recieved_confirmed, 
+              ->{where(confirmed: true)},
+            class_name: :Friendship, 
+            foreign_key: :friend_id
+
+  has_many :friends_accepted, 
+            through: :requests_recieved_confirmed, 
+            source: :user
 end
