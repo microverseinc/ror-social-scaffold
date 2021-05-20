00df2e303595ff48bd7e59addca243527735265c
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-20 14:47:42 +0200

    Fix text case of Friendship  model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Friendship, type: :model do
   	let(:u2) { User.create(name: 'Felix', email: 'Felix@gmail.com', password: '246888') }
 
 	it 'It check if the friendship already exist' do
-		Friendship.create(u1.id, u2.id)
+		Friendship.create({sender_id: u1.id, receiver_id: u2.id})
 	    expect(Friendship.requested_before?(u1.id,u2.id)).to eql(true)
 	    expect(Friendship.requested_before?(u1.id,3)).to eql(false)
   	end
