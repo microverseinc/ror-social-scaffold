bf86aace97f99d7a82c107cbb063ea5ec05a71e2
samroodAli <devsamrood@gmail.com>
2021-05-20 19:21:38 +0530

    Associations:Add association between requests, reciever and sender

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :requests, class_name: :Friendship, foreign_key: :user_id
-  has_many :requesteds, class_name: :Friendship, foreign_key: :friend_id
+
+  
+  has_many :requests_send, class_name: :Friendship, foreign_key: :user_id
+  has_many :requestees, through: :requests_send, source: :friend
+
+  has_many :requests_recieved, class_name: :Friendship, foreign_key: :friend_id
+  has_many :requesters, through: :requests_recieved, source: :user
 end
