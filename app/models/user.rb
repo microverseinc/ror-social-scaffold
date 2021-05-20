498ba41228e72ab0783cc2041d693b4d6531c6fa
samroodAli <devsamrood@gmail.com>
2021-05-20 18:43:07 +0530

    Feat:Add association between requests and requesteds

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :requests, class_name: :Friendship, foreign_key: :user_id
-  has_many :requestees, through: :requests, source: :user
+  has_many :requesteds, class_name: :Friendship, foreign_key: :friend_id
 end
