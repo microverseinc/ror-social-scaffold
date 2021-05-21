dbc0122da543c8eeddef9f9e5240d87ccf02625a
samroodAli <devsamrood@gmail.com>
2021-05-21 16:28:37 +0530

    Flash:Add flash messages for successful invitation and cancellation

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,17 @@
 class FriendshipsController < ApplicationController
 
   def create
+    new_request = current_user.requests_sent_unconfirmed.build(friend_id:params[:friend_id])
     user_requested = User.find(params[:friend_id])
-    current_user.users_requested<< user_requested
+    if new_request.valid?
+      current_user.requests_sent_unconfirmed << new_request 
+      flash[:notice] = "Invitation Sent to #{user_requested.name}"
+    else
+      flash[:error] = "Invitation failed, invalid request"
+    end
+
     if params[:index] == "true"
-         redirect_to users_url
+      redirect_to users_url
     else
       redirect_to user_requested
     end
@@ -14,6 +21,7 @@ class FriendshipsController < ApplicationController
   def destroy
     request = current_user.requests_sent_unconfirmed.find(params[:id]).destroy
     user_requested = User.find(params[:user_id])
+    flash[:notice] = "Cancelled invitation to #{user_requested.name}"
     if params[:index] == "true"
          redirect_to users_url
     else
