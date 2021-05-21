637a40bd36157cd4e66c7191ef0dbd6a483e3091
samroodAli <devsamrood@gmail.com>
2021-05-21 18:21:47 +0530

    Relationship:Method in helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
   def requesting_user?(user)
     current_user.users_requesting_ids.include?(user.id)
   end
+
+  def relationship(user)
+    Relationship.find_by(user_id: user.id,friend_id:current_user.id) 
+    ||
+    Relationship.find_by(friend_id:current_user.id,friend_id:user.id)
+  end
 end
