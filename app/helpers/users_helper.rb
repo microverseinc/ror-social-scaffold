a19cefc64735f2c52db22b03af094be8120222fd
samroodAli <devsamrood@gmail.com>
2021-05-21 17:21:29 +0530

    Accept:Add acceptance button in pending invitations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
   def requested_user?(user)
     current_user.users_requested_ids.include?(user.id)
   end
+
+  def requesting_user?(user)
+    current_user.users_requesting_ids.include?(user.id)
+  end
 end
