956ffec10e058a16c873701d53720c1a2fbda285
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-05-21 16:06:55 +0100

    Add helper methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,30 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :inverse_friendships, class_name: 'friendship', foreign_key: 'friend_id'
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+end
+
+
+def confirm_friend(user)
+  friendship = inverse_friendships.find{|friendship| friendship.user == user}
+  friendship.confirmed = true
+  friendship.save
+end
+
+def friend?(user)
+  friends.include?(user)
 end
