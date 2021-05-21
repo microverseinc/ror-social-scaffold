73ea64812f1737e72420c23528ab6cd54a42b98e
samroodAli <devsamrood@gmail.com>
2021-05-21 15:54:36 +0530

    Redirect:Redirect to users page or index page when inviting or cancelling

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,13 +3,21 @@ class FriendshipsController < ApplicationController
   def create
     user_requested = User.find(params[:friend_id])
     current_user.users_requested<< user_requested
-    redirect_to user_requested
+    if params[:index] == "true"
+         redirect_to users_url
+    else
+      redirect_to user_requested
+    end
   end 
 
 
   def destroy
     request = current_user.requests_sent_unconfirmed.find(params[:id]).destroy
     user_requested = User.find(params[:user_id])
-    redirect_to user_requested
+    if params[:index] == "true"
+         redirect_to users_url
+    else
+      redirect_to user_requested
+    end
   end
 end
