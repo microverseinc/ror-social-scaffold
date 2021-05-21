50968c5615a5e6665be3d8e7d70a74678e4ae3f1
samroodAli <devsamrood@gmail.com>
2021-05-21 14:01:58 +0530

    Feat:Add friendship request partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @request = current_user.requests_send_unconfirmed.build
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
