36cd928c98801e51cd9bc981a91697b8e2e7eea6
samroodAli <devsamrood@gmail.com>
2021-05-21 15:19:00 +0530

    Cancel:Add option to cancel requests for users

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,15 @@
 class FriendshipsController < ApplicationController
 
   def create
-    requestee = User.find(params[:friend_id]) 
-    current_user.users_requested << requestee
-    redirect_to requestee
+    user_requested = User.find(params[:user_id]) 
+    current_user.users_requested << user_requested
+    redirect_to user_requested
   end
 
+
+  def destroy
+    request = current_user.requests_sent_unconfirmed.find(params[:id]).destroy
+    user_requested = User.find(params[:user_id])
+    redirect_to user_requested
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_132903) do
+ActiveRecord::Schema.define(version: 2021_05_21_093214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2021_05_20_132903) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "confirmed", default: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
