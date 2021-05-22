c32bd2db83a179215785e582eb823300fa8aba45
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-05-22 16:49:25 +0200

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,8 +62,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
+  Shoulda::Matchers.configure do |configs|
+    configs.integrate do |with|
       # Choose a test framework:
       with.test_framework :rspec
       with.library :rails
