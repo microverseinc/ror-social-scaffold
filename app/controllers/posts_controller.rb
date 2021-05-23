9e98a53e4cb5e223982023d2c92203555490ca00
Fernando Marques <frpm1001@gmail.com>
2021-05-22 22:59:32 -0300

    Update views, fix rubocop and rspec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,16 +21,11 @@ class PostsController < ApplicationController
 
   def timeline_posts
     target_posts = []
-    current_user.friends_id.each do |friend_id|
-      target_posts << Post.where(user_id: friend_id).all
+    current_user.friends.each do |friend|
+      target_posts << friend.posts
     end
     target_posts << current_user.posts
-    @timeline_posts ||= target_posts.flatten.sort_by(&:updated_at).reverse.uniq
-
-    # SELECT content, user_id, created_at, updated_at
-    # FROM users JOIN posts on users.id == posts.user_id
-    # WHERE user.id == user_id
-    # new_table = User.joins('INNER JOIN posts ON users.id == posts.user_id')
+    @timeline_posts ||= target_posts.flatten.sort_by(&:updated_at).reverse
   end
 
   def post_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,44 +16,42 @@ class User < ApplicationRecord
   has_many :invitations_sent, foreign_key: :inviter_id, class_name: 'Friendship', dependent: :destroy
   has_many :who_i_invited, through: :invitations_sent, source: :invitee, dependent: :destroy
 
-  def friendship_requests(user)
+  def friendship_request(user)
     return false if who_i_invited.include?(user) || who_invited_me.include?(user)
 
     who_i_invited << user
   end
 
   def accept_request(user)
-    friend_request = invitations_i_got.where(inviter_id: user.id).first
+    friend_request = invitations_received.where(inviter_id: user.id).first
     friend_request.update(accepted: true)
   end
 
   def reject_request(user)
-    friend_request = invitations_i_got.where(inviter_id: user.id).first
+    friend_request = invitations_received.where(inviter_id: user.id).first
     friend_request.destroy
   end
 
   def friends
-    User.where(id: invitations_i_got.where(accepted: true).pluck(:inviter_id))
+    User.where(id: invitations_received.where(accepted: true).pluck(:inviter_id))
   end
 
   def friend?(user)
     friends.include?(user)
   end
 
-  def pending_requests_received?(user)
-    pending_requests_i_received.include?(user)
-  end
-
   def pending_requests_sent?(user)
     pending_requests_i_sent.include?(user)
   end
 
-  # People who haven't accepted my request yet
+  def pending_requests_received?(user)
+    pending_requests_i_received.include?(user)
+  end
+
   def pending_requests_i_sent
     invitations_sent.map { |friendship| friendship.invitee unless friendship.accepted }
   end
 
-  # People who I haven't accepted their request yet
   def pending_requests_i_received
     invitations_received.map { |friendship| friendship.inviter unless friendship.accepted }
   end
