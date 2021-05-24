d4ecf9790e376bf5bf430b5ff4a4db71fa9c30db
samroodAli <devsamrood@gmail.com>
2021-05-24 19:38:15 +0530

    Fix:Fix rubocop linter offenses

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -19,21 +19,20 @@ feature 'Friendship integration testing by requests sent and accepted' do
     expect(page).to have_selector('.users-list')
     expect(page).to have_selector('.user')
   end
-  
+
   scenario 'users have invite to friendship button' do
     visit users_path
     expect(page).to have_selector('.user-name')
     expect(page).to have_selector("input[value='Invite to friendship']")
-    page.has_button?("Invite to friendship")
+    page.has_button?('Invite to friendship')
   end
-  
+
   scenario 'users have option to invite to friendship and can cancel invite' do
     visit user_path(users(:John))
-    count_before  = Friendship.count
+    count_before = Friendship.count
     click_button('Invite to friendship')
     expect(Friendship.count).to eq(count_before + 1)
     click_button('Cancel invitation')
     expect(Friendship.count).to eq(count_before)
   end
-
 end
