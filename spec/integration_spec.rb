cdf32993f7052831fa01085ad802fdaf129e0978
samroodAli <devsamrood@gmail.com>
2021-05-24 18:50:22 +0530

    Test:Add test for new post and users in users page

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
+require './spec/support/helpers'
 
-feature 'Sends an invitation' do
-  scenario 'with valid email and password' do
-    visit root_path
-    expect(page).to have_content('You need to sign in')
+feature 'Friendship integration testing by requests sent and accepted' do
+  background do
+    sam = User.new(name: 'samrood', email: 'user@example.com', password: 'password')
+    sign_up_with(sam)
+  end
+
+  scenario 'home page has new post input tag' do
+    page.has_selector?('#post_content')
+  end
+
+  scenario 'users page has users' do
+    visit users_path
+    page.has_selector?('.user')
   end
 end
