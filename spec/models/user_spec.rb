c9bcfad5cbb4c6616a7054ef64b02cea244c947d
samroodAli <devsamrood@gmail.com>
2021-05-24 15:59:24 +0530

    Test:Write validity checks for user name and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,25 @@ RSpec.describe User do
     end
   end
 
+  describe :invalid_name do
+    it "should have valid name" do
+      user = User.new(name:'',email:'some@gmail.com')
+      user.valid?
+      expect(user.errors.full_messages).to include("Name can't be blank")
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe :invalid_password do
+    it "should have valid name" do
+      user = User.new(name:'samrood',email:'some@gmail.com')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+      expect(user).not_to be_valid
+    end
+  end
+
+
   describe :invalidity do
     it 'should not be valid with blank fields' do
       expect(invalid_user).not_to be_valid
@@ -28,7 +47,7 @@ RSpec.describe User do
   describe 'friends should have request sent' do
     it 'should have user sam from fixtures in friends array' do
       friend_requested = users(:Sam)
-      expect(subject.friends_from_requests_sent).to be_include(friend_requested)
+      expect(subject.friends_from_requests_sent).to include(friend_requested)
     end
   end
 
