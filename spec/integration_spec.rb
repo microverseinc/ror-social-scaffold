0fb4e521d903937345dff45a056abd586f5660bf
samroodAli <devsamrood@gmail.com>
2021-05-24 19:05:01 +0530

    Test:Add fixtures to integration tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require './spec/support/helpers'
 
 feature 'Friendship integration testing by requests sent and accepted' do
+  fixtures :users
+  fixtures :friendships
   background do
     sam = User.new(name: 'samrood', email: 'user@example.com', password: 'password')
     sign_up_with(sam)
@@ -13,6 +15,15 @@ feature 'Friendship integration testing by requests sent and accepted' do
 
   scenario 'users page has users' do
     visit users_path
+    expect(page).to have_selector('.user-name')
+    expect(page).to have_selector('.users-list')
     expect(page).to have_selector('.user')
+  end
+  
+  scenario 'users have invite to friendship button' do
+    visit users_path
+    expect(page).to have_selector('.user-name')
+    expect(page).to have_selector("input[value='Invite to friendship']")
+    
   end
 end
