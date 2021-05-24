4eb799b1e23a868d4dbe848bf06b5b33f4139148
samroodAli <devsamrood@gmail.com>
2021-05-24 16:19:53 +0530

    Test:Write t to check devise

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,1 +1,8 @@
+require 'rails_helper'
 
+feature 'Sends an invitation' do
+  scenario 'with valid email and password' do
+    visit root_path
+    expect(page).to have_content('You need to sign in')
+  end
+end
