caf2f7b3b86cf879a1370138e770042f4c49c81d
verissimohenry <verissimohenry04@gmail.com>
2021-05-31 17:05:46 +0100

    add rspec for friendship and user modfy files

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context ' test friendship model' do
+    before do
+      @user = User.create(name: 'henry', email: 'verissimohemry04@gmail.com', password: '123456')
+      @friend = User.create(name: 'tobi', email: 'tobi@gmail.com', password: '123456')
+    end
+    it 'return false if friend_id is not provided' do
+      @invite = Friendship.create(user_id: @user.id)
+      expect(@invite).to_not be_valid
+    end
+    it 'return false if user_id is not provided' do
+      @invite = Friendship.create(friend_id: @friend.id)
+      expect(@invite).to_not be_valid
+    end
+
+    it 'return true if user_id and friend_id got provided' do
+      @invite = Friendship.create(user_id: @user.id, friend_id: @friend.id)
+      expect(@invite).to be_valid
+    end
+  end
 end
