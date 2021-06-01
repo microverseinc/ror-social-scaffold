2f65463f0f80aad2746249e3d93e1b5eb8415791
verissimohenry <verissimohenry04@gmail.com>
2021-06-01 16:22:59 +0100

    modify controller folder

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,37 +20,7 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
-  end
-
-  def post_params
-    params.require(:post).permit(:content)
-  end
-end
-
-class PostsController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @post = Post.new
-    timeline_posts
-  end
-
-  def create
-    @post = current_user.posts.new(post_params)
-
-    if @post.save
-      redirect_to posts_path, notice: 'Post was successfully created.'
-    else
-      timeline_posts
-      render :index, alert: 'Post was not created.'
-    end
-  end
-
-  private
-
-  def timeline_posts
-    @timeline_posts = Post.where(user: (current_user.friends.to_a << current_user)).ordered_by_most_recent
+    @timeline_posts = Post.where(user: (current_user.friendss.to_a << current_user)).ordered_by_most_recent
   end
 
   def post_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships
 
-  def friends
+  def friendss
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
     friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
