60f8efecb2763cdbb225e4c8dffa08f6438dde39
verissimohenry <verissimohenry04@gmail.com>
2021-06-01 15:24:46 +0100

    modify contriller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @sent = Friendship.where(user_id: current_user.id).where(confirmed: nil)
+    @pending = Friendship.where(friend_id: current_user.id).where(confirmed: nil)
   end
 end
