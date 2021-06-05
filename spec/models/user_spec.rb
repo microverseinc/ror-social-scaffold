2599393c3ae56dc99d90825e161cc809dbfb9e08
francis uloko <francisuloko@yahoo.com>
2021-06-05 19:39:33 +0100

    Fix rubocop trailing whitespace error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
   describe 'Associations' do
     it { should have_many(:friendships) }
     it { should have_many(:inverse_friendships) }
