6674bf4dc312a533e198d3a705de8ebf5659b475
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-06-16 15:54:33 +0100

    Create model with associations and features

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
