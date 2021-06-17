b141b1ba514ae75d4b9e5c4730300f2fbcc6259d
ivana <ivananl@yahoo.com>
2021-06-17 15:57:14 +0200

    Add friendship model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |index|
+    User.create(name: "Doe#{index}", email: "doe#{index}@gamil.com", password: "#{index}123456", password_confirmation: "#{index}123456")
+end
