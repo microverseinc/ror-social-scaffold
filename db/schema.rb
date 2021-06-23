ce38d85b97dff88b2d1d5d5c4a464693c9316b6c
od-c0d3r <omar_rashad@aol.com>
2021-06-23 12:38:49 +0200

    Add double index column to Friendships table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_101641) do
+ActiveRecord::Schema.define(version: 2021_06_23_083913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(version: 2021_06_21_101641) do
 
   create_table "friendships", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "friend_id" 
+    t.bigint "friend_id"
     t.boolean "confirmed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
