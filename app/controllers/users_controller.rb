fc2747d73e2ee3eef21a4e767cf1e11a17b49f85
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-23 16:25:27 +0200

    Add my friends action to list all friends and show their friendship status.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @friendships = @user.friendships.all
-    # debugger
+   
     @posts = @user.posts.ordered_by_most_recent
   end
 
   def my_friends
-    @friendships = current_user.friendships
+    @friendships = Friendship.where(user_id: current_user.id, confirmed: true)
+    @friendships += Friendship.where(friend_id: current_user.id, confirmed: true)
   end
 
   def requests
-    @pending_requests = current_user.friendships.where(user_id: current_user.id, confirmed:nil)  
-    @friend_requests = current_user.friendships.where(friend_id: current_user.id, confirmed:nil)
+    @pending_requests = Friendship.where(user_id: current_user.id, confirmed:nil)  
+    @friend_requests = Friendship.where(friend_id: current_user.id, confirmed:nil)
+    
   end
 end
