ccf76fc221fa3989dd24e4ea295254218971138c
Victor Otieno <victorotieno598@gmail.com>
2021-06-23 12:13:16 +0300

    Add confirm method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,17 @@ class UsersController < ApplicationController
     @all_friends = []
     @friends.each { |u| @all_friends << User.find(u.user_id) }
   end
+
+  def confirm
+    user = User.find(params[:friend_id])
+    friend = Friendship.where('friend_id = ? and user_id = ?', current_user.id, user.id).first
+    friend.status = true
+    if friend.save
+      @add_friend = Friendship.new(user_id: current_user.id, friend_id: user.id, status: true)
+      @add_friend.save
+      redirect_to user_path(current_user), notice: 'Accepted'
+    else
+      redirect_to user_path(current_user), alert: 'Rejected'
+    end
+  end
 end
