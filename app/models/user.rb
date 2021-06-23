ee6dd9f3988b796976df4854a2cca2ed58619100
Victor Otieno <victorotieno598@gmail.com>
2021-06-23 15:16:57 +0300

    Adjust show method and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
+  end
 end
