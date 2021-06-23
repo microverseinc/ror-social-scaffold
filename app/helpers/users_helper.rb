bb4d0c309562f6b222949eccb4d785baad739032
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-23 23:16:59 +0200

    Create garvatar helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,5 +5,11 @@ module UsersHelper
     def is_friend?(user)
         !!(current_user.friendships.where(user_id: current_user.id ,friend_id:user.id,confirmed: true) || current_user.friendships.where(user_id: user.id, friend_id: current_user.id, confirmed: true))
     end
+
+    def gravatar_for(user)
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
     
 end
