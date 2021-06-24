74bcd53a42eb92bda2f753a447a238091ed7f166
Victor Otieno <victorotieno598@gmail.com>
2021-06-24 15:18:21 +0300

    Add method to return logged in user's post and their friends'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,10 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    # returns ids of friends of currently logged in user
+    ids = current_user.friendships.pluck(:id) << current_user.id
+    # returns posts of currently logged in user and their friends 
+    @timeline_posts ||= Post.where(user_id: ids).ordered_by_most_recent.includes(:user)
   end
 
   def post_params
