0a95bc97356727c63e4c3e3071526335258d4699
Victor Otieno <victorotieno598@gmail.com>
2021-06-25 13:40:17 +0300

    Fix uninitialized post class

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 1000,
+                                                too_long: '1000 characters in post is the maximum allowed.' }
+
+  belongs_to :user
+
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+end
