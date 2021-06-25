a7eb81ff56a972f52cb8272db1431c2c37df2e66
od-c0d3r <omar_rashad@aol.com>
2021-06-25 20:46:48 +0200

    Fix: Add friend link is functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,39 +21,3 @@ class UsersController < ApplicationController
     @friend_requests = Friendship.where(friend_id: current_user.id, confirmed: nil)
   end
 end
-
-
-
-# friend_arr for the current_user
-
-# def current_friends
-  # 0- friend_arr = [] 
-  # 1- get user friendships collection
-  # 2- loop over friendships
-      # check if friendship.user_id == current_user.id
-        # friend_arr << User.find_by(id: friendship.friend_id)
-        # else 
-          # friend_arr << User.find_by(id: friendship.user_id)
-        #end
-# return current_friends
-
-
-# friend_arr for the friend
-
-# def user_friends(user)
-  # 0- friend_arr = [] 
-  # 1- get user friendships collection
-  # 2- loop over friendships
-      # check if friendship.user_id == user.id
-        # friend_arr << User.find_by(id: friendship.friend_id)
-        # else 
-          # friend_arr << User.find_by(id: friendship.user_id)
-        #end
-# return user_friends
-
-
-# def mutual_arr(current_friends,user_friends)
-  # mutual_arr = current_friends & user_friends
-  # return mutual_num = mutual_arr.cout
-
-
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
 
   def request_sent?(friend)
-    !current_user.friendships.where(friend_id: friend.id, confirmed: nil).empty?
+    current_user.friendships.where(friend_id: friend.id, confirmed: nil).empty?
   end
 
   def is_friend?(user)
-    value1 = current_user.friendships.where(user_id: current_user.id, friend_id: user.id, confirmed: true)
-    value2 = current_user.friendships.where(user_id: user.id, friend_id: current_user.id, confirmed: true)
-    return !!(value1 || value2)
+    value1 = Friendship.where(user_id: current_user.id, friend_id: user.id, confirmed: true)
+    value2 = Friendship.where(user_id: user.id, friend_id: current_user.id, confirmed: true)
+    value1.present? || value2.present? 
   end
 
   def gravatar_for(user)
