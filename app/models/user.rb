710cf35f96c1abb8246856e4f4ffc5741a1ced36
Daniel Samuel <dansam5000@gmail.com>
2021-06-25 13:20:22 +0100

    Create a friends method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
-end
+
+  # Helper methods
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  end
