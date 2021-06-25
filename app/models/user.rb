cf1c2a95ae3035b25985d39a5b77553d8c300fed
Daniel Samuel <dansam5000@gmail.com>
2021-06-25 14:05:32 +0100

    Add pending freind request and friend request methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,15 @@ class User < ApplicationRecord
     friends_array.compact
   end
 
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
   end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  
+end
