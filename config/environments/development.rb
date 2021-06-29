23e3eb0e6aee30a5887241a6f2faa78013f84778
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-06-28 20:58:34 -0600

    successfully setup letter opener in development enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,16 +39,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  LetterOpener.configure do |config|
-    # To overrider the location for message storage.
-    # Default value is `tmp/letter_opener`
-    config.location = Rails.root.join('tmp', 'my_mails')
-
-    # To render only the message body, without any metadata or extra containers or styling.
-    # Default value is `:default` that renders styled message with showing useful metadata.
-    config.message_template = :light
-  end
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'https://fast-wildwood-38105.herokuapp.com' }
 end
