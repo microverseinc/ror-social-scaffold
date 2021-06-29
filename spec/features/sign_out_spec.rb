e329543f4666ed62f77725b2161aee2ab7605c8b
henryhaulka <onuhenry24@gmail.com>
2021-06-29 15:37:15 +0100

    add integrated test for sign out

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,13 @@
+describe "Sign out" , type: :feature do
+    before :each do
+        User.create(email: 'user@example.com', password: 'password', name: "user")
+        visit '/users/sign_in'
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'password'
+        click_button "Log in"
+      end
+      it "signs me out" do
+        click_link 'Sign out'
+        expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      end
+end
