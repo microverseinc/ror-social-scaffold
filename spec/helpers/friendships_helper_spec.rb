c0c34a561aa5b317674eaca8689841c37bef46ca
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-06-29 12:25:14 +0200

    Add rspec tests for user model

--- spec/helpers/friendships_helper_spec.rb
+++ spec/helpers/friendships_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe FriendshipsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe FriendshipsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,19 @@ require "rails_helper"
 
 RSpec.describe User, :type => :model do
 
-  context "Associations" do
-    it "orders them in reverse chronologically" do
-      post = Post.create!
-      comment1 = post.comments.create!(:body => "first comment")
-      comment2 = post.comments.create!(:body => "second comment")
-      expect(post.reload.comments).to eq([comment2, comment1])
-    end
+  describe "associations" do
+    it { should have_many(:posts) }
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
+    it { should have_many(:friendships) }
+    it { should have_many(:inverse_friendships).class_name("Friendship") }
+
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(20) }
+  end
+
+  
 end
