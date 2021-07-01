70492d9ae144bbe8a81095a579591105b1b7c9da
Daniel Samuel <dansam5000@gmail.com>
2021-07-01 12:12:26 +0100

    Implement action allow User invite freind request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def request_friend
+    request = current_user.friendships.build(friend_id: params['id'], status: false)
+    request.save
+    flash[:notice] = 'Friend Request Successfully sent'
+    redirect_to users_path
+  end
+
+  
 end
