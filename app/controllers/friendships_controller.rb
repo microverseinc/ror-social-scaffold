d6d83c865d2ab061f312303cc633ac1a767046b3
JuliCarracedo <julicarra@hotmail.com>
2021-07-03 19:21:55 -0300

    Fix linters

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -10,7 +10,10 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    friendship = Friendship.find_by(inviter_id: params[:inviter_id], invitee_id: params[:invitee_id]) || Friendship.find_by(inviter_id: params[:invitee_id], invitee_id: params[:inviter_id])
+    friendship = Friendship.find_by(inviter_id: params[:inviter_id],
+                                    invitee_id: params[:invitee_id]) || Friendship.find_by(
+                                      inviter_id: params[:invitee_id], invitee_id: params[:inviter_id]
+                                    )
     friendship.delete
     redirect_to user_path(User.find(params[:show_user]))
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,38 +10,39 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :friendships 
+  has_many :friendships
   has_many :friendship_invitees, foreign_key: :inviter_id, class_name: 'Friendship'
   has_many :invitees, through: :friendship_invitees
   has_many :friendship_inviters, foreign_key: :invitee_id, class_name: 'Friendship'
   has_many :inviters, through: :friendship_inviters
 
   def friends
-  invitee_friends + inviter_friends
-  end
-  def friends_unfiltered
-  invitees + inviters
+    invitee_friends + inviter_friends
   end
 
- def pending_inviter_friends
-  inviter_ids = friendship_inviters.pending.select(:inviter_id)
-  User.where(id: inviter_ids)
- end
- 
- def pending_invitee_friends
-  invitee_ids = friendship_invitees.pending.select(:invitee_id)
-  User.where(id: invitee_ids)
- end
+  def friends_unfiltered
+    invitees + inviters
+  end
+
+  def pending_inviter_friends
+    inviter_ids = friendship_inviters.pending.select(:inviter_id)
+    User.where(id: inviter_ids)
+  end
+
+  def pending_invitee_friends
+    invitee_ids = friendship_invitees.pending.select(:invitee_id)
+    User.where(id: invitee_ids)
+  end
 
   private
- def invitee_friends
-  invitee_ids = friendship_invitees.accepted.select(:invitee_id)
-  User.where(id: invitee_ids)
- end
- def inviter_friends
-  inviter_ids = friendship_inviters.accepted.select(:inviter_id)
-  User.where(id: inviter_ids)
- end
 
+  def invitee_friends
+    invitee_ids = friendship_invitees.accepted.select(:invitee_id)
+    User.where(id: invitee_ids)
+  end
 
+  def inviter_friends
+    inviter_ids = friendship_inviters.accepted.select(:inviter_id)
+    User.where(id: inviter_ids)
+  end
 end
