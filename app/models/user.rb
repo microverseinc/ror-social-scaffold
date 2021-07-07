f0d7760b8c7ea6630f46d08b44ada611628808a7
unknown <danielmainawaweru@gmail.com>
2021-07-07 12:40:01 +0300

    Add association between the friendship and the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 end
