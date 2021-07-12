85a4e0614f6da9328e7f2fe48106180982b88693
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-12 18:51:13 -0300

    Change Invitation#create redirection page
    It now redirects to invited user's profile page.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,7 +2,7 @@ class InvitationsController < ApplicationController
   def create
     invitation = Invitation.new(user_id: params[:user], friend_id: params[:friend], confirmed: false)
     if invitation.save
-      redirect_to current_user
+      redirect_to User.find(invitation.friend_id)
     else
       render
     end
