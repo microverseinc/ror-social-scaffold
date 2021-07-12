04930b845580a9b3fd0b8ffe8d8233e5226a3945
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-07-12 16:19:24 +0100

    add invitation link

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-
-  get 'invitations/update'
-  get 'invitations/create'
-  get 'invitations/destroy'
   root 'posts#index'
 
   devise_for :users
 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
-  resources :comments, only: [:create]
-  resources :likes, only: [:create, :destroy]
-  end
-  resources :invitations, only:[:create]
+      resources :comments, only: [:create]
+      resources :likes, only: [:create, :destroy]
+    end
+  resources :invitations, only:[:create, :destroy]
+    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
