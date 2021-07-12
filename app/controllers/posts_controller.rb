182f8599b20be1d645d89d87a6dbef2b828508ed
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-07-12 18:52:35 -0300

    Filter timeline posts
    Now a user can only see their own and their friends' posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,11 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    relevant_posts = current_user.posts
+    current_user.friends.includes(:posts).each do |friend|
+      relevant_posts += friend.posts
+    end
+    @timeline_posts = relevant_posts
   end
 
   def post_params
