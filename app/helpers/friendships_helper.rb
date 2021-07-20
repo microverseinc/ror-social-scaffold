1686a158db0a7cf0ca7c98cbc4df008f0cd84045
Ángel Díaz <diazangel9311@gmail.com>
2021-07-20 12:30:26 -0500

    Add link to freind requester page

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,7 +1,7 @@
 module FriendshipsHelper
   def new_request?(friend)
     request1 = Friendship.find_by(user_id: current_user.id, friend_id: friend, confirmed: false)
-    request2 = Friendship.find_by(user_id: current_user.id, friend_id: friend, confirmed: false)
+    request2 = Friendship.find_by(user_id: friend, friend_id: current_user.id, confirmed: false)
     request1.nil? && request2.nil?
   end
 
@@ -13,6 +13,11 @@ module FriendshipsHelper
 
   def pending_requests?
     requests = Friendship.where(friend_id: current_user.id, confirmed: false)
+    requests.empty?
+  end
+
+  def pending_request?(user)
+    requests = Friendship.where(user_id: user, friend_id: current_user.id, confirmed: false)
     requests.empty?
   end
 end
