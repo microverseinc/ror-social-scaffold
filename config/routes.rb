8dfaa21cf0819adbddac9d461c6bb5ee418026a1
tmampa <tmampa94@gmail.com>
2021-07-21 15:43:03 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  post 'friendships/create'
   root 'posts#index'
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do 
+      post :confirm
+      delete :destroy
+    end
+  end
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
