2004c5059f5c1f6c0f94d183fb67b0e85e571092
tmampa <tmampa94@gmail.com>
2021-07-21 15:48:21 +0200

    Fix user table show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
     @friends = Friendship.where('friend_id = ? and status = ?', current_user.id, true)
-    @friends.all.map(&:user_id)
-    @all_friends = []
-    @friends.each { |u| @all_friends << User.find(u.user_id) }
+    # @friends.all.map(&:user_id)
+    # @all_friends = []
+    # @friends.each { |u| @all_friends << User.find(u.user_id) }
   end
 
   def confirm
