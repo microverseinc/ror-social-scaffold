6a88b4b6435f3efbda4f20caf46db0350fa14563
tmampa <tmampa94@gmail.com>
2021-07-21 15:16:01 +0200

    Show friendship status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @friends = Friendship.where('friend_id = ? and status = ?', current_user.id, true)
+    @friends.all.map(&:user_id)
+    @all_friends = []
+    @friends.each { |u| @all_friends << User.find(u.user_id) }
   end
 
   def confirm
