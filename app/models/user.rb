882b1c19edb46c3ba951e6ff8c88a71d75a8e303
tongoonamujera <tongoonamujera@gmail.com>
2021-08-20 17:44:35 +0200

    Add relationships between user and frienship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 end
