7ad917234697f444348aa0b3f43540faed4bbe60
tongoonamujera <tongoonamujera@gmail.com>
2021-08-20 17:35:04 +0200

    Add friendship model

---
+++ app/views/friendships/_friendship.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! friendship, :id, :user_id, :created_at, :updated_at
+json.url friendship_url(friendship, format: :json)
---
+++ db/migrate/20210820152656_create_friendships.rb
@@ -0,0 +1,10 @@
+class CreateFriendships < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
