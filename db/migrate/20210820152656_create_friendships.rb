ce7ad7afd6c6d9f01ec17f858d4c755ed9d1607b
tongoonamujera <tongoonamujera@gmail.com>
2021-08-22 20:25:24 +0200

    Add more migrations to friendships table

--- db/migrate/20210820152656_create_friendships.rb
+++ db/migrate/20210820152656_create_friendships.rb
@@ -2,7 +2,9 @@ class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
       t.references :user, null: false, foreign_key: true
-      t.boolean :confirmed
+      t.references :request_sent_to, null: false, foreign_key: {to_table: :users}
+      t.references :request_sent_by, null: false, foreign_key: {to_table: :users}
+      t.boolean :confirmed, default: false
 
       t.timestamps
     end
