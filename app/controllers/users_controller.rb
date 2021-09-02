2e97fb740b6d094d58a78860bcaccd583b8a2b42
Uzair <uzairali19b@gmail.com>
2021-09-02 16:28:45 +0500

    update friendships and user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,22 +19,30 @@ class UsersController < ApplicationController
     friend = Friendship.where('friend_id = ? and user_id = ?', current_user.id, user.id).first
     friend.confirmed = true
     if friend.save
-      @add_friend = Friendship.new(user_id: current_user.id, friend_id: user.id, status: true)
+      @add_friend = Friendship.new(user_id: current_user.id, friend_id: user.id, confirmed: true)
       @add_friend.save
       redirect_to user_path(current_user), notice: 'Friend Request Accepted'
     else
       redirect_to user_path(current_user), alert: 'Friend Request Rejected'
     end
   end
-end
 
-def friends?(user)
-  Friendship.exists?(user_id: current_user.id, friend_id: user)
-end
-
-def sent_requests
-  @requested_friends = []
-  @sent_requests = Friendship.where('user_id = ? and status = ?', current_user.id, false)
-  @sent_requests.each { |u| @requested_friends << User.find(u.friend_id) }
-  @requested_friends
+  def destroy
+    user = User.find(params[:friend_id])
+    first_friend = Friendship.where('friend_id = ? and user_id = ?', user.id, current_user.id).first
+    second_friend = Friendship.where('friend_id = ? and user_id = ?', current_user.id, user.id).first
+    if !first_friend.nil?
+      if first_friend.delete
+        redirect_to user_path(current_user), notice: 'Friend Request Rejected'
+      else
+        redirect_to user_path(current_user), alert: 'Please Try Again'
+      end
+    elsif !second_friend.nil?
+      if second_friend.delete
+        redirect_to user_path(current_user), notice: 'Friend Request Rejected'
+      else
+        redirect_to user_path(current_user), alert: 'Please Try Again'
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,22 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friend_requests
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
+
+  def friends?(user)
+    Friendship.exists?(user_id: current_user.id, friend_id: user)
+  end
+
+  def sent_requests
+    @requested_friends = []
+    @sent_requests = Friendship.where('user_id = ? and confirmed = ?', current_user.id, false)
+    @sent_requests.each { |u| @requested_friends << User.find(u.friend_id) }
+    @requested_friends
   end
 end
