190af6d6279b5d94bafa2565f9bfdcbabee5311a
eroauditore <alfa.fenrir@gmail.com>
2021-10-06 12:32:32 -0500

    Comments controller API json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_sessiongit
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @comments = Post.find(params[:post_id]).comments
+    respond_to do |format|
+      format.html { @comments }
+      format.xml { render xml: @comments }
+      format.json { render json: @comments }
+    end
+  end
   def create
     @comment = Comment.new(comment_params)
     @comment.post_id = params[:post_id]
     @comment.user = current_user
-
     if @comment.save
       respond_to do |format|
-        format.json { render :json => @comment }
+        format.json { render json: @comment, status: :created }
+        format.html { redirect_to posts_path, notice: 'Comment was successfully created.' }
       end
     else
       redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
     end
   end
-
   private
-
   def comment_params
     params.require(:comment).permit(:content)
   end
