0c3efd53ad12dd6074fe4849a16a3ea51a46fc3a
eroauditore <alfa.fenrir@gmail.com>
2021-10-06 12:49:37 -0500

    Rubocop corrections

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
       format.json { render json: @comments }
     end
   end
+
   def create
     @comment = Comment.new(comment_params)
     @comment.post_id = params[:post_id]
@@ -21,7 +22,9 @@ class CommentsController < ApplicationController
       redirect_to posts_path, alert: @comment.errors.full_messages.join('. ').to_s
     end
   end
+
   private
+
   def comment_params
     params.require(:comment).permit(:content)
   end
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,4 +1,3 @@
-
 module Response
   def json_response(object, status = :ok)
     render json: object, status: status
