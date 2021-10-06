b6dbdf86b9157047068432594c47009b74d561d8
eroauditore <alfa.fenrir@gmail.com>
2021-10-06 12:31:04 -0500

    applitacion controller api protect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
