32d4321e266516adbeb9eb43bea2d3368281c96e
Francis Uloko <francisuloko@yahoo.com>
2021-10-09 19:44:44 +0100

    Fix linters error

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,25 +7,26 @@ class Api::SessionsController < Devise::SessionsController
     unless request.format == :json
       sign_out
       render status: 406,
-             json: { message: "JSON request only." } and return
+             json: { message: 'JSON request only.' } and return
     end
 
     # auth_options should have `scope: :api_user`
     resource = warden.authenticate!(auth_options)
 
     if resource.blank?
-      render status: 401, json: { response: "Access denied." } and return
+      render status: 401, json: { response: 'Access denied.' } and return
     end
 
     sign_in(resource_name, resource)
     respond_with resource, location:
       after_sign_in_path_for(resource) do |format|
-        format.json {
+        format.json do
           render json:
           { success: true,
             jwt: current_token,
-            response: "Authentication successful." }
-        }
+            response: 'Authentication successful.'
+          }
+        end
       end
   end
 
