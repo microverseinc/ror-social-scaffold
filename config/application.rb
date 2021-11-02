7ba14ecf7c783222edc62c769fb9cc3266a6cd9f
elyor doniyorov <doniyorovelyor@gmail.com>
2021-11-02 17:40:43 +0500

    changing an existing app

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module RorSocialScaffold
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.debug_exception_response_format = :api
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
