05c07b9b8cad7941e95e43d3cb54011cf07d81cb
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-02 13:58:08 +0100

    Create routes for posts endpoint

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,8 @@
+class ApiController < ApplicationController
+  before_action :set_default_format
+
+  private
+  def set_default_format
+    request.format = :json
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :create]
+    end 
+  end
+
   post 'friendships/create'
   root 'posts#index'
 
