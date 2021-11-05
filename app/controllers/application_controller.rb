f3c3c467cdc41b526b4e3b8e6da88e293ae9adae
aristides1000 <aristides1000@gmail.com>
2021-11-05 16:54:20 -0400

    Fix rubocop linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, if: :json_request?
-  #protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
